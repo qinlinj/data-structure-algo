5da3b59b6ec77238da02152851a4ad83eb36739b
qinlinj <justinqinlin@gmail.com>
2025-02-07 14:39:21 -0800

    test(ArrayStackTest): add a test class for ArrayStack to validate stack operations

---
+++ src/main/java/org/qinlinj/linear/stack/ArrayStackTest.java
@@ -0,0 +1,19 @@
+package org.qinlinj.linear.stack;
+
+public class ArrayStackTest {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new ArrayStack<>(15);
+//        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(10);
+        System.out.println(stack);
+        stack.push(20);
+        System.out.println(stack);
+        stack.push(30);
+        System.out.println(stack);
+
+        stack.pop();
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+    }
+}
