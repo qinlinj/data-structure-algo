4e0ef2f3a60744457b432fd46d5e3a02d26ee8d4
qinlinj <justinqinlin@gmail.com>
2025-02-14 01:42:21 -0800

    feat(ThreeWayQuickSorter): implement three-way quicksort algorithm for improved sorting efficiency and handling of duplicate values

--- src/main/java/org/qinlinj/linear/algo/sort/ThreeWayQuickSorter.java
+++ src/main/java/org/qinlinj/linear/algo/sort/ThreeWayQuickSorter.java
@@ -9,6 +9,39 @@ public class ThreeWayQuickSorter extends Sorter {
         System.out.println(Arrays.toString(data));
     }
 
+    public void sort(int[] data) {
+        if (data == null || data.length <= 1) return;
+        sort(data, 0, data.length - 1);
+    }
+
+    private void sort(int[] data, int lo, int hi) {
+        if (lo >= hi) return;
+        int mid = lo + (hi - lo) / 2;
+        swap(data, mid, hi);
+        int pivot = data[hi];
+
+        int less = lo;
+        int great = hi;
+
+        int i = lo;
+        while (i <= great) {
+            if (data[i] < pivot) {
+                swap(data, i, less);
+                less++;
+                i++;
+            } else if (data[i] > pivot) {
+                swap(data, i, great);
+                great--;
+            } else {
+                i++;
+            }
+        }
+
+        sort(data, lo, less - 1);
+        sort(data, great + 1, hi);
+    }
+
+
     public void sort_ql(int[] data) {
         if (data == null || data.length < 2) return;
 
