0052adba2c69b0b218177d6b3d242ab33f1cae5d
qinlinj <justinqinlin@gmail.com>
2025-02-16 05:17:03 -0800

    feat(PeakIndexInAMountainArray): implement peakIndexInMountainArray method to find peak index in a mountain array
    This method handles edge cases for null or empty arrays and uses binary search for efficient peak index retrieval.

--- src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_852_PeakIndexInAMountainArray.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_852_PeakIndexInAMountainArray.java
@@ -1,4 +1,20 @@
 package org.qinlinj.linear.algo.binarysearch.practice;
 
 public class _852_PeakIndexInAMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (arr[mid] > arr[mid - 1]) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
 }
