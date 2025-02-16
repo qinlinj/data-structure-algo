0abafd5b141166d4dac182dc8bd84053991c3ba4
qinlinj <justinqinlin@gmail.com>
2025-02-16 02:46:00 -0800

    feat(binary search): implement searchRange method to find first and last positions of a target in a sorted array
    This change introduces a new `Solution` class with methods to find the
    first and last indices of a given target in a sorted array. This
    enhances the functionality of the binary search algorithm by allowing
    users to retrieve both positions efficiently.

--- src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_34_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_34_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,4 +1,44 @@
 package org.qinlinj.linear.algo.binarysearch.practice;
 
 public class _34_FindFirstAndLastPositionOfElementInSortedArray {
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return new int[]{-1, -1};
+            }
+            return new int[]{searchFirstIndex(nums, target), searchLastIndex(nums, target)};
+        }
+
+        public int searchFirstIndex(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    if (mid != 0 && nums[mid] == nums[mid - 1]) {
+                        right = mid - 1;
+                    } else return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else right = mid - 1;
+            }
+            return -1;
+        }
+
+        public int searchLastIndex(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    if (mid != nums.length - 1 && nums[mid] == nums[mid + 1]) {
+                        left = mid + 1;
+                    } else return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else right = mid - 1;
+            }
+            return -1;
+        }
+    }
 }
