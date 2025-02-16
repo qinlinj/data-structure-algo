656ec3e4e490f56b679053e74963d7f4ff256d25
qinlinj <justinqinlin@gmail.com>
2025-02-16 04:02:30 -0800

    feat(SearchInRotatedSortedArray): implement search method to find target in rotated sorted array
    This method handles edge cases for null or empty arrays and uses binary search to efficiently locate the target.

--- src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_33_SearchInRotatedSortedArray.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_33_SearchInRotatedSortedArray.java
@@ -1,4 +1,36 @@
 package org.qinlinj.linear.algo.binarysearch.practice;
 
 public class _33_SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                // left is ordered
+                if (target >= nums[left] && target < nums[mid]) {
+                    // search from left
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // right is ordered
+                if (target <= nums[right] && target > nums[mid]) {
+                    // search from right
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
