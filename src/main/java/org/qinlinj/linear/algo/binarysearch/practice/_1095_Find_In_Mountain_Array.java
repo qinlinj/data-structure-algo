d5b513d60ce278ed9be2598168f1bcf04f1447bd
qinlinj <justinqinlin@gmail.com>
2025-02-16 05:46:52 -0800

    feat(binarysearch): implement findInMountainArray method to search for a target in a mountain array
    This change introduces a new method to efficiently find a target value
    in a mountain array by first locating the peak and then performing
    binary searches on both sides of the peak. This improves search
    efficiency in mountain-shaped data structures.

--- src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_1095_Find_In_Mountain_Array.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_1095_Find_In_Mountain_Array.java
@@ -1,4 +1,52 @@
 package org.qinlinj.linear.algo.binarysearch.practice;
 
 public class _1095_Find_In_Mountain_Array {
+    public int findInMountainArray(int target, int[] nums) {
+        int peakIndex = searchPeakIndex(nums);
+        int index = binarySearchFrontPart(nums, 0, peakIndex, target);
+        if (index != -1) {
+            return index;
+        }
+        return binarySearchLatterPart(nums, peakIndex, nums.length - 1, target);
+    }
+
+    private int searchPeakIndex(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    private int binarySearchFrontPart(int[] nums, int left, int peakIndex, int target) {
+        while (left < peakIndex) {
+            int mid = left + (peakIndex - left) / 2;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                peakIndex = mid;
+            }
+        }
+        if (nums[left] == target) return left;
+        return -1;
+    }
+    
+    private int binarySearchLatterPart(int[] nums, int peakIndex, int right, int target) {
+        while (peakIndex < right) {
+            int mid = peakIndex + (right - peakIndex) / 2;
+            if (target < nums[mid]) {
+                peakIndex = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[peakIndex] == target) return peakIndex;
+        return -1;
+    }
 }
