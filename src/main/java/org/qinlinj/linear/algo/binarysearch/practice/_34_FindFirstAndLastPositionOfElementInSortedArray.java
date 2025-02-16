da5ae1aea0bbe4973fb139dddd073e3f3c11fbd7
qinlinj <justinqinlin@gmail.com>
2025-02-16 02:46:10 -0800

    feat(binary search): add searchRange_dm method to find first and last positions of a target in a sorted array
    This method improves the functionality of the class by providing a way to
    retrieve both the first and last indices of a specified target value,
    enhancing the usability of the binary search algorithm.

--- src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_34_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_34_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -40,5 +40,48 @@ public class _34_FindFirstAndLastPositionOfElementInSortedArray {
             }
             return -1;
         }
+
+        // ---------------------------------------
+        public int[] searchRange_dm(int[] nums, int target) {
+            if (nums == null || nums.length == 0)
+                return new int[]{-1, -1};
+
+            int firstTargetIndex = searchFirstTargetIndex(nums, target);
+            if (firstTargetIndex == -1) {
+                return new int[]{-1, -1};
+            }
+            int lastTargetIndex = searchLastTargetIndex(nums, target);
+            return new int[]{firstTargetIndex, lastTargetIndex};
+        }
+
+        private int searchFirstTargetIndex(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            if (nums[left] == target) return left;
+            return -1;
+        }
+
+        private int searchLastTargetIndex(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left + 1) / 2;
+                if (target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid;
+                }
+            }
+            if (nums[left] == target) return left;
+            return -1;
+        }
     }
 }
