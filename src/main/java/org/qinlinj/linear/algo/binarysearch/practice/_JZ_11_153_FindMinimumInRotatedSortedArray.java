5b5390e4ca88f53cbd04389062c8996b3ffbd042
qinlinj <justinqinlin@gmail.com>
2025-02-16 04:54:04 -0800

    feat(binarysearch): add methods to find minimum in rotated sorted array for both duplicate and unique element cases
    This change introduces two methods: `findMin_dm` for handling arrays with duplicate elements and `findMin` for arrays with unique elements. This enhances the functionality of the class to accommodate different scenarios when searching for the minimum value in a rotated sorted array.

--- src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_JZ_11_153_FindMinimumInRotatedSortedArray.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_JZ_11_153_FindMinimumInRotatedSortedArray.java
@@ -1,4 +1,43 @@
 package org.qinlinj.linear.algo.binarysearch.practice;
 
 public class _JZ_11_153_FindMinimumInRotatedSortedArray {
+    // fix the situation of duplicate element array
+    public int findMin_dm(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+
+    // array of unique element
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
 }
