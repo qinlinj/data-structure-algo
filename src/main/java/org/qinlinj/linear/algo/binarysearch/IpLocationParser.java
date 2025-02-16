04472c995e7c51e7b4365b71b9778d093852b10a
qinlinj <justinqinlin@gmail.com>
2025-02-16 02:01:59 -0800

    feat(IpLocationParser): implement IP location parsing and binary search functionality
    Add functionality to read IP location data from a file and store it in a
    sorted list. Implement binary search to retrieve the location based on
    the given IP address. This enhances the application by allowing it to
    efficiently determine the geographical location of an IP address.

--- src/main/java/org/qinlinj/linear/algo/binarysearch/IpLocationParser.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/IpLocationParser.java
@@ -1,25 +1,78 @@
 package org.qinlinj.linear.algo.binarysearch;
 
-import org.qinlinj.linear.array.ArrayList;
-
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class IpLocationParser {
     private static ArrayList<IpLocation> sortedIpLocationList = new ArrayList<>();
 
     static {
         try {
-            BufferedReader bf = new BufferedReader(new FileReader("data\\ip_location.txt"));
-        } catch (FileNotFoundException e) {
+            BufferedReader br = new BufferedReader(new FileReader("data/ip_location.txt"));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] tmps = line.split(" ");
+                IpLocation ipLocation = new IpLocation();
+                ipLocation.startIp = ip2Score(tmps[0]);
+                ipLocation.endIp = ip2Score(tmps[1]);
+                ipLocation.location = tmps[2];
+                sortedIpLocationList.add(ipLocation);
+            }
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        // sort IpLocationList
+        Collections.sort(sortedIpLocationList, new Comparator<IpLocation>() {
+            @Override
+            public int compare(IpLocation o1, IpLocation o2) {
+                if (o1.startIp < o2.startIp) return -1;
+                else if (o1.startIp > o2.startIp) return 1;
+                else return 0;
+            }
+        });
     }
+
+    public static void main(String[] args) {
+        System.out.println(getIpLocation("202.101.48.198"));
+    }
+
+    private static long ip2Score(String ip) {
+        String[] tmps = ip.split("\\.");
+        long res = 256 * 256 * 256 * Long.parseLong(tmps[0]) +
+                256 * 256 * Long.parseLong(tmps[1]) +
+                256 * Long.parseLong(tmps[2]) +
+                Long.parseLong(tmps[3]);
+        return res;
+    }
+
+    public static String getIpLocation(String ip) {
+        long target = ip2Score(ip);
+        int left = 0;
+        int right = sortedIpLocationList.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target >= sortedIpLocationList.get(mid).startIp) {
+                if (mid == sortedIpLocationList.size() - 1 || target < sortedIpLocationList.get(mid + 1).startIp) {
+                    if (target <= sortedIpLocationList.get(mid).endIp) {
+                        return sortedIpLocationList.get(mid).location;
+                    } else break;
+                } else left = mid + 1;
+            } else if (target > sortedIpLocationList.get(mid).endIp) {
+                left = mid + 1;
+            } else right = mid - 1;
+        }
+        return null;
+    }
+
 
     private static class IpLocation {
         private long startIp;
         private long endIp;
-        private int location;
+        private String location;
     }
 }
