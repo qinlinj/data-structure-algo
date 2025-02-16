c9cce9fb4c85b0b378eaaab43ea7cbd08709a83c
qinlinj <justinqinlin@gmail.com>
2025-02-16 03:04:34 -0800

    feat(SearchInsertPosition): implement searchInsert method to find target index in sorted array

--- src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_35_SearchInsertPosition.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/practice/_35_SearchInsertPosition.java
@@ -1,4 +1,26 @@
 package org.qinlinj.linear.algo.binarysearch.practice;
 
 public class _35_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null) {
+            return -1;
+        }
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (target > nums[nums.length - 1]) return nums.length;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                if (mid != 0 && nums[mid - 1] >= target) {
+                    right = mid - 1;
+                } else return mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
 }
