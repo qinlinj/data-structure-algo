bfab3f6101d6ce5afb0320c4c589c76ddb1d9a9e
qinlinj <justinqinlin@gmail.com>
2025-02-16 01:15:31 -0800

    feat(ip_location): add ip_location.txt file to store IP ranges and locations feat(IpLocationParser): implement static block to read IP location data from file and initialize sorted list

--- src/main/java/org/qinlinj/linear/algo/binarysearch/IpLocationParser.java
+++ src/main/java/org/qinlinj/linear/algo/binarysearch/IpLocationParser.java
@@ -1,4 +1,25 @@
 package org.qinlinj.linear.algo.binarysearch;
 
+import org.qinlinj.linear.array.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class IpLocationParser {
+    private static ArrayList<IpLocation> sortedIpLocationList = new ArrayList<>();
+
+    static {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader("data\\ip_location.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class IpLocation {
+        private long startIp;
+        private long endIp;
+        private int location;
+    }
 }
