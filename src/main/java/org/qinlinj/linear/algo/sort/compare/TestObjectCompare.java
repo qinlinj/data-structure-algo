14e2ce598900f57ceb8d777b11e71ea9ff94a5b5
qinlinj <justinqinlin@gmail.com>
2025-02-16 17:51:09 -0800

    feat(TestObjectCompare.java): add main method to compare Person objects by age using a Comparator

--- src/main/java/org/qinlinj/linear/algo/sort/compare/TestObjectCompare.java
+++ src/main/java/org/qinlinj/linear/algo/sort/compare/TestObjectCompare.java
@@ -1,4 +1,13 @@
 package org.qinlinj.linear.algo.sort.compare;
 
+import java.util.Comparator;
+
 public class TestObjectCompare {
+    public static void main(String[] args) {
+        Person p1 = new Person("jus", 40);
+        Person p2 = new Person("chel", 30);
+        Comparator<Person> comparator = ((o1, o2) -> o1.getAge() - o2.getAge());
+        int res = comparator.compare(p1, p2);
+        System.out.println(res);
+    }
 }
