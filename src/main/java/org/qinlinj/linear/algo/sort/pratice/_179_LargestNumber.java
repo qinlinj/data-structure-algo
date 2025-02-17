f5fd59cd17ec723efe75807913379a6d4dc4a7de
qinlinj <justinqinlin@gmail.com>
2025-02-16 19:59:11 -0800

    feat(largest-number): implement solution to find the largest number from an array of integers
    This change introduces a new method to sort an array of integers and
    form the largest possible number by concatenating them. The solution
    utilizes a custom comparator for sorting based on string concatenation.

--- src/main/java/org/qinlinj/linear/algo/sort/pratice/_179_LargestNumber.java
+++ src/main/java/org/qinlinj/linear/algo/sort/pratice/_179_LargestNumber.java
@@ -1,4 +1,34 @@
 package org.qinlinj.linear.algo.sort.pratice;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class _179_LargestNumber {
+    class Solution {
+        public String largestNumber(int[] nums) {
+            String[] tmp = new String[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                tmp[i] = String.valueOf(nums[i]);
+            }
+
+            Arrays.sort(tmp, new Comparator<String>() {
+                @Override
+                // compare: if yx - xy < 0, swap x and y
+                public int compare(String x, String y) {
+                    String xy = x + "" + y;
+                    String yx = y + "" + x;
+                    return yx.compareTo(xy);
+                }
+            });
+
+            if (tmp[0].equals("0")) {
+                return "0";
+            }
+            StringBuilder sb = new StringBuilder();
+            for (String string : tmp) {
+                sb.append(string);
+            }
+            return sb.toString();
+        }
+    }
 }
