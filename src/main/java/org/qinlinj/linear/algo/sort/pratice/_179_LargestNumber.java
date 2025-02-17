16871961ed2b8bbaac595ee92d437679f6aea67c
qinlinj <justinqinlin@gmail.com>
2025-02-16 20:03:10 -0800

    feat(largestNumber): implement a quick sort algorithm for largest number problem
    This change introduces a new sorting method (Solution2) using quick sort
    to improve performance and efficiency in generating the largest number
    from an array of integers. The existing method remains intact for
    comparison and validation purposes.

--- src/main/java/org/qinlinj/linear/algo/sort/pratice/_179_LargestNumber.java
+++ src/main/java/org/qinlinj/linear/algo/sort/pratice/_179_LargestNumber.java
@@ -4,31 +4,80 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class _179_LargestNumber {
-    class Solution {
-        public String largestNumber(int[] nums) {
-            String[] tmp = new String[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                tmp[i] = String.valueOf(nums[i]);
-            }
-
-            Arrays.sort(tmp, new Comparator<String>() {
-                @Override
-                // compare: if yx - xy < 0, swap x and y
-                public int compare(String x, String y) {
-                    String xy = x + "" + y;
-                    String yx = y + "" + x;
-                    return yx.compareTo(xy);
-                }
-            });
-
-            if (tmp[0].equals("0")) {
-                return "0";
-            }
-            StringBuilder sb = new StringBuilder();
-            for (String string : tmp) {
-                sb.append(string);
-            }
-            return sb.toString();
+    public String largestNumber(int[] nums) {
+        String[] tmp = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            tmp[i] = String.valueOf(nums[i]);
         }
+
+        Arrays.sort(tmp, new Comparator<String>() {
+            @Override
+            // compare: if yx - xy < 0, swap x and y
+            public int compare(String x, String y) {
+                String xy = x + "" + y;
+                String yx = y + "" + x;
+                return yx.compareTo(xy);
+            }
+        });
+
+        if (tmp[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String string : tmp) {
+            sb.append(string);
+        }
+        return sb.toString();
+    }
+
+
+    //-----------------------------------------
+    // using quick sort
+    public String Solution2(int[] nums) {
+
+        sort(nums, 0, nums.length - 1);
+
+        if (nums[0] == 0) {
+            return "0";
+        } // "00000"
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : nums) {
+            sb.append(num);
+        }
+        return sb.toString();
+    }
+
+    private void sort(int[] data, int lo, int hi) {
+        if (lo >= hi) return;
+        int pivot = data[hi];
+
+        int less = lo;
+        int great = hi;
+
+        int i = lo;
+        while (i <= great) {
+            String xy = data[i] + "" + pivot;
+            String yx = pivot + "" + data[i];
+            if (xy.compareTo(yx) > 0) {
+                swap(data, i, less);
+                less++;
+                i++;
+            } else if (xy.compareTo(yx) < 0) {
+                swap(data, i, great);
+                great--;
+            } else {
+                i++;
+            }
+        }
+
+        sort(data, lo, less - 1);
+        sort(data, great + 1, hi);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
