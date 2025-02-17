4628113dcd278ab274b755fff9abf7e1a3ac4b50
qinlinj <justinqinlin@gmail.com>
2025-02-16 17:42:26 -0800

    feat(JavaSorter): implement sorting functionality for integers and Person objects to demonstrate sorting capabilities feat(Person): implement Comparable interface to allow natural ordering by age and add toString method for better output representation

--- src/main/java/org/qinlinj/linear/algo/sort/JavaSorter.java
+++ src/main/java/org/qinlinj/linear/algo/sort/JavaSorter.java
@@ -1,4 +1,50 @@
 package org.qinlinj.linear.algo.sort;
 
+import org.qinlinj.linear.algo.sort.compare.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class JavaSorter {
+    public static void main(String[] args) {
+        int[] data = new int[]{34, 33, 12, 78, 21, 1, 98, 100};
+        Arrays.sort(data); // general sort
+        System.out.println(Arrays.toString(data));
+
+        Person p1 = new Person("jus", 40);
+        Person p2 = new Person("chel", 30);
+        Person p3 = new Person("guo", 32);
+        Person p4 = new Person("song", 33);
+        Person[] people = new Person[]{p1, p2, p3, p4};
+        //Arrays.sort(people);
+//        Comparator<Person> comparator = ((o1, o2) -> o1.getAge() - o2.getAge());
+        Comparator<Person> comparator = (new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        });
+        //Arrays.sort(people, comparator);
+
+
+        // insertion sort in small size of dataset
+        // merge sort in big size of dataset
+        //System.out.println(Arrays.toString(people));
+
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+        list.add(p4);
+        // Arrays.sort
+
+        // using comparator
+        Collections.sort(list, comparator);
+
+        // using compareTo
+        Collections.sort(list);
+        System.out.println(list);
+    }
 }
--- src/main/java/org/qinlinj/linear/algo/sort/compare/Person.java
+++ src/main/java/org/qinlinj/linear/algo/sort/compare/Person.java
@@ -1,4 +1,34 @@
 package org.qinlinj.linear.algo.sort.compare;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return other.age - this.age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+
 }
