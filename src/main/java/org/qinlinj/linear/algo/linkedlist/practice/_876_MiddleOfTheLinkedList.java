d7c4bf22db3b6d6af52eac3e0b6577e33cf8c3cc
qinlinj <justinqinlin@gmail.com>
2025-02-18 23:57:12 -0800

    feat(linkedlist): implement a solution to find the middle node of a linked list using the fast and slow pointer technique

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_876_MiddleOfTheLinkedList.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_876_MiddleOfTheLinkedList.java
@@ -1,4 +1,22 @@
 package org.qinlinj.linear.algo.linkedlist.practice;
 
+import org.qinlinj.linear.algo.linkedlist.ListNode;
+
 public class _876_MiddleOfTheLinkedList {
+    class Solution1 {
+        public ListNode middleNode(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode dummyHead = new ListNode(-1);
+            dummyHead.next = head;
+            ListNode fast = dummyHead;
+            ListNode slow = head;
+            while (fast != null && fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
 }
