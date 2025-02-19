6611eaa17bfe503238f42a3db43759badb2156e5
qinlinj <justinqinlin@gmail.com>
2025-02-19 14:40:38 -0800

    feat(InsertionSortList): implement insertion sort for linked list to enhance sorting functionality

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_147_Insertion_Sort_List.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_147_Insertion_Sort_List.java
@@ -4,6 +4,32 @@ import org.qinlinj.linear.algo.linkedlist.ListNode;
 
 public class _147_Insertion_Sort_List {
     class Solution1 {
+        public ListNode insertionSortList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode dummyNode = new ListNode(-1);
+            dummyNode.next = head;
+
+            ListNode curr = head.next;
+            ListNode lastSorted = head;
+            while (curr != null) {
+                if (lastSorted.val <= curr.val) {
+                    lastSorted = curr;
+                } else {
+                    ListNode prev = dummyNode;
+                    while (prev.next.val <= curr.val) {
+                        prev = prev.next;
+                    }
+                    lastSorted.next = curr.next;
+                    curr.next = prev.next;
+                    prev.next = curr;
+                }
+                curr = lastSorted.next;
+            }
+
+            return dummyNode.next;
+        }
 
         public void swap(ListNode listPrev1, ListNode listPrev2) {
             if (listPrev1 == null || listPrev2 == null ||
