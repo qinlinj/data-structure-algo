4a9546582e8de2189afe65bddad2dbb7d4900349
qinlinj <justinqinlin@gmail.com>
2025-02-19 02:52:29 -0800

    refactor(linkedlist): restructure the RemoveNthNodeFromEndOfList class for better readability and maintainability
    - Move the main method into a static inner class named Solution1.
    - Simplify the removeNthFromEnd method by using a dummy node and
      improving variable naming for clarity.
    - Enhance the logic flow to make it easier to understand the
      removal process of the nth node from the end of the list.

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_19_RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_19_RemoveNthNodeFromEndOfList.java
@@ -3,37 +3,38 @@ package org.qinlinj.linear.algo.linkedlist.practice;
 import org.qinlinj.linear.algo.linkedlist.ListNode;
 
 public class _19_RemoveNthNodeFromEndOfList {
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        ListNode subNode = new ListNode(2);
-        head.next = subNode;
-        _19_RemoveNthNodeFromEndOfList solution = new _19_RemoveNthNodeFromEndOfList();
-        System.out.println(solution.removeNthFromEnd(head, 2).toString());
+    static class Solution1 {
+        public static void main(String[] args) {
+            ListNode head = new ListNode(1);
+            ListNode subNode = new ListNode(2);
+            head.next = subNode;
+            Solution1 solution = new Solution1();
+            System.out.println(solution.removeNthFromEnd(head, 2).toString());
+        }
+
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            if (head == null) return null;
+            ListNode dummyNode = new ListNode(-1);
+            dummyNode.next = head;
+
+            ListNode slow = dummyNode;
+            ListNode fast = dummyNode;
+
+            while (n-- > 0) {
+                fast = fast.next;
+            }
+
+            while (fast.next != null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            ListNode delNode = slow.next;
+            slow.next = delNode.next;
+            delNode.next = null;
+
+            return dummyNode.next;
+        }
     }
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null || head.next == null) {
-            return null;
-        }
-        ListNode dummyHead = new ListNode(-1);
-        dummyHead.next = head;
-        ListNode fast = head;
-        ListNode slow = head;
-        ListNode prev = dummyHead;
-
-        for (int i = 0; i < n - 1; i++) {
-            fast = fast.next;
-        }
-
-        while (fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
-            prev = prev.next;
-        }
-
-        prev.next = slow.next;
-        slow = null;
-
-        return dummyHead.next;
-    }
 }
