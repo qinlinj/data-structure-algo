6fa2ef0de63d5cc3a795ca7c15c12ff237c65454
qinlinj <justinqinlin@gmail.com>
2025-02-18 23:20:44 -0800

    feat(linkedlist): make ListNode fields public for easier access in other classes feat(remove-linked-list-elements): implement removeElements method to remove nodes with a specific value from the linked list

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_203_RemoveLinkedListElements.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_203_RemoveLinkedListElements.java
@@ -1,5 +1,30 @@
 package org.qinlinj.linear.algo.linkedlist.practice;
 
-public class _203_RemoveLinkedListElements {
+import org.qinlinj.linear.algo.linkedlist.ListNode;
 
+public class _203_RemoveLinkedListElements {
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            if (head == null) {
+                return head;
+            }
+            ListNode dummyhead = new ListNode(-1);
+            dummyhead.next = head;
+            ListNode curr = dummyhead.next;
+            ListNode prev = dummyhead;
+            while (curr != null) {
+                if (curr.val == val) {
+                    ListNode newCurr = curr.next;
+                    curr = null;
+                    prev.next = newCurr;
+                    curr = prev.next;
+                } else {
+                    curr = curr.next;
+                    prev = prev.next;
+                }
+
+            }
+            return dummyhead.next;
+        }
+    }
 }
