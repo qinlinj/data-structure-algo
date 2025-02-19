f0de101b1b4c9672234e8fc2f0e0c8f7123816b5
qinlinj <justinqinlin@gmail.com>
2025-02-19 13:58:42 -0800

    feat(MergeKSortedLists.java): add a new solution class for merging k sorted lists using merge sort algorithm refactor(MergeKSortedLists.java): rename existing solution class to Solution1 for clarity and organization

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_23_MergeKSortedLists.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_23_MergeKSortedLists.java
@@ -3,7 +3,7 @@ package org.qinlinj.linear.algo.linkedlist.practice;
 import org.qinlinj.linear.algo.linkedlist.ListNode;
 
 public class _23_MergeKSortedLists {
-    class Solution {
+    class Solution1 {
         public ListNode mergeKLists(ListNode[] lists) {
             if (lists == null || lists.length == 0) {
                 return null;
@@ -36,6 +36,48 @@ public class _23_MergeKSortedLists {
             } else {
                 list2.next = merge(list1, list2.next);
                 return list2;
+            }
+        }
+    }
+
+    class Solution2 {
+        class Solution {
+            public ListNode mergeKLists(ListNode[] lists) {
+                if (lists == null || lists.length == 0) {
+                    return null;
+                }
+                if (lists.length == 1) {
+                    return lists[0];
+                }
+                // merge sort
+                return segment(lists, 0, lists.length - 1);
+            }
+
+            public ListNode segment(ListNode[] lists, int left, int right) {
+                if (left == right) {
+                    return lists[left];
+                }
+                int mid = left + (right - left) / 2;
+                ListNode leftNode = segment(lists, left, mid);
+                ListNode rightNode = segment(lists, mid + 1, right);
+                return merge(leftNode, rightNode);
+            }
+
+            public ListNode merge(ListNode list1, ListNode list2) {
+                if (list1 == null) {
+                    return list2;
+                }
+                if (list2 == null) {
+                    return list1;
+                }
+
+                if (list1.val <= list2.val) {
+                    list1.next = merge(list1.next, list2);
+                    return list1;
+                } else {
+                    list2.next = merge(list1, list2.next);
+                    return list2;
+                }
             }
         }
     }
