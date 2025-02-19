307ebed345c1042b7030ebc010ae247f6e21a857
qinlinj <justinqinlin@gmail.com>
2025-02-18 23:57:46 -0800

    feat(linkedlist): add middleNode1 method to find the middle node of a linked list using a single traversal

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_876_MiddleOfTheLinkedList.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_876_MiddleOfTheLinkedList.java
@@ -3,6 +3,15 @@ package org.qinlinj.linear.algo.linkedlist.practice;
 import org.qinlinj.linear.algo.linkedlist.ListNode;
 
 public class _876_MiddleOfTheLinkedList {
+    public ListNode middleNode1(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode curr = head;
+        while (curr != null) { // O(n)
+            curr = curr.next;
+        }
+        return head;
+    }
+
     class Solution1 {
         public ListNode middleNode(ListNode head) {
             if (head == null || head.next == null) return head;
