cb3077c3fb293a89cd52d96e483f52189510f46c
qinlinj <justinqinlin@gmail.com>
2025-02-19 02:37:34 -0800

    feat(linkedlist): implement removeNthFromEnd method to remove the nth node from the end of a linked list test(linkedlist): add main method to test removeNthFromEnd functionality with a simple linked list

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_19_RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_19_RemoveNthNodeFromEndOfList.java
@@ -1,4 +1,39 @@
 package org.qinlinj.linear.algo.linkedlist.practice;
 
+import org.qinlinj.linear.algo.linkedlist.ListNode;
+
 public class _19_RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode subNode = new ListNode(2);
+        head.next = subNode;
+        _19_RemoveNthNodeFromEndOfList solution = new _19_RemoveNthNodeFromEndOfList();
+        System.out.println(solution.removeNthFromEnd(head, 1).toString());
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = dummyHead;
+
+        for (int i = 0; i < n - 1; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+            prev = prev.next;
+        }
+
+        prev.next = slow.next;
+        slow = null;
+
+        return head;
+    }
 }
