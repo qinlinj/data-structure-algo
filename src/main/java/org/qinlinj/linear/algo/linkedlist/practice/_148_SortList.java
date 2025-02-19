0af27a06414d8a72d8e113b617c6cd7c2e35677d
qinlinj <justinqinlin@gmail.com>
2025-02-19 15:12:27 -0800

    feat(SortList.java): implement merge sort for linked list to improve sorting efficiency

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_148_SortList.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_148_SortList.java
@@ -1,4 +1,54 @@
 package org.qinlinj.linear.algo.linkedlist.practice;
 
+import org.qinlinj.linear.algo.linkedlist.ListNode;
+
 public class _148_SortList {
+    class Solution1 {
+        public ListNode sortList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            ListNode rightHead = slow.next;
+            slow.next = null;
+            ListNode left = sortList(head);
+            ListNode right = sortList(rightHead);
+            return merge(left, right);
+        }
+
+        public ListNode merge(ListNode head1, ListNode head2) {
+            if (head1 == null) {
+                return head2;
+            }
+            if (head2 == null) {
+                return head1;
+            }
+
+            ListNode dummyNode = new ListNode(-1);
+            ListNode curr = dummyNode;
+
+            while (head1 != null && head2 != null) {
+                if (head1.val <= head2.val) {
+                    curr.next = head1;
+                    head1 = head1.next;
+                } else {
+                    curr.next = head2;
+                    head2 = head2.next;
+                }
+                curr = curr.next;
+            }
+
+            if (head1 == null)
+                curr.next = head2;
+            if (head2 == null)
+                curr.next = head1;
+
+            return dummyNode.next;
+        }
+    }
 }
