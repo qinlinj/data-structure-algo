ce37cb36b038d1ef0bf38d0aaa8ca3ae4a05d530
qinlinj <justinqinlin@gmail.com>
2025-02-18 23:48:47 -0800

    feat(ReverseLinkedList.java): implement iterative solution for reversing a linked list to enhance functionality and provide an alternative approach

--- src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_206_ReverseLinkedList.java
+++ src/main/java/org/qinlinj/linear/algo/linkedlist/practice/_206_ReverseLinkedList.java
@@ -1,4 +1,19 @@
 package org.qinlinj.linear.algo.linkedlist.practice;
 
+import org.qinlinj.linear.algo.linkedlist.ListNode;
+
 public class _206_ReverseLinkedList {
+    class Solution1 {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+    }
 }
