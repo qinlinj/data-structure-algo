92cf79f2b539bcc1f24464e4f8209912d0bd9c40
qinlinj <justinqinlin@gmail.com>
2025-02-18 22:37:56 -0800

    feat(reverse pairs): implement a new solution using traversal method to count reverse pairs in an array feat(reverse pairs): add a private method to sort and count reverse pairs during the traversal process

--- src/main/java/org/qinlinj/linear/algo/sort/pratice/_JZ_51_493_ReversePairs.java
+++ src/main/java/org/qinlinj/linear/algo/sort/pratice/_JZ_51_493_ReversePairs.java
@@ -1,4 +1,63 @@
 package org.qinlinj.linear.algo.sort.pratice;
 
 public class _JZ_51_493_ReversePairs {
+    class Solution1 {
+        // traversal
+        public int reversePairs(int[] nums) {
+            if (nums == null || nums.length < 2) {
+                return 0;
+            }
+
+            int[] tmp = new int[nums.length];
+            System.arraycopy(nums, 0, tmp, 0, nums.length);
+            int count = 0;
+
+            for (int size = 1; size < nums.length; size *= 2) {
+                for (int left = 0; left < nums.length - size; left += size * 2) {
+                    int mid = left + size;
+                    int right = Math.min(left + size * 2 - 1, nums.length - 1);
+                    count += sortAndCount(nums, tmp, left, mid, right);
+                }
+                System.arraycopy(tmp, 0, nums, 0, nums.length);
+            }
+
+            return count;
+        }
+
+        private int sortAndCount(int[] nums, int[] tmp, int left, int mid, int right) {
+            int count = 0;
+            int pos = left;
+            int start1 = left;
+            int start2 = mid;
+
+            int j = mid;
+            for (int i = left; i < mid; i++) {
+                while (j <= right && (long) nums[i] > (long) 2 * nums[j]) {
+                    j++;
+                }
+                count += j - mid;
+            }
+
+            while (start1 < mid && start2 <= right) {
+                if (nums[start1] <= nums[start2]) {
+                    tmp[pos++] = nums[start1++];
+                } else {
+                    tmp[pos++] = nums[start2++];
+                }
+            }
+
+            while (start1 < mid) {
+                tmp[pos++] = nums[start1++];
+            }
+            while (start2 <= right) {
+                tmp[pos++] = nums[start2++];
+            }
+
+            return count;
+        }
+    }
+
+    //----------------------------------------------------
+    // recursion
+
 }
