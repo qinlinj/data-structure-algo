3b65265f46c8f8e6c38d3dfad74c7fefac1c9a00
qinlinj <justinqinlin@gmail.com>
2025-02-20 03:45:44 -0800

    feat(InvertBinaryTree): implement iterative solution for inverting a binary tree using a stack
    This change introduces a new iterative approach to invert a binary tree, enhancing the existing functionality and providing an alternative to recursive methods.

--- src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_226_InvertBinaryTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_226_InvertBinaryTree.java
@@ -1,5 +1,37 @@
 package org.qinlinj.nonlinear.tree.binarytree.practice;
 
-public class _226_InvertBinaryTree {
+import java.util.Stack;
 
+public class _226_InvertBinaryTree {
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+
+            while (!stack.isEmpty()) {
+                TreeNode curr = stack.pop();
+                invert(curr);
+                if (curr.right != null) {
+                    stack.push(curr.right);
+                }
+                if (curr.left != null) {
+                    stack.push(curr.left);
+                }
+            }
+
+            return root;
+        }
+
+        public void invert(TreeNode node) {
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
+    }
+
+    
 }
