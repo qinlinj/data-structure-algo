c0625d0d11f96f342e988fe254a5b80323d0e201
qinlinj <justinqinlin@gmail.com>
2025-02-20 02:43:38 -0800

    feat(MaximumDepthOfBinaryTree.java): add BFS solution for calculating maximum depth of binary tree
    This change introduces a new class `Solution3` that implements a breadth-first search (BFS) approach to calculate the maximum depth of a binary tree. This provides an alternative method to the existing recursive solutions, enhancing the versatility of the implementation.

--- src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_104_MaximumDepthOfBinaryTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_104_MaximumDepthOfBinaryTree.java
@@ -2,6 +2,9 @@ package org.qinlinj.nonlinear.tree.binarytree.practice;
 
 import org.qinlinj.nonlinear.tree.binarytree.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class _104_MaximumDepthOfBinaryTree {
     // recursion version 1
     class Solution1 {
@@ -29,6 +32,26 @@ public class _104_MaximumDepthOfBinaryTree {
             int leftMaxDepth = maxDepth1(root.left);
             int rightMaxDepth = maxDepth1(root.right);
             int maxDepth = Math.max(leftMaxDepth, rightMaxDepth) + 1;
+            return maxDepth;
+        }
+    }
+
+    class Solution3 {
+        // BFS
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            int maxDepth = 0;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = queue.poll();
+                    if (curr.left != null) queue.offer(curr.left);
+                    if (curr.right != null) queue.offer(curr.right);
+                }
+                maxDepth++;
+            }
             return maxDepth;
         }
     }
