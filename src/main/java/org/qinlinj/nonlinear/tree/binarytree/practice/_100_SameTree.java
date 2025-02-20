9750aa7fbf6067acff6f9e699022b2108dc5f273
qinlinj <justinqinlin@gmail.com>
2025-02-20 13:38:22 -0800

    feat(SameTree.java): add a more concise recursive solution for tree comparison to enhance readability and maintainability

--- src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_100_SameTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_100_SameTree.java
@@ -29,4 +29,18 @@ public class _100_SameTree {
             return true;
         }
     }
+
+    // ---------------------------------------
+    // more concise version using recursion
+    class Solution2 {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) return true;
+            if (p == null || q == null) return false;
+            if (p.val != q.val) return false;
+
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
+    //-------------------------------------------
 }
