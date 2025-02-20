9ef8706994617a3caf46d5e4202d19451fb6c6db
qinlinj <justinqinlin@gmail.com>
2025-02-20 13:37:03 -0800

    feat(SameTree): add Solution1 class to implement isSameTree method for tree comparison
    This change introduces a new class, Solution1, which contains the
    isSameTree method. This method checks if two binary trees are
    identical by comparing their structure and node values.

--- src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_100_SameTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/binarytree/practice/_100_SameTree.java
@@ -1,5 +1,32 @@
 package org.qinlinj.nonlinear.tree.binarytree.practice;
 
 public class _100_SameTree {
-
+    class Solution1 {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null ^ q == null) {
+                return false;
+            }
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p.val != q.val) {
+                return false;
+            }
+            if ((p.left != null ^ q.left != null) || (p.right != null ^ q.right != null)) {
+                return false;
+            } else {
+                if (p.left != null && q.left != null) {
+                    if (!isSameTree(p.left, q.left)) {
+                        return false;
+                    }
+                }
+                if (p.right != null && q.right != null) {
+                    if (!isSameTree(p.right, q.right)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
 }
