e24acb0da5d751b46a4460b2f346d81f8164a063
qinlinj <justinqinlin@gmail.com>
2025-02-21 20:02:34 -0800

    chore(.gitignore): update .gitignore to exclude specific doc directory instead of all content files feat(ReverseLinkedList.java): implement reverseList method to reverse a linked list for improved functionality

--- src/main/java/org/qinlinj/leetcode/editor/en/ReverseLinkedList.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/ReverseLinkedList.java
@@ -20,7 +20,23 @@ public class ReverseLinkedList{
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        return head;
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = head.next;
+
+        while (curr != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
