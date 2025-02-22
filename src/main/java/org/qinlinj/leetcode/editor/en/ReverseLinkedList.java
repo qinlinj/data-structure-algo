fdd9ab3e1234dbada90b4483f71c2169d1ba7f7a
qinlinj <justinqinlin@gmail.com>
2025-02-21 20:08:07 -0800

    feat(editor.xml): add LeetcodeEditor configurations for multiple problems to enhance coding experience refactor(ReverseLinkedList.java): improve code formatting and structure for better readability chore(workspace.xml): update workspace settings for improved IDE performance delete(partition-list.lcv): remove outdated solution documentation for partition list problem to maintain clarity

--- src/main/java/org/qinlinj/leetcode/editor/en/ReverseLinkedList.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/ReverseLinkedList.java
@@ -3,42 +3,55 @@ package org.qinlinj.leetcode.editor.en;
 import org.qinlinj.linear.algo.linkedlist.ListNode;
 
 // [206] Reverse Linked List
-public class ReverseLinkedList{
+public class ReverseLinkedList {
     public static void main(String[] args) {
         Solution solution = new ReverseLinkedList().new Solution();
     }
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
 
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = head.next;
-
-        while (curr != null) {
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-            if (next != null) {
-                next = next.next;
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
             }
+            ListNode p = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return p;
         }
-        return prev;
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
+
+    class Solution2 {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode prev = null;
+            ListNode curr = head;
+            ListNode next = head.next;
+
+            while (curr != null) {
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+            return prev;
+        }
+    }
 
 }
