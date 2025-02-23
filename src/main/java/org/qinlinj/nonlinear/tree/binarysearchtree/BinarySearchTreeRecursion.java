e4c48263ec820aec44fba8e286676765f717abc0
qinlinj <justinqinlin@gmail.com>
2025-02-22 23:47:56 -0800

    feat(BinarySearchTreeRecursion): implement add method to insert elements into the binary search tree
    This change allows users to add elements to the binary search tree,
    enhancing its functionality and enabling dynamic data management.

--- src/main/java/org/qinlinj/nonlinear/tree/binarysearchtree/BinarySearchTreeRecursion.java
+++ src/main/java/org/qinlinj/nonlinear/tree/binarysearchtree/BinarySearchTreeRecursion.java
@@ -17,6 +17,28 @@ public class BinarySearchTreeRecursion<E extends Comparable<E>> {
         return size == 0;
     }
 
+    /************************* Insert *******************************/
+    // O(logn)
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    private TreeNode add(TreeNode node, E e) {
+
+        if (node == null) {
+            size++;
+            return new TreeNode(e);
+        }
+
+        if (e.compareTo(node.data) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.data) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+
     private class TreeNode {
         E data;
         TreeNode left;
