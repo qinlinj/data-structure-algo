790639e3e9781e9750f83035140a31f55176ae03
qinlinj <justinqinlin@gmail.com>
2025-02-23 00:28:13 -0800

    feat(statistics.xml, workspace.xml, TwoSumIiInputArrayIsSorted.java): update statistics and workspace configurations to reflect changes in problem-solving status and add debugging support for TwoSumIiInputArrayIsSorted class fix(TwoSumIiInputArrayIsSorted.java): improve twoSum method to handle edge cases and ensure correct results

--- src/main/java/org/qinlinj/leetcode/editor/en/TwoSumIiInputArrayIsSorted.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/TwoSumIiInputArrayIsSorted.java
@@ -9,8 +9,30 @@ public class TwoSumIiInputArrayIsSorted {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] twoSum(int[] numbers, int target) {
-            int[] res = new int[]{};
-
+            if (numbers == null || numbers.length < 2) {
+                return null;
+            }
+            int[] res = new int[2];
+            int fast;
+            int slow;
+            int count = 0;
+            while (count < numbers.length) {
+                slow = count;
+                fast = slow + 1;
+                for (int i = count; i < numbers.length - 1; i++) {
+                    if (target - numbers[fast] == numbers[slow]) {
+                        res[0] = slow + 1;
+                        res[1] = fast + 1;
+                        return res;
+                    } else if (target - numbers[fast] < numbers[slow]) {
+                        break;
+                    } else {
+                        fast++;
+                    }
+                }
+                count++;
+            }
+            return null;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
