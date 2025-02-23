f0d361d88c248f31035bad1cd464e47a61d84763
qinlinj <justinqinlin@gmail.com>
2025-02-22 23:46:55 -0800

    feat(BinarySearchTreeRecursion): add TreeNode class and root, size fields to enhance tree structure representation

--- src/main/java/org/qinlinj/nonlinear/tree/binarysearchtree/BinarySearchTreeRecursion.java
+++ src/main/java/org/qinlinj/nonlinear/tree/binarysearchtree/BinarySearchTreeRecursion.java
@@ -1,5 +1,16 @@
 package org.qinlinj.nonlinear.tree.binarysearchtree;
 
 public class BinarySearchTreeRecursion<E extends Comparable<E>> {
-    
+    private TreeNode root;
+    private int size;
+
+    private class TreeNode {
+        E data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(E data) {
+            this.data = data;
+        }
+    }
 }
