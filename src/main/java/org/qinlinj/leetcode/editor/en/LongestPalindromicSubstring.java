4bbb3a790c77b3886e78c54603db413d58aba148
qinlinj <justinqinlin@gmail.com>
2025-02-23 03:17:08 -0800

    feat(editor): add Longest Palindromic Substring and Squares of a Sorted Array solutions to the editor configuration fix(statistics): update medium and solved total counts to reflect new problems added chore(editor): update editor.xml and workspace.xml to include new entries for the added problems

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+package org.qinlinj.leetcode.editor.en;
+
+// [5] Longest Palindromic Substring
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new LongestPalindromicSubstring().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestPalindrome(String s) {
+            char[] charArray = s.toCharArray();
+            String res = "";
+            for (int i = 0; i < s.length(); i++) {
+                String resCurr1 = getPalindrome(i, i, s);
+                String resCurr2 = getPalindrome(i, i + 1, s);
+                res = res.length() > resCurr1.length() ? res : resCurr1;
+                res = res.length() > resCurr2.length() ? res : resCurr2;
+            }
+            return res;
+        }
+
+        private String getPalindrome(int left, int right, String s) {
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            return s.substring(left + 1, right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/org/qinlinj/leetcode/editor/en/SquaresOfASortedArray.java
@@ -0,0 +1,15 @@
+package org.qinlinj.leetcode.editor.en;
+// [977] Squares of a Sorted Array
+public class SquaresOfASortedArray{
+    public static void main(String[] args) {
+        Solution solution = new SquaresOfASortedArray().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
