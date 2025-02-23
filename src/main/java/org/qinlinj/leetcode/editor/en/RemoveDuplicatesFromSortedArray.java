b17957209519b32c83f22033ec008b8f3401af11
qinlinj <justinqinlin@gmail.com>
2025-02-22 23:56:10 -0800

    style(RemoveDuplicatesFromSortedArray.java): format code for better readability and consistency fix(RemoveDuplicatesFromSortedArray.java): correct the order of assignment in removeDuplicates method to ensure proper functionality

--- src/main/java/org/qinlinj/leetcode/editor/en/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/RemoveDuplicatesFromSortedArray.java
@@ -1,28 +1,26 @@
 package org.qinlinj.leetcode.editor.en;
-
 // [26] Remove Duplicates from Sorted Array
-public class RemoveDuplicatesFromSortedArray {
+public class RemoveDuplicatesFromSortedArray{
     public static void main(String[] args) {
         Solution solution = new RemoveDuplicatesFromSortedArray().new Solution();
     }
-
-    //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
-        public int removeDuplicates(int[] nums) {
-            if (nums.length == 0) {
-                return 0;
-            }
-            int slow = 0, fast = 0;
-            while (fast < nums.length) {
-                if (nums[fast] != nums[slow]) {
-                    nums[slow] = nums[fast];
-                    slow++;
-                }
-                fast++;
-            }
-            return slow + 1;
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
         }
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
     }
+}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
