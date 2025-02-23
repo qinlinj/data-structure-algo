611e0160f2ad980caf3fdcac5f1949b8aac621ef
qinlinj <justinqinlin@gmail.com>
2025-02-22 23:53:34 -0800

    feat(tests): add main method to BinarySearchTreeTest for testing BST operations chore(workspace): update workspace.xml to reflect changes in BinarySearchTreeTest file path and adjust work item duration

--- src/main/java/org/qinlinj/nonlinear/tree/binarysearchtree/BinarySearchTreeTest.java
+++ src/main/java/org/qinlinj/nonlinear/tree/binarysearchtree/BinarySearchTreeTest.java
@@ -1,4 +1,34 @@
 package org.qinlinj.nonlinear.tree.binarysearchtree;
 
 public class BinarySearchTreeTest {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.add(33);
+        bst.add(22);
+        bst.add(66);
+        bst.add(12);
+        bst.add(35);
+        bst.add(70);
+        bst.add(34);
+        bst.add(50);
+        bst.add(68);
+        bst.add(99);
+        System.out.println(bst.inOrder());
+        System.out.println(bst.contains(34));
+
+        System.out.println(bst.minValue());
+        System.out.println(bst.maxValue());
+
+        System.out.println(bst.removeMin());
+        System.out.println(bst.inOrder());
+
+        System.out.println(bst.removeMax());
+        System.out.println(bst.inOrder());
+
+        bst.remove(66);
+        System.out.println(bst.inOrder());
+
+        // bst.set(50, 100); // Breaking the properties of BST
+        System.out.println(bst.inOrder());
+    }
 }
