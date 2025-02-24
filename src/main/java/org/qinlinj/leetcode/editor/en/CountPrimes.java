3c6a4d1f20a44bdbd92c5a550201055d18d223cd
qinlinj <justinqinlin@gmail.com>
2025-02-24 15:15:06 -0800

    chore(.gitignore): add .idea/ to .gitignore to prevent IntelliJ project files from being tracked feat(CountPrimes.java): add solution2 class with an optimized countPrimes method for better performance in prime counting

--- src/main/java/org/qinlinj/leetcode/editor/en/CountPrimes.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/CountPrimes.java
@@ -33,7 +33,27 @@ public class CountPrimes {
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
-
+    
+    class solution2 {
+        public int countPrimes(int n) {
+            boolean[] isPrime = new boolean[n];
+            Arrays.fill(isPrime, true);
+            for (int i = 2; i * i < n; i++) {
+                if (isPrime[i]) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+            int count = 0;
+            for (int i = 2; i < isPrime.length; i++) {
+                if (isPrime[i]) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
 }
 
 
