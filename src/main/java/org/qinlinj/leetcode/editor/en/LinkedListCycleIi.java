71996deb43cc1634448bb2b9ed9221eacc4265c3
qinlinj <justinqinlin@gmail.com>
2025-02-23 18:02:33 -0800

    feat(leetcode): add LinkedListCycleIi.java solution and update editor configuration fix(statistics.xml): increment medium and solved total question counts to reflect recent changes fix(workspace.xml): update work item duration to correct value

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/LinkedListCycleIi.java
@@ -0,0 +1,46 @@
+package org.qinlinj.leetcode.editor.en;
+
+import org.qinlinj.linear.algo.linkedlist.ListNode;
+
+// [142] Linked List Cycle II
+public class LinkedListCycleIi{
+    public static void main(String[] args) {
+        Solution solution = new LinkedListCycleIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
