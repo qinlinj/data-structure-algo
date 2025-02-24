d13b877f5c74dc07da2088f1eaf4bc29747c90e0
qinlinj <justinqinlin@gmail.com>
2025-02-24 14:12:17 -0800

    feat: add UglyNumberIi.java solution for LeetCode problem 264
    chore: update editor.xml to include configuration for UglyNumberIi.java
    
    chore: modify workspace.xml to reflect changes in editor.xml and update work item duration

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/UglyNumberIi.java
@@ -0,0 +1,64 @@
+package org.qinlinj.leetcode.editor.en;
+// [264] Ugly Number II
+public class UglyNumberIi{
+    public static void main(String[] args) {
+        Solution solution = new UglyNumberIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int nthUglyNumber(int n) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+
+
+
+
+
+
+
+
+//An ugly number is a positive integer whose prime factors are limited to 2, 3, 
+//and 5. 
+//
+// Given an integer n, return the nᵗʰ ugly number. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: n = 10
+//Output: 12
+//Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 10 
+//ugly numbers.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: n = 1
+//Output: 1
+//Explanation: 1 has no prime factors, therefore all of its prime factors are 
+//limited to 2, 3, and 5.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= n <= 1690 
+// 
+//
+// Related TopicsHash Table | Math | Dynamic Programming | Heap (Priority Queue)
+// 
+//
+// 👍 6632, 👎 416bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
