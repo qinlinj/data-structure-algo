1cd750ae50fc5106de88585e4af7ac52fa627475
qinlinj <justinqinlin@gmail.com>
2025-02-23 23:35:09 -0800

    feat(AVLTree): implement basic structure for AVLTree with size and root properties feat(AVLTree): add methods to get size and check if the tree is empty feat(AVLTree): add private method to retrieve the height of a given node

--- src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTree.java
@@ -1,6 +1,27 @@
 package org.qinlinj.nonlinear.tree.avltree;
 
 public class AVLTree {
+    private TreeNode root;
+    private int size;
+
+    public AVLTree() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+        return node.height;
+    }
+
     private class TreeNode<E extends Comparable<E>> {
         E data;
         TreeNode left;
@@ -11,4 +32,5 @@ public class AVLTree {
             this.data = data;
         }
     }
+
 }
