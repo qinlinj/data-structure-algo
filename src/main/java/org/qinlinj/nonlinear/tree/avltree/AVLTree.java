a95b378c7d5bf485e53ca7f4a3310ced13e5043c
qinlinj <justinqinlin@gmail.com>
2025-02-23 23:33:40 -0800

    feat(avltree): add TreeNode inner class to represent nodes in the AVL tree for better structure and encapsulation

--- src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTree.java
@@ -1,4 +1,14 @@
 package org.qinlinj.nonlinear.tree.avltree;
 
 public class AVLTree {
+    private class TreeNode<E extends Comparable<E>> {
+        E data;
+        TreeNode left;
+        TreeNode right;
+        int height = 1;
+
+        public TreeNode(E data) {
+            this.data = data;
+        }
+    }
 }
