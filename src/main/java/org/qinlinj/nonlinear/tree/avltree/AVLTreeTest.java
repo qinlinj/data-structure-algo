d8753b062574ba441978ba490c4fe85cdc1356a3
qinlinj <justinqinlin@gmail.com>
2025-02-23 23:54:03 -0800

    feat(AVLTreeTest): add main method to AVLTreeTest for testing AVL tree functionality chore(workspace.xml): update run configuration to use AVLTreeTest as the main application

--- src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTreeTest.java
+++ src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTreeTest.java
@@ -1,4 +1,16 @@
 package org.qinlinj.nonlinear.tree.avltree;
 
 public class AVLTreeTest {
+    public static void main(String[] args) {
+        AVLTree<Integer> avl = new AVLTree<>();
+
+        avl.add(9);
+        avl.add(10);
+        avl.add(11);
+
+        avl.add(13);
+
+        System.out.println("is binary search tree：" + avl.isBinarySearchTree());
+        System.out.println("is balanced：" + avl.isBalanced());
+    }
 }
