0b215e0b508003bec58dd4d33ec1df77bbae1f11
qinlinj <justinqinlin@gmail.com>
2025-02-23 23:39:33 -0800

    feat(AVLTree.java): implement add, contains, and traversal methods for AVL tree to enhance functionality and usability

--- src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/avltree/AVLTree.java
@@ -1,5 +1,9 @@
 package org.qinlinj.nonlinear.tree.avltree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class AVLTree<E extends Comparable<E>> {
     private TreeNode root;
     private int size;
@@ -15,6 +19,105 @@ public class AVLTree<E extends Comparable<E>> {
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    /************************* Insert *******************************/
+    // O(logn)
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    private TreeNode add(TreeNode node, E e) {
+
+        if (node == null) {
+            size++;
+            return new TreeNode(e);
+        }
+
+        if (e.compareTo(node.data) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.data) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+
+    /************************* Retrieve *******************************/
+    public boolean contains(E target) {
+        TreeNode node = find(target);
+        if (node == null) return false;
+        return true;
+    }
+
+    public TreeNode find(E target) {
+        return find(root, target);
+    }
+
+    private TreeNode find(TreeNode node, E target) {
+        if (node == null) return null;
+
+        if (target.compareTo(node.data) == 0) {
+            return node;
+        } else if (target.compareTo(node.data) < 0) {
+            return find(node.left, target);
+        } else {
+            return find(node.right, target);
+        }
+    }
+
+    public List<E> preOrder() {
+        List<E> res = new ArrayList<>();
+
+        preOrder(root, res);
+
+        return res;
+    }
+
+    private void preOrder(TreeNode node, List<E> res) {
+        if (node == null) {
+            return;
+        }
+
+        res.add(node.data);
+        preOrder(node.left, res);
+        preOrder(node.right, res);
+    }
+
+    public List<E> inOrder() {
+        List<E> res = new ArrayList<>();
+
+        inOrder(root, res);
+
+        return res;
+    }
+
+    private void inOrder(TreeNode node, List<E> res) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, res);
+        res.add(node.data);
+        inOrder(node.right, res);
+    }
+
+    public List<E> postOrder() {
+        LinkedList res = new LinkedList<>();
+
+        postOrder(root, res);
+
+        return res;
+    }
+
+    private void postOrder(TreeNode node, List<E> res) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left, res);
+        postOrder(node.right, res);
+        res.add(node.data);
     }
 
     private class TreeNode {
