b41dd1b42d779bbfcf0a9d4368d9a6709dd9513c
qinlinj <justinqinlin@gmail.com>
2025-02-24 23:10:17 -0800

    feat(RBTree.java): add TreeNode inner class to represent nodes in the RBTree for better structure and clarity

--- src/main/java/org/qinlinj/nonlinear/tree/rbtree/RBTree.java
+++ src/main/java/org/qinlinj/nonlinear/tree/rbtree/RBTree.java
@@ -3,4 +3,16 @@ package org.qinlinj.nonlinear.tree.rbtree;
 public class RBTree<E extends Comparable<E>> {
     private static boolean RED = true;
     private static boolean BLACK = false;
+
+    private class TreeNode {
+        E data;
+        TreeNode left;
+        TreeNode right;
+        boolean color; // 颜色
+
+        public TreeNode(E data) {
+            this.data = data;
+            this.color = RED;
+        }
+    }
 }
