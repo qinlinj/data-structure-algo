f1dd088800c47d3addaa0442595ab9b6c3398755
qinlinj <justinqinlin@gmail.com>
2025-02-25 12:52:51 -0800

    feat(KthSmallestElementInASortedMatrix): implement kthSmallest method to return the k-th smallest element in a sorted matrix
    This change provides a functional implementation of the kthSmallest method, allowing users to retrieve the k-th smallest element efficiently from a given sorted matrix.

--- src/main/java/org/qinlinj/leetcode/editor/en/KthSmallestElementInASortedMatrix.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/KthSmallestElementInASortedMatrix.java
@@ -9,8 +9,19 @@ public class KthSmallestElementInASortedMatrix {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int kthSmallest(int[][] matrix, int k) {
-
-            return 0;
+            if (k == 1) {
+                return matrix[0][0];
+            }
+            int size = matrix[0].length;
+            int columnNum = k / size;
+            int rowNum;
+            if (k % size == 0) {
+                columnNum = columnNum - 1;
+                rowNum = size - 1;
+            } else {
+                rowNum = k % size - 1;
+            }
+            return matrix[rowNum][columnNum];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
