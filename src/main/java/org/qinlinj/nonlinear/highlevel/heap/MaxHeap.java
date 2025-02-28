32ae629f95f87d46173022ae4e9708700171f38e
qinlinj <justinqinlin@gmail.com>
2025-02-27 23:50:01 -0800

    feat(MaxHeap.java): add ArrayList to store heap elements and initialize it in the constructor to manage heap data efficiently

--- src/main/java/org/qinlinj/nonlinear/highlevel/heap/MaxHeap.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/heap/MaxHeap.java
@@ -1,4 +1,11 @@
 package org.qinlinj.nonlinear.highlevel.heap;
 
+import java.util.ArrayList;
+
 public class MaxHeap<E extends Comparable<E>> {
+    private ArrayList<E> data;
+
+    public MaxHeap(int capacity) {
+        this.data = new ArrayList<>(capacity);
+    }
 }
