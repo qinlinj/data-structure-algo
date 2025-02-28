f8c1a34cc6871aafe0435a6789862e55bafd4a85
qinlinj <justinqinlin@gmail.com>
2025-02-27 23:52:26 -0800

    feat(MaxHeap.java): add leftChild and rightChild methods to improve heap navigation functionality

--- src/main/java/org/qinlinj/nonlinear/highlevel/heap/MaxHeap.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/heap/MaxHeap.java
@@ -20,4 +20,12 @@ public class MaxHeap<E extends Comparable<E>> {
     public boolean isEmpty() {
         return data.isEmpty();
     }
+
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
 }
