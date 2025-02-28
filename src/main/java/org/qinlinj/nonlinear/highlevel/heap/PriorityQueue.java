f26feb9f0afd696af58bf6c3748b8a4c7e38d27c
qinlinj <justinqinlin@gmail.com>
2025-02-28 01:17:25 -0800

    feat(PriorityQueue.java): implement a generic priority queue using a max heap to enhance functionality and usability

--- src/main/java/org/qinlinj/nonlinear/highlevel/heap/PriorityQueue.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/heap/PriorityQueue.java
@@ -1,4 +1,36 @@
 package org.qinlinj.nonlinear.highlevel.heap;
 
-public class PriorityQueue {
+import org.qinlinj.linear.queue.Queue;
+
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        this.maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.removeMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
 }
