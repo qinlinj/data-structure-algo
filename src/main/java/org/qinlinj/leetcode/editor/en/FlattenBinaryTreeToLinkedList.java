6a3f721351d65e752384695640ba3d3fa097762e
qinlinj <justinqinlin@gmail.com>
2025-02-28 18:34:02 -0800

    feat(tree): add TreeNode class for binary tree representation and implement FlattenBinaryTreeToLinkedList class for flattening functionality

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,85 @@
+package org.qinlinj.leetcode.editor.en;
+
+// [114] Flatten Binary Tree to Linked List
+public class FlattenBinaryTreeToLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new FlattenBinaryTreeToLinkedList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public void flatten(TreeNode root) {
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+//Given the root of a binary tree, flatten the tree into a "linked list":
+//
+// 
+// The "linked list" should use the same TreeNode class where the right child 
+//pointer points to the next node in the list and the left child pointer is always 
+//null. 
+// The "linked list" should be in the same order as a pre-order traversal of 
+//the binary tree. 
+// 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: root = [1,2,5,3,4,null,6]
+//Output: [1,null,2,null,3,null,4,null,5,null,6]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [0]
+//Output: [0]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 2000]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//Follow up: Can you flatten the tree in-place (with 
+//O(1) extra space)?
+//
+// Related TopicsLinked List | Stack | Tree | Depth-First Search | Binary Tree 
+//
+// 👍 12778, 👎 573bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
