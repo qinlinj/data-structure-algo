383e6f9b0d933d1e0499349fcc05e9eb36e77ea2
qinlinj <justinqinlin@gmail.com>
2025-03-01 23:59:45 -0800

    feat(TestFileReader.java): implement readFile method to read words from a file and return them as a list for improved file handling functionality

--- src/main/java/org/qinlinj/others/test/TestFileReader.java
+++ src/main/java/org/qinlinj/others/test/TestFileReader.java
@@ -1,4 +1,32 @@
 package org.qinlinj.others.test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class TestFileReader {
+    public static List<String> readFile(String fileName) {
+        List<String> words = new ArrayList<>();
+        try {
+            BufferedReader reader =
+                    new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String trimmedLine = line.trim();
+                if (trimmedLine.isEmpty()) {
+                    continue;
+                }
+                String[] lineWords = trimmedLine.split(" ");
+                for (String word : lineWords) {
+                    words.add(word);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
 }
