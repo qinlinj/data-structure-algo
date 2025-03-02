33de4769bd5048361e9945d920e46549513b5d9d
qinlinj <justinqinlin@gmail.com>
2025-03-01 23:52:57 -0800

    feat(TestSet.java): add testSet method to measure performance of set.contains for a given word

--- src/main/java/org/qinlinj/nonlinear/highlevel/set/TestSet.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/set/TestSet.java
@@ -1,4 +1,22 @@
 package org.qinlinj.nonlinear.highlevel.set;
 
+import java.util.List;
+
 public class TestSet {
+    private static double testSet(Set<String> set, List<String> words) {
+        for (String word : words)
+            set.add(word);
+
+        long start = System.nanoTime();
+
+
+        for (int i = 0; i < 10000; i++) {
+            set.contains("father");
+        }
+
+
+        long end = System.nanoTime();
+
+        return (end - start) / 1000_000_000.0;
+    }
 }
