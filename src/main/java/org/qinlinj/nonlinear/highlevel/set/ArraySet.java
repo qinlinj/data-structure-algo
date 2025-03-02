17d1dbf19f403579c000a0c8bce159d70fcbd6d0
qinlinj <justinqinlin@gmail.com>
2025-03-01 23:45:23 -0800

    feat(ArraySet): implement Set interface and add basic method stubs for size, isEmpty, add, remove, and contains to establish a foundational structure for the ArraySet class

--- src/main/java/org/qinlinj/nonlinear/highlevel/set/ArraySet.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/set/ArraySet.java
@@ -1,4 +1,32 @@
 package org.qinlinj.nonlinear.highlevel.set;
 
-public class ArraySet {
+public class ArraySet implements Set {
+    public ArraySet() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public void remove(Object o) {
+
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
 }
