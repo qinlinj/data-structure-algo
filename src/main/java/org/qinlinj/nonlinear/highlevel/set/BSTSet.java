7d4da79c98b3d9984b19e04dfd81b23a19a56861
qinlinj <justinqinlin@gmail.com>
2025-03-01 23:44:27 -0800

    feat(BSTSet): implement Set interface in BSTSet class to define basic set operations

--- src/main/java/org/qinlinj/nonlinear/highlevel/set/BSTSet.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/set/BSTSet.java
@@ -1,4 +1,28 @@
 package org.qinlinj.nonlinear.highlevel.set;
 
-public class BSTSet {
+public class BSTSet implements Set {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public void remove(Object o) {
+
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
 }
