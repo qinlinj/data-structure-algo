4c13e895de4cf3f2f9a1679bbd7550f1caccafee
qinlinj <justinqinlin@gmail.com>
2025-03-01 23:47:04 -0800

    feat(ArraySet.java): implement generic type support for ArraySet class to enhance type safety and usability refactor(ArraySet.java): replace placeholder methods with actual implementations using ArrayList for size, isEmpty, add, remove, and contains methods

--- src/main/java/org/qinlinj/nonlinear/highlevel/set/ArraySet.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/set/ArraySet.java
@@ -1,32 +1,38 @@
 package org.qinlinj.nonlinear.highlevel.set;
 
-public class ArraySet implements Set {
-    public ArraySet() {
+import org.qinlinj.linear.arraylist.ArrayList;
 
+public class ArraySet<E> implements Set<E> {
+    private ArrayList<E> data;
+
+    public ArraySet() {
+        this.data = new ArrayList<>();
     }
 
     @Override
     public int size() {
-        return 0;
+        return data.getSize();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return data.isEmpty();
     }
 
     @Override
-    public void add(Object o) {
-
+    public void add(E e) { // O(n)
+        if (!data.contains(e)) {
+            data.addLast(e);
+        }
     }
 
     @Override
-    public void remove(Object o) {
-
+    public void remove(E e) { // O(n)
+        data.removeElement(e);
     }
 
     @Override
-    public boolean contains(Object o) {
-        return false;
+    public boolean contains(E e) { // O(n)
+        return data.contains(e);
     }
 }
