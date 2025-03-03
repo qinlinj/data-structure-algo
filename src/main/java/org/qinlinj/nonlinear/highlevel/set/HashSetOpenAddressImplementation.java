86ecf982f50f424d0554a1e9d7da0c614a77a712
qinlinj <justinqinlin@gmail.com>
2025-03-02 21:32:50 -0800

    feat(HashSetImplementations): implement Set interface in HashSetLinkedListImplementation and HashSetOpenAddressImplementation classes to define standard set operations
    This change provides a consistent API for both implementations, allowing for better usability and integration with other components.

--- src/main/java/org/qinlinj/nonlinear/highlevel/set/HashSetOpenAddressImplementation.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/set/HashSetOpenAddressImplementation.java
@@ -1,4 +1,31 @@
 package org.qinlinj.nonlinear.highlevel.set;
 
-public class HashSetOpenAddressImplementation {
+public class HashSetOpenAddressImplementation<E> implements Set<E> {
+    public HashSetOpenAddressImplementation() {
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
 }
