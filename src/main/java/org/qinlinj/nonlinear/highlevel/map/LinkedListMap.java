05df395b044d5d098bde5749a52e55afdcef5771
qinlinj <justinqinlin@gmail.com>
2025-03-03 22:05:26 -0800

    feat(LinkedListMap): implement a new LinkedListMap class for key-value storage using a linked list structure

---
+++ src/main/java/org/qinlinj/nonlinear/highlevel/map/LinkedListMap.java
@@ -0,0 +1,108 @@
+package org.qinlinj.nonlinear.highlevel.map;
+
+public class LinkedListMap<K, V> implements Map<K, V> {
+
+    public LinkedListMap() {
+    }
+
+    /**
+     * Get the number of key-value pairs stored in the map.
+     *
+     * @return the size of the map (number of entries)
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Check if the map is empty.
+     *
+     * @return true if the map contains no key-value pairs, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Add a new key-value pair to the map.
+     * If the key already exists, it will be updated with the new value.
+     *
+     * @param key   the key to be added
+     * @param value the value associated with the key
+     */
+    @Override
+    public void add(K key, V value) {
+
+    }
+
+    /**
+     * Remove the key-value pair associated with the specified key.
+     *
+     * @param key the key to be removed
+     * @return the value previously associated with the key, or null if not found
+     */
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    /**
+     * Update the value associated with the specified key.
+     *
+     * @param key      the key whose value is to be updated
+     * @param newValue the new value to be associated with the key
+     */
+    @Override
+    public void set(K key, V newValue) {
+
+    }
+
+    /**
+     * Retrieve the value associated with the specified key.
+     *
+     * @param key the key to look up
+     * @return the value associated with the key, or null if the key is not found
+     */
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    /**
+     * Check if the map contains the specified key.
+     *
+     * @param key the key to check for existence
+     * @return true if the key exists in the map, false otherwise
+     */
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    private class Node {
+        K key;
+        V value;
+        Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(K key, V value) {
+            this(key, value, null);
+        }
+
+        public Node() {
+            this(null, null, null);
+        }
+
+        @Override
+        public String toString() {
+            return key.toString() + "->" + value.toString();
+        }
+    }
+}
