5a315f2a3152f6df2934c257af747df038adacc5
qinlinj <justinqinlin@gmail.com>
2025-03-04 23:59:15 -0800

    feat(SkipList.java): add MAX_LEVEL constant and initialize levelCount, dummyHead, and random for skip list implementation

--- src/main/java/org/qinlinj/nonlinear/highlevel/skiplist/SkipList.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/skiplist/SkipList.java
@@ -1,4 +1,13 @@
 package org.qinlinj.nonlinear.highlevel.skiplist;
 
+import java.util.Random;
+
 public class SkipList {
+    private static final int MAX_LEVEL = 16;
+
+    private int levelCount;
+
+    private Node dummyHead;
+    private Random random = new Random();
+
 }
