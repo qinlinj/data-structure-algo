78e719551d173b9bc4bf2498ae5e0d0052c73ed5
qinlinj <justinqinlin@gmail.com>
2025-03-04 23:57:02 -0800

    feat(JavaSetMap): add main method to demonstrate usage of HashSet and HashMap for better understanding of data structures

--- src/main/java/org/qinlinj/nonlinear/highlevel/map/JavaSetMap.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/map/JavaSetMap.java
@@ -1,4 +1,12 @@
 package org.qinlinj.nonlinear.highlevel.map;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JavaSetMap {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        Map<Integer, Integer> map = new HashMap<>();
+    }
 }
