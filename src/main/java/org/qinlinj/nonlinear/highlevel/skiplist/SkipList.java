ca439901fde628caa4bc899daf1534eb622fae26
qinlinj <justinqinlin@gmail.com>
2025-03-04 23:59:29 -0800

    feat(SkipList.java): add inner Node class to encapsulate node data and next nodes for better structure and readability feat(SkipList.java): implement toString method in Node class for easier debugging and visualization of node information

--- src/main/java/org/qinlinj/nonlinear/highlevel/skiplist/SkipList.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/skiplist/SkipList.java
@@ -6,8 +6,31 @@ public class SkipList {
     private static final int MAX_LEVEL = 16;
 
     private int levelCount;
-
     private Node dummyHead;
     private Random random = new Random();
+
+    private class Node<E extends Comparable<E>> {
+        E data;
+
+        Node[] nextNodes;
+
+        int maxIndexLevel = 0;
+
+        Node(E data) {
+            this.data = data;
+            nextNodes = new Node[MAX_LEVEL];
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{ data: ");
+            builder.append(data);
+            builder.append("; levels: ");
+            builder.append(maxIndexLevel);
+            builder.append(" }");
+            return builder.toString();
+        }
+    }
 
 }
