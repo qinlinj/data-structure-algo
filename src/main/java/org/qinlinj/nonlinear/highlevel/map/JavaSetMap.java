2a889710e9c106341820dfd2085c933101140311
qinlinj <justinqinlin@gmail.com>
2025-03-05 02:12:32 -0800

    docs(JavaSetMap.java): add class-level documentation and comments to explain Set and Map implementations

--- src/main/java/org/qinlinj/nonlinear/highlevel/map/JavaSetMap.java
+++ src/main/java/org/qinlinj/nonlinear/highlevel/map/JavaSetMap.java
@@ -3,10 +3,17 @@ package org.qinlinj.nonlinear.highlevel.map;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Class demonstrating basic Java Set and Map implementations
+ */
 public class JavaSetMap {
     public static void main(String[] args) {
+        // TreeSet: implemented using Red-Black tree, elements are ordered
+        // HashSet: implemented using hash function, uses array + linked list for collision resolution
         Set<Integer> set = new HashSet<>();
 
+        // TreeMap: implemented using Red-Black tree, keys are ordered
+        // HashMap: implemented using hash function, uses array + linked list for collision resolution
         Map<Integer, Integer> map = new HashMap<>();
     }
 }
