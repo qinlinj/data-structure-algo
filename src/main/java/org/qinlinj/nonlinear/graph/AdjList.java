76ca3689353adaafba86cb2ea01fb3336bb228ca
qinlinj <justinqinlin@gmail.com>
2025-03-05 23:46:05 -0800

    feat(AdjList.java): implement Graph interface with basic methods for edges and vertices
    This change provides a foundational implementation of the Graph interface,
    including methods to get the number of edges and vertices, check for edges,
    retrieve adjacent vertices, and determine the degree of a vertex. This
    enhances the functionality of the AdjList class and prepares it for further
    development.

--- src/main/java/org/qinlinj/nonlinear/graph/AdjList.java
+++ src/main/java/org/qinlinj/nonlinear/graph/AdjList.java
@@ -1,4 +1,61 @@
 package org.qinlinj.nonlinear.graph;
 
-public class AdjList {
+import java.util.Collection;
+import java.util.List;
+
+public class AdjList implements Graph {
+
+    /**
+     * Get the number of edges in the graph
+     *
+     * @return the number of edges
+     */
+    @Override
+    public int getE() {
+        return 0;
+    }
+
+    /**
+     * Get the number of vertices in the graph
+     *
+     * @return the number of vertices
+     */
+    @Override
+    public int getV() {
+        return 0;
+    }
+
+    /**
+     * Determine if there is an edge between two specified vertices
+     *
+     * @param v first vertex
+     * @param w second vertex
+     * @return true if an edge exists, false otherwise
+     */
+    @Override
+    public boolean hasEdge(int v, int w) {
+        return false;
+    }
+
+    /**
+     * Get all adjacent vertices of the specified vertex
+     *
+     * @param v the vertex
+     * @return a collection of adjacent vertices
+     */
+    @Override
+    public Collection<Integer> adj(int v) {
+        return List.of();
+    }
+
+    /**
+     * Get the degree of the specified vertex
+     *
+     * @param v the vertex
+     * @return the degree of the vertex
+     */
+    @Override
+    public int degree(int v) {
+        return 0;
+    }
 }
