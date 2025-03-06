42d316dfda4b3a2017dc155b5e25b389e8f15cf4
qinlinj <justinqinlin@gmail.com>
2025-03-05 23:53:13 -0800

    feat(AdjMatrix): implement constructor to initialize adjacency matrix from a file to enhance graph representation capabilities fix(AdjMatrix): add vertex validation and prevent self-loops in adjacency matrix to ensure graph integrity

--- src/main/java/org/qinlinj/nonlinear/graph/AdjMatrix.java
+++ src/main/java/org/qinlinj/nonlinear/graph/AdjMatrix.java
@@ -1,10 +1,51 @@
 package org.qinlinj.nonlinear.graph;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
 public class AdjMatrix implements Graph {
-    public AdjMatrix() {
+    private int V;
+    private int E;
+    private int[][] adj;
+
+    public AdjMatrix(String fileName) {
+        try {
+            BufferedReader reader
+                    = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            String[] arr = line.split(" ");
+            this.V = Integer.valueOf(arr[0]);
+            this.E = Integer.valueOf(arr[1]);
+
+            this.adj = new int[V][V];
+            while ((line = reader.readLine()) != null) {
+                arr = line.split(" ");
+                int a = Integer.valueOf(arr[0]);
+                validateVertex(a);
+                int b = Integer.valueOf(arr[1]);
+                validateVertex(b);
+
+                if (a == b) {
+                    throw new RuntimeException("");
+                }
+
+                if (adj[a][b] == 1) {
+                    throw new RuntimeException("");
+                }
+                adj[a][b] = 1;
+                adj[b][a] = 1;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void validateVertex(int a) {
+
     }
 
     /**
