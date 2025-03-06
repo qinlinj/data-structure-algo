6e410ce1cf8ef99e5eabeca7c5e76b81951e2a4e
qinlinj <justinqinlin@gmail.com>
2025-03-05 23:47:05 -0800

    feat(AdjList): implement constructor to initialize graph from a file to enhance graph creation flexibility chore(AdjList): add vertex validation method to ensure graph integrity during initialization

--- src/main/java/org/qinlinj/nonlinear/graph/AdjList.java
+++ src/main/java/org/qinlinj/nonlinear/graph/AdjList.java
@@ -1,5 +1,8 @@
 package org.qinlinj.nonlinear.graph;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,6 +12,46 @@ public class AdjList implements Graph {
     private int V;
     private int E;
     private LinkedList<Integer>[] adj;
+
+    public AdjList(String fileName) {
+        try {
+            BufferedReader reader
+                    = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            String[] arr = line.split(" ");
+            this.V = Integer.valueOf(arr[0]);
+            this.E = Integer.valueOf(arr[1]);
+
+            this.adj = new LinkedList[V];
+            for (int i = 0; i < V; i++) {
+                adj[i] = new LinkedList<>();
+            }
+            while ((line = reader.readLine()) != null) { // O(E)
+                arr = line.split(" ");
+                int a = Integer.valueOf(arr[0]);
+                validateVertex(a);
+                int b = Integer.valueOf(arr[1]);
+                validateVertex(b);
+
+                if (a == b) {
+                    throw new RuntimeException("");
+                }
+
+                if (adj[a].contains(b)) { // O(V)
+                    throw new RuntimeException("");
+                }
+                adj[a].add(b);
+                adj[b].add(a);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void validateVertex(int a) {
+
+    }
 
     /**
      * Get the number of edges in the graph
