fc9d5a84bb8036b80fac5804f7912ac5f7e022c0
qinlinj <justinqinlin@gmail.com>
2025-03-05 23:44:28 -0800

    feat(Graph.java): convert Graph class to interface and add graph methods to define graph behavior and structure

--- src/main/java/org/qinlinj/nonlinear/graph/Graph.java
+++ src/main/java/org/qinlinj/nonlinear/graph/Graph.java
@@ -1,4 +1,44 @@
 package org.qinlinj.nonlinear.graph;
 
-public class Graph {
+import java.util.Collection;
+
+public interface Graph {
+    /**
+     * Get the number of edges in the graph
+     *
+     * @return the number of edges
+     */
+    int getE();
+
+    /**
+     * Get the number of vertices in the graph
+     *
+     * @return the number of vertices
+     */
+    int getV();
+
+    /**
+     * Determine if there is an edge between two specified vertices
+     *
+     * @param v first vertex
+     * @param w second vertex
+     * @return true if an edge exists, false otherwise
+     */
+    boolean hasEdge(int v, int w);
+
+    /**
+     * Get all adjacent vertices of the specified vertex
+     *
+     * @param v the vertex
+     * @return a collection of adjacent vertices
+     */
+    Collection<Integer> adj(int v);
+
+    /**
+     * Get the degree of the specified vertex
+     *
+     * @param v the vertex
+     * @return the degree of the vertex
+     */
+    int degree(int v);
 }
