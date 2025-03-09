aa9dede9781365308f434278b135b2ef50a71459
qinlinj <justinqinlin@gmail.com>
2025-03-08 20:21:00 -0800

    feat(GraphBFS): implement breadth-first search (BFS) algorithm for graph traversal
    Add a constructor to initialize the graph and perform BFS on all vertices.
    This change allows for efficient traversal of the graph and
    ensures all connected components are visited.

--- src/main/java/org/qinlinj/nonlinear/graph/bfs/GraphBFS.java
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/GraphBFS.java
@@ -1,4 +1,24 @@
 package org.qinlinj.nonlinear.graph.bfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphBFS {
+    private Graph g;
+    private boolean[] visited;
+    private List<Integer> res;
+
+    public GraphBFS(Graph g) {
+        this.g = g;
+
+        this.visited = new boolean[g.getV()];
+        this.res = new ArrayList<>();
+
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) bfs(v);
+        }
+    }
 }
+
