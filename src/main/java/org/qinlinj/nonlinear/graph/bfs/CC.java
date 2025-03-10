5f83cf3d1a751cc9c0d840cf3e0d55bdf731c3a3
qinlinj <justinqinlin@gmail.com>
2025-03-09 20:51:00 -0700

    feat(CC.java): implement breadth-first search (BFS) method for connected components to enhance graph traversal capabilities

--- src/main/java/org/qinlinj/nonlinear/graph/bfs/CC.java
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/CC.java
@@ -3,6 +3,8 @@ package org.qinlinj.nonlinear.graph.bfs;
 import org.qinlinj.nonlinear.graph.Graph;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CC {
     private Graph g;
@@ -23,5 +25,23 @@ public class CC {
         }
     }
 
-    
+    private void bfs(int v, int ccId) {
+        if (g == null) return;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        visited[v] = ccId;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+
+            for (int w : g.adj(curr)) {
+                if (visited[w] == -1) {
+                    queue.add(w);
+                    visited[w] = ccId;
+                }
+            }
+        }
+    }
+
+
 }
