8dba815ceb505fada0be66d62e6b441f582f3dce
qinlinj <justinqinlin@gmail.com>
2025-03-09 20:47:00 -0700

    feat(CC.java): implement connected components algorithm using BFS to identify and count connected components in a graph

--- src/main/java/org/qinlinj/nonlinear/graph/bfs/CC.java
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/CC.java
@@ -1,4 +1,27 @@
 package org.qinlinj.nonlinear.graph.bfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.Arrays;
+
 public class CC {
+    private Graph g;
+    private int[] visited;
+    private int ccCount;
+
+    public CC(Graph g) {
+        this.g = g;
+
+        this.visited = new int[g.getV()];
+        Arrays.fill(visited, -1);
+
+        for (int v = 0; v < g.getV(); v++) {
+            if (visited[v] == -1) {
+                ccCount++;
+                bfs(v, ccCount);
+            }
+        }
+    }
+
+    
 }
