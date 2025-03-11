02ffb32f29a39d7c51bd94c5cd1ed9c929690ac6
qinlinj <justinqinlin@gmail.com>
2025-03-10 23:54:14 -0700

    feat(GraphDFS): implement depth-first search (DFS) initialization in GraphDFS class to traverse the graph and store results chore(GraphDFS): add necessary imports and class variables for graph traversal functionality

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/GraphDFS.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/GraphDFS.java
@@ -1,4 +1,30 @@
 package org.qinlinj.nonlinear.graph.dfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.*;
+
 public class GraphDFS {
+    private Graph g;
+
+    private List<Integer> res;
+    private boolean[] visited;
+
+    public GraphDFS(Graph g) {
+        this.g = g;
+        if (g == null) return;
+
+        this.res = new ArrayList<>();
+        this.visited = new boolean[g.getV()];
+        // 遍历图中每个顶点
+        for (int v = 0; v < g.getV(); v++) {
+            // 先判断，没有遍历的顶点才能进行深度优先遍历
+            if (!visited[v]) {
+                dfs(v);
+            }
+        }
+    }
+
+    private void dfs(int v) {
+    }
 }
