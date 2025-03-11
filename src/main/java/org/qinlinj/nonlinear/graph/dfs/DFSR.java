a85b626d7c6a06d06e3f2559827681659f91e4c3
qinlinj <justinqinlin@gmail.com>
2025-03-10 23:57:28 -0700

    feat(DFSR): add DFSR class for depth-first search implementation in graph module to facilitate graph traversal operations

---
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/DFSR.java
@@ -0,0 +1,11 @@
+package org.qinlinj.nonlinear.graph.dfs;
+
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.*;
+
+public class DFSR {
+    private Graph g;
+    private List<Integer> res;
+    private boolean[] visited;
+}
