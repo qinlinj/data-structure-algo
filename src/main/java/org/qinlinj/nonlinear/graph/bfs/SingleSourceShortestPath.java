6de0267ea57562ce2e03a2e904fcf7bf6896260c
qinlinj <justinqinlin@gmail.com>
2025-03-10 22:59:33 -0700

    feat(graph): add SingleSourceShortestPath class to implement BFS for shortest path calculation

---
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/SingleSourceShortestPath.java
@@ -0,0 +1,13 @@
+package org.qinlinj.nonlinear.graph.bfs;
+
+import org.qinlinj.nonlinear.graph.Graph;
+
+public class SingleSourceShortestPath {
+    private Graph g;
+    private boolean[] visited;
+    private int[] prevs;
+    private int[] distance;
+
+    private int source;
+
+}
