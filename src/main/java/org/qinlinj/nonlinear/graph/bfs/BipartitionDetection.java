8b79d85cbb9b539ac0caf85905d1a457ea988b3c
qinlinj <justinqinlin@gmail.com>
2025-03-10 23:46:01 -0700

    feat(BipartitionDetection): implement constructor to initialize graph and detect bipartition using BFS algorithm
    chore(BipartitionDetection): add private bfs method placeholder for future implementation

--- src/main/java/org/qinlinj/nonlinear/graph/bfs/BipartitionDetection.java
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/BipartitionDetection.java
@@ -2,9 +2,31 @@ package org.qinlinj.nonlinear.graph.bfs;
 
 import org.qinlinj.nonlinear.graph.Graph;
 
+import java.util.*;
+
 public class BipartitionDetection {
     private Graph g;
     private boolean[] visited;
     private int[] colors;
     private boolean isBipartition = true;
+
+    public BipartitionDetection(Graph g) {
+        this.g = g;
+
+        this.visited = new boolean[g.getV()];
+        this.colors = new int[g.getV()];
+        Arrays.fill(this.colors, -1);
+
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) {
+                if (!bfs(v)) {
+                    isBipartition = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    private boolean bfs(int v) {
+    }
 }
