27f24246ffb64b9dc56ada2fb526b598da13906e
qinlinj <justinqinlin@gmail.com>
2025-03-10 23:09:53 -0700

    feat(SingleSourceShortestPath): implement constructor and initialize BFS variables for single source shortest path algorithm

--- src/main/java/org/qinlinj/nonlinear/graph/bfs/SingleSourceShortestPath.java
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/SingleSourceShortestPath.java
@@ -2,6 +2,8 @@ package org.qinlinj.nonlinear.graph.bfs;
 
 import org.qinlinj.nonlinear.graph.Graph;
 
+import java.util.*;
+
 public class SingleSourceShortestPath {
     private Graph g;
     private boolean[] visited;
@@ -10,4 +12,19 @@ public class SingleSourceShortestPath {
 
     private int source;
 
+    public SingleSourceShortestPath(Graph g, int source) {
+        this.g = g;
+        this.source = source;
+        this.visited = new boolean[g.getV()];
+        this.prevs = new int[g.getV()];
+        this.distance = new int[g.getV()];
+        Arrays.fill(this.prevs, -1);
+        Arrays.fill(this.distance, -1);
+
+        bfs(source);
+    }
+
+    private void bfs(int v) {
+
+    }
 }
