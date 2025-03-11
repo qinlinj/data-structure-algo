80f4c6c00825960d15328aa9cf797ff83bd3dd54
qinlinj <justinqinlin@gmail.com>
2025-03-10 23:57:59 -0700

    refactor(graph): remove DFSR class and replace it with GraphDFSR class to improve clarity and functionality

---
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/GraphDFSR.java
@@ -0,0 +1,28 @@
+package org.qinlinj.nonlinear.graph.dfs;
+
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.*;
+
+public class GraphDFSR {
+    private Graph g;
+    private List<Integer> res;
+    private boolean[] visited;
+
+    public GraphDFSR(Graph g) {
+        this.g = g;
+
+        if (g == null) return;
+
+        this.res = new ArrayList<>();
+        this.visited = new boolean[g.getV()];
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) {
+                dfs(v);
+            }
+        }
+    }
+
+    private void dfs(int v) {
+    }
+}
