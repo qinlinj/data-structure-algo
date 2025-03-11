e98f65a4941a5721bb20ca547eb88c429c3e2680
qinlinj <justinqinlin@gmail.com>
2025-03-10 23:45:47 -0700

    feat(BipartitionDetection.java): add new class for detecting bipartition in graphs to support graph algorithms

---
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/BipartitionDetection.java
@@ -0,0 +1,10 @@
+package org.qinlinj.nonlinear.graph.bfs;
+
+import org.qinlinj.nonlinear.graph.Graph;
+
+public class BipartitionDetection {
+    private Graph g;
+    private boolean[] visited;
+    private int[] colors;
+    private boolean isBipartition = true;
+}
