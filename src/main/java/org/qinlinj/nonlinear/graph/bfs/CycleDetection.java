52c0209dfe08317e2fd367fc3c1fbbaee9d48bfd
qinlinj <justinqinlin@gmail.com>
2025-03-10 23:23:03 -0700

    feat(CycleDetection): implement CycleDetection class for detecting cycles in a graph using BFS algorithm

---
+++ src/main/java/org/qinlinj/nonlinear/graph/bfs/CycleDetection.java
@@ -0,0 +1,32 @@
+package org.qinlinj.nonlinear.graph.bfs;
+
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.*;
+
+public class CycleDetection {
+    private Graph g;
+    private boolean[] visited;
+    private int[] prevs;
+
+    private boolean hasCycle = false;
+
+    public CycleDetection(Graph g) {
+        this.g = g;
+        this.visited = new boolean[g.getV()];
+        this.prevs = new int[g.getV()];
+        Arrays.fill(this.prevs, -1);
+
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) {
+                if (bfs(v)) {
+                    hasCycle = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    private boolean bfs(int v) {
+    }
+}
