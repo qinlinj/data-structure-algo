032f31d3d73693ee24d5ebfcd0b2fcdc295c4710
qinlinj <justinqinlin@gmail.com>
2025-03-14 00:14:35 -0700

    feat(SingleSourcePath): implement SingleSourcePath class to find paths in a graph using DFS algorithm
    This change introduces the SingleSourcePath class, which initializes
    the graph and source vertex, sets up visited and previous arrays,
    and prepares for depth-first search (DFS) traversal.

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/SingleSourcePath.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/SingleSourcePath.java
@@ -1,4 +1,27 @@
 package org.qinlinj.nonlinear.graph.dfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
 public class SingleSourcePath {
+    private Graph g;
+
+    private int source;
+
+    private boolean[] visited;
+    private int[] prevs;
+
+    public SingleSourcePath(Graph g, int source) {
+        this.g = g;
+        this.source = source;
+
+        this.visited = new boolean[g.getV()];
+        prevs = new int[g.getV()];
+        for (int i = 0; i < g.getV(); i++) {
+            prevs[i] = -1;
+        }
+        dfs(source, source);
+    }
+
+    private void dfs(int source, int source1) {
+    }
 }
