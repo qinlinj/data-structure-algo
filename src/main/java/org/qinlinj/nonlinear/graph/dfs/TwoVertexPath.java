256bce045a9c6075c55f9e2ad6691ac697928e16
qinlinj <justinqinlin@gmail.com>
2025-03-14 02:30:51 -0700

    feat(TwoVertexPath.java): implement constructor and initialize fields for TwoVertexPath class to facilitate depth-first search for paths between two vertices

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/TwoVertexPath.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/TwoVertexPath.java
@@ -14,4 +14,30 @@ public class TwoVertexPath {
     private int[] prevs;
 
     private List<Integer> res;
+
+    public TwoVertexPath(Graph g, int source, int target) {
+        this.g = g;
+        this.source = source;
+        this.target = target;
+
+        this.res = new ArrayList<>();
+
+        this.visited = new boolean[g.getV()];
+        prevs = new int[g.getV()];
+
+        for (int i = 0; i < g.getV(); i++) {
+            prevs[i] = -1;
+        }
+
+        dfs(source, source);
+
+        path();
+    }
+
+    private void path() {
+    }
+
+    private void dfs(int source, int source1) {
+
+    }
 }
