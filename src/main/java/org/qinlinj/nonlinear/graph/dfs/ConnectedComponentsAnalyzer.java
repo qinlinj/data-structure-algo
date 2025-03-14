f40a75920edb2a3119872cc50dd0a7a03dcc482e
qinlinj <justinqinlin@gmail.com>
2025-03-13 23:39:13 -0700

    feat(ConnectedComponentsAnalyzer): add ConnectedComponentsAnalyzer class to analyze connected components in a graph

---
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/ConnectedComponentsAnalyzer.java
@@ -0,0 +1,11 @@
+package org.qinlinj.nonlinear.graph.dfs;
+
+import org.qinlinj.nonlinear.graph.Graph;
+
+public class ConnectedComponentsAnalyzer {
+    private Graph g;
+
+    private int[] visited;
+
+    private int ccCount = 0;
+}
