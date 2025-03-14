2f884174ce3edb2d0d31c694ada8a0bb3a6f66e6
qinlinj <justinqinlin@gmail.com>
2025-03-14 02:30:22 -0700

    feat(TwoVertexPath): add fields for graph representation and path tracking to enhance functionality

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/TwoVertexPath.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/TwoVertexPath.java
@@ -1,4 +1,17 @@
 package org.qinlinj.nonlinear.graph.dfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.*;
+
 public class TwoVertexPath {
+    private Graph g;
+
+    private int source;
+    private int target;
+
+    private boolean[] visited;
+    private int[] prevs;
+
+    private List<Integer> res;
 }
