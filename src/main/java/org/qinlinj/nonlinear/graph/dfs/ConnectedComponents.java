0125fe3108eccdb92c68ec1aef715971825f6d5b
qinlinj <justinqinlin@gmail.com>
2025-03-13 23:33:53 -0700

    feat(ConnectedComponents): implement constructor to initialize graph and count connected components chore(ConnectedComponents): add placeholder for dfs method to prepare for depth-first search implementation

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/ConnectedComponents.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/ConnectedComponents.java
@@ -4,8 +4,27 @@ import org.qinlinj.nonlinear.graph.Graph;
 
 public class ConnectedComponents {
     private Graph g;
-    
+
     private boolean[] visited;
 
     private int ccCount = 0;
+
+    public ConnectedComponents(Graph g) {
+        this.g = g;
+
+        if (g == null) return;
+
+        this.visited = new boolean[g.getV()];
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) {
+                ccCount++;
+                dfs(v);
+            }
+        }
+    }
+
+    private void dfs(int v) {
+    }
+
+
 }
