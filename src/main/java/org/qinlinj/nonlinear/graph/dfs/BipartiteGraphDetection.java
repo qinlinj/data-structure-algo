9b3a2ad3a670b46ad8b2bb40c445f9253f24d410
qinlinj <justinqinlin@gmail.com>
2025-03-14 00:08:30 -0700

    feat(BipartiteGraphDetection): implement initial structure for bipartite graph detection with graph representation and state variables

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/BipartiteGraphDetection.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/BipartiteGraphDetection.java
@@ -1,4 +1,14 @@
 package org.qinlinj.nonlinear.graph.dfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
 public class BipartiteGraphDetection {
+    private Graph g;
+    private boolean[] visited;
+    private int[] colors;
+    private boolean isBipartition = true;
+
+    public BipartiteGraphDetection(Graph g) {
+        this.g = g;
+    }
 }
