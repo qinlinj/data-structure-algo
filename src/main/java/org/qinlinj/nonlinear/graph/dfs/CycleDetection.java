68962c4ba97ec482abcbd3ec5524dc627b956080
qinlinj <justinqinlin@gmail.com>
2025-03-13 23:48:37 -0700

    feat(CycleDetection): add CycleDetection class with graph initialization and cycle detection state variables to support cycle detection in graphs

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/CycleDetection.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/CycleDetection.java
@@ -1,4 +1,13 @@
 package org.qinlinj.nonlinear.graph.dfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
 public class CycleDetection {
+    private Graph g;
+    private boolean[] visited;
+    private boolean hasCycle = false;
+
+    public CycleDetection(Graph g) {
+        this.g = g;
+    }
 }
