83395b5e2464453933035c333918f4af86a9caaa
qinlinj <justinqinlin@gmail.com>
2025-03-14 00:08:53 -0700

    feat(BipartiteGraphDetection): implement DFS for bipartite graph detection by adding color assignment and visited tracking
    This change introduces a depth-first search (DFS) algorithm to detect if a graph is bipartite. It initializes the visited array and colors, ensuring proper traversal and color assignment during the detection process.

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/BipartiteGraphDetection.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/BipartiteGraphDetection.java
@@ -2,6 +2,8 @@ package org.qinlinj.nonlinear.graph.dfs;
 
 import org.qinlinj.nonlinear.graph.Graph;
 
+import java.util.*;
+
 public class BipartiteGraphDetection {
     private Graph g;
     private boolean[] visited;
@@ -10,5 +12,20 @@ public class BipartiteGraphDetection {
 
     public BipartiteGraphDetection(Graph g) {
         this.g = g;
+
+        this.visited = new boolean[g.getV()];
+        this.colors = new int[g.getV()];
+        Arrays.fill(colors, -1);
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) {
+                if (!dfs(v, 0)) {
+                    isBipartition = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    private boolean dfs(int v, int i) {
     }
 }
