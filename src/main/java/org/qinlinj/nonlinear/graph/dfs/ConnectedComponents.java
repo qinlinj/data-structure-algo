3d7c114a6d183de369f14c5f9978f21826320f32
qinlinj <justinqinlin@gmail.com>
2025-03-13 23:33:21 -0700

    feat(ConnectedComponents.java): add fields for graph representation and tracking visited nodes to support connected components functionality

--- src/main/java/org/qinlinj/nonlinear/graph/dfs/ConnectedComponents.java
+++ src/main/java/org/qinlinj/nonlinear/graph/dfs/ConnectedComponents.java
@@ -1,4 +1,11 @@
 package org.qinlinj.nonlinear.graph.dfs;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
 public class ConnectedComponents {
+    private Graph g;
+    
+    private boolean[] visited;
+
+    private int ccCount = 0;
 }
