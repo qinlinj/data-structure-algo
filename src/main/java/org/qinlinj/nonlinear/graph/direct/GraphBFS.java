afcb70c7a415384c700f341f221a16dfe34db6fb
qinlinj <justinqinlin@gmail.com>
2025-03-14 23:53:05 -0700

    feat(GraphBFS): implement constructor to initialize graph traversal and visited nodes
    This change adds a constructor to the GraphBFS class that initializes the graph, the visited array, and starts the breadth-first search (BFS) traversal for all vertices. This enhances the functionality of the class by ensuring that the BFS algorithm is executed upon instantiation.

--- src/main/java/org/qinlinj/nonlinear/graph/direct/GraphBFS.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/GraphBFS.java
@@ -8,5 +8,18 @@ public class GraphBFS {
     private Graph g;
     private boolean[] visited;
     private List<Integer> res;
-    
+
+    public GraphBFS(Graph g) {
+        this.g = g;
+
+        this.visited = new boolean[g.getV()];
+        this.res = new ArrayList<>();
+
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) bfs(v);
+        }
+    }
+
+    private void bfs(int v) {
+    }
 }
