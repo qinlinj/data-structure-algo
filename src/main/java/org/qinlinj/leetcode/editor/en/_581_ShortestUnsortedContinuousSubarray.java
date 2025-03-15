c65e899fd5c88b236cef1f6e3c47786ff7f392cc
qinlinj <justinqinlin@gmail.com>
2025-03-14 23:49:58 -0700

    feat: add implementation for finding the shortest unsorted continuous subarray
    This commit introduces a new Java class that provides a solution to the problem of identifying the shortest continuous subarray that needs to be sorted for the entire array to be sorted. The class includes a main method for testing and a placeholder for the solution logic.

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/_581_ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,78 @@
+package org.qinlinj.leetcode.editor.en;
+
+import java.util.*;
+import org.qinlinj.leetcode.editor.common.*;
+// [581] Shortest Unsorted Continuous Subarray
+public class _581_ShortestUnsortedContinuousSubarray {
+    
+    public static void main(String[] args) {
+        Solution solution = new _581_ShortestUnsortedContinuousSubarray().new Solution();
+        // put your test code here
+        
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findUnsortedSubarray(int[] nums) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
+
+
+
+
+
+//Given an integer array nums, you need to find one continuous subarray such 
+//that if you only sort this subarray in non-decreasing order, then the whole array 
+//will be sorted in non-decreasing order. 
+//
+// Return the shortest such subarray and output its length. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,6,4,8,10,9,15]
+//Output: 5
+//Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the 
+//whole array sorted in ascending order.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [1,2,3,4]
+//Output: 0
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [1]
+//Output: 0
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁵ <= nums[i] <= 10⁵ 
+// 
+//
+// 
+//Follow up: Can you solve it in 
+//O(n) time complexity?
+//
+// Related TopicsArray | Two Pointers | Stack | Greedy | Sorting | Monotonic 
+//Stack 
+//
+// 👍 7842, 👎 268bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
