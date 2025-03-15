e2729389a2fdbeb4b2421597e407184c6155462c
qinlinj <justinqinlin@gmail.com>
2025-03-14 23:52:55 -0700

    feat(GraphBFS.java): add fields for graph representation, visited nodes, and result list to support BFS algorithm implementation

--- src/main/java/org/qinlinj/nonlinear/graph/direct/GraphBFS.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/GraphBFS.java
@@ -1,4 +1,12 @@
 package org.qinlinj.nonlinear.graph.direct;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.*;
+
 public class GraphBFS {
+    private Graph g;
+    private boolean[] visited;
+    private List<Integer> res;
+    
 }
