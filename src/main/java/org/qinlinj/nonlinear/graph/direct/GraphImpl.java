2ba8cf7439c881a21ae7eb17da396983b1747307
qinlinj <justinqinlin@gmail.com>
2025-03-14 23:57:07 -0700

    feat(GraphImpl.java): implement Graph interface in GraphImpl class to adhere to the graph structure contract feat(GraphImpl.java): add methods to GraphImpl class for edge and vertex operations to enhance graph functionality

--- src/main/java/org/qinlinj/nonlinear/graph/direct/GraphImpl.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/GraphImpl.java
@@ -1,8 +1,10 @@
 package org.qinlinj.nonlinear.graph.direct;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
 import java.util.*;
 
-public class GraphImpl {
+public class GraphImpl implements Graph {
     private int V;
     private int E;
     private TreeSet<Integer>[] adj;
@@ -11,4 +13,62 @@ public class GraphImpl {
 
     private int[] indegrees;
     private int[] outdegrees;
+
+    public GraphImpl(int v) {
+        V = v;
+    }
+
+    /**
+     * Get the number of edges in the graph
+     *
+     * @return the number of edges
+     */
+    @Override
+    public int getE() {
+        return 0;
+    }
+
+    /**
+     * Get the number of vertices in the graph
+     *
+     * @return the number of vertices
+     */
+    @Override
+    public int getV() {
+        return 0;
+    }
+
+    /**
+     * Determine if there is an edge between two specified vertices
+     *
+     * @param v first vertex
+     * @param w second vertex
+     * @return true if an edge exists, false otherwise
+     */
+    @Override
+    public boolean hasEdge(int v, int w) {
+        return false;
+    }
+
+    /**
+     * Get all adjacent vertices of the specified vertex
+     *
+     * @param v the vertex
+     * @return a collection of adjacent vertices
+     */
+    @Override
+    public Collection<Integer> adj(int v) {
+        return List.of();
+    }
+
+    /**
+     * Get the degree of the specified vertex
+     *
+     * @param v the vertex
+     * @return the degree of the vertex
+     */
+    @Override
+    public int degree(int v) {
+        return 0;
+    }
 }
