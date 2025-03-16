a02c70f501240feca2713f980ce34ad6b9f9d4df
qinlinj <justinqinlin@gmail.com>
2025-03-15 23:33:22 -0700

    feat(ConnectedComponentsMaxCount): add Graph dependency and initialize fields for connected components calculation

--- src/main/java/org/qinlinj/nonlinear/graph/floodfill/ConnectedComponentsMaxCount.java
+++ src/main/java/org/qinlinj/nonlinear/graph/floodfill/ConnectedComponentsMaxCount.java
@@ -1,4 +1,14 @@
 package org.qinlinj.nonlinear.graph.floodfill;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
 public class ConnectedComponentsMaxCount {
+    private Graph g;
+
+    private boolean[] visited;
+    private int maxVertexNum = 0;
+
+    public ConnectedComponentsMaxCount(Graph g) {
+        this.g = g;
+    }
 }
