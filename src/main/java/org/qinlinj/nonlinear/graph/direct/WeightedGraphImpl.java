4e3046b204499a8e806c421f41032ca83c89d2d5
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:49:44 -0700

    feat(WeightedGraphImpl): add cycle detection and result storage to WeightedGraphImpl for enhanced graph functionality

--- src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
@@ -1,4 +1,20 @@
 package org.qinlinj.nonlinear.graph.direct;
 
 public class WeightedGraphImpl {
+    private GraphImpl g;
+
+    private int[] res;
+    private boolean hasCycle = false;
+
+    public WeightedGraphImpl(GraphImpl g) {
+        this.g = g;
+    }
+
+    public boolean isHasCycle() {
+        return hasCycle;
+    }
+
+    public int[] getRes() {
+        return res;
+    }
 }
