f73cadfbb5b09ba2187d81fd9b660a3059df4090
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:55:34 -0700

    feat(WeightedGraphImpl.java): add fields for vertices, edges, adjacency list, and degree arrays to enhance graph representation chore(WeightedGraphImpl.java): import java.util package to support data structures used in the graph implementation

--- src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
@@ -1,5 +1,15 @@
 package org.qinlinj.nonlinear.graph.direct;
 
+import java.util.*;
+
 public class WeightedGraphImpl {
+    private int V; //
+    private int E; //
+    private TreeMap<Integer, Integer>[] adj;
+
+    private boolean isDirected;
+
+    private int[] indegrees;
+    private int[] outdegrees;
 
 }
