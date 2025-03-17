211a696b1b28b0a7948efe2bcf1a8e8c29f052d9
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:50:10 -0700

    fix(WeightedGraphImpl): add validation to ensure the graph is directed to prevent incorrect usage feat(WeightedGraphImpl): initialize indegrees array to store the indegree of each vertex for cycle detection

--- src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
@@ -7,7 +7,15 @@ public class WeightedGraphImpl {
     private boolean hasCycle = false;
 
     public WeightedGraphImpl(GraphImpl g) {
+        if (!g.isDirected()) {
+            throw new IllegalArgumentException("error");
+        }
         this.g = g;
+
+        int[] indegrees = new int[g.getV()];
+        for (int v = 0; v < g.getV(); v++) {
+            indegrees[v] = g.indegree(v);
+        }
     }
 
     public boolean isHasCycle() {
