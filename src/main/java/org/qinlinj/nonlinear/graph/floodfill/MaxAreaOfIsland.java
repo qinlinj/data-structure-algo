de9cb6a548061159bfd83940dc122ff0ed620b0b
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:30:22 -0700

    feat(MaxAreaOfIsland.java): add graph representation and directions array to enhance flood fill algorithm functionality

--- src/main/java/org/qinlinj/nonlinear/graph/floodfill/MaxAreaOfIsland.java
+++ src/main/java/org/qinlinj/nonlinear/graph/floodfill/MaxAreaOfIsland.java
@@ -1,8 +1,13 @@
 package org.qinlinj.nonlinear.graph.floodfill;
 
+import java.util.*;
+
 public class MaxAreaOfIsland {
     private int rows;
     private int cols;
 
     private int[][] grid;
+
+    private Set<Integer>[] graph;
+    private int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 }
