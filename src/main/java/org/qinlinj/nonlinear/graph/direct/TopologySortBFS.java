a90bab23740498eef4c8da08ea107a796ecd3390
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:52:35 -0700

    feat(TopologySortBFS): implement topological sorting using BFS and cycle detection for directed graphs refactor(WeightedGraphImpl): remove redundant topological sorting logic and cycle detection from WeightedGraphImpl to improve code clarity and maintainability

--- src/main/java/org/qinlinj/nonlinear/graph/direct/TopologySortBFS.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/TopologySortBFS.java
@@ -1,4 +1,60 @@
 package org.qinlinj.nonlinear.graph.direct;
 
+import java.util.*;
+
 public class TopologySortBFS {
+    private GraphImpl g;
+
+    private int[] res;
+    private boolean hasCycle = false;
+
+    public TopologySortBFS(GraphImpl g) {
+        if (!g.isDirected()) {
+            throw new IllegalArgumentException("error");
+        }
+        this.g = g;
+
+        int[] indegrees = new int[g.getV()];
+        for (int v = 0; v < g.getV(); v++) {
+            indegrees[v] = g.indegree(v);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int v = 0; v < g.getV(); v++) {
+            if (indegrees[v] == 0) {
+                queue.add(v);
+            }
+        }
+
+        this.res = new int[g.getV()];
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int v = queue.remove();
+            res[index++] = v;
+            for (int w : g.adj(v)) {
+                indegrees[w]--;
+                if (indegrees[w] == 0) {
+                    queue.add(w);
+                }
+            }
+        }
+        if (index != g.getV()) {
+            hasCycle = true;
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphImpl g = new GraphImpl("data/directedgraph-dfs.txt", true);
+        TopologySortBFS bfs = new TopologySortBFS(g);
+        System.out.println(bfs.isHasCycle());
+        System.out.println(Arrays.toString(bfs.getRes()));
+    }
+
+    public boolean isHasCycle() {
+        return hasCycle;
+    }
+
+    public int[] getRes() {
+        return res;
+    }
 }
--- src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
@@ -1,53 +1,5 @@
 package org.qinlinj.nonlinear.graph.direct;
 
-import java.util.*;
-
 public class WeightedGraphImpl {
-    private GraphImpl g;
 
-    private int[] res;
-    private boolean hasCycle = false;
-
-    public WeightedGraphImpl(GraphImpl g) {
-        if (!g.isDirected()) {
-            throw new IllegalArgumentException("error");
-        }
-        this.g = g;
-
-        int[] indegrees = new int[g.getV()];
-        for (int v = 0; v < g.getV(); v++) {
-            indegrees[v] = g.indegree(v);
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int v = 0; v < g.getV(); v++) {
-            if (indegrees[v] == 0) {
-                queue.add(v);
-            }
-        }
-
-        this.res = new int[g.getV()];
-        int index = 0;
-        while (!queue.isEmpty()) {
-            int v = queue.remove();
-            res[index++] = v;
-            for (int w : g.adj(v)) {
-                indegrees[w]--;
-                if (indegrees[w] == 0) {
-                    queue.add(w);
-                }
-            }
-        }
-        if (index != g.getV()) {
-            hasCycle = true;
-        }
-    }
-
-    public boolean isHasCycle() {
-        return hasCycle;
-    }
-
-    public int[] getRes() {
-        return res;
-    }
 }
