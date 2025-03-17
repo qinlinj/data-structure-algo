bfc63638ab55b102df955c56d579dd2e1aca2701
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:47:06 -0700

    feat(CycleDetection.java): add CycleDetection class to implement cycle detection in directed graphs

---
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/CycleDetection.java
@@ -0,0 +1,12 @@
+package org.qinlinj.nonlinear.graph.direct;
+
+import org.qinlinj.nonlinear.graph.Graph;
+
+public class CycleDetection {
+    private Graph g;
+    private boolean hasCycle = false;
+
+    private boolean[] visited;
+    private boolean[] isOnPath;
+
+}
