12c5a9a70ee3f3a6f70c73ac83616c27bbffa3e2
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:33:49 -0700

    feat(MaxAreaOfIsland1.java): implement MaxAreaOfIsland1 class to calculate the maximum area of an island in a grid

---
+++ src/main/java/org/qinlinj/nonlinear/graph/floodfill/MaxAreaOfIsland1.java
@@ -0,0 +1,36 @@
+package org.qinlinj.nonlinear.graph.floodfill;
+
+import java.util.*;
+
+public class MaxAreaOfIsland1 {
+    private int rows;
+    private int cols;
+
+    private int[][] grid;
+
+    private Set<Integer>[] graph;
+    private int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+        };
+        MaxAreaOfIsland1 maxAreaOfIsland = new MaxAreaOfIsland1();
+        System.out.println(maxAreaOfIsland.maxAreaOfIsland(grid));
+    }
+
+    private boolean maxAreaOfIsland(int[][] grid) {
+    }
+
+    private boolean inArea(int row, int col) {
+        return row >= 0 && row < rows && col >= 0 && col < cols;
+    }
+}
