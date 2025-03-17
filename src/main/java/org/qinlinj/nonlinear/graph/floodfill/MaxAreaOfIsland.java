903fc351c9f48001ae17ee111af8d5cffecfddd6
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:30:48 -0700

    feat(MaxAreaOfIsland.java): implement initial structure for maxAreaOfIsland method to handle grid input and check for null or empty cases

--- src/main/java/org/qinlinj/nonlinear/graph/floodfill/MaxAreaOfIsland.java
+++ src/main/java/org/qinlinj/nonlinear/graph/floodfill/MaxAreaOfIsland.java
@@ -10,4 +10,20 @@ public class MaxAreaOfIsland {
 
     private Set<Integer>[] graph;
     private int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    private boolean[] visited;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null) return 0;
+
+        rows = grid.length;
+        if (rows == 0) return 0;
+
+        cols = grid[0].length;
+        if (cols == 0) return 0;
+
+        this.grid = grid;
+
+        return 0;
+    }
 }
