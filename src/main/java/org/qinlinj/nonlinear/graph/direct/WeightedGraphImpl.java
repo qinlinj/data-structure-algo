b45656560cadbfc588c6b8d2895351db070a382e
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:56:48 -0700

    feat(WeightedGraphImpl): add methods for degree, adjacency, and edge existence to enhance graph functionality

--- src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/WeightedGraphImpl.java
@@ -37,5 +37,38 @@ public class WeightedGraphImpl implements Graph {
     public int getE() {
         return E;
     }
-    
+
+    /**
+     * Get the degree of the specified vertex
+     *
+     * @param v the vertex
+     * @return the degree of the vertex
+     */
+    @Override
+    public int degree(int v) {
+        return 0;
+    }
+
+    /**
+     * Get all adjacent vertices of the specified vertex
+     *
+     * @param v the vertex
+     * @return a collection of adjacent vertices
+     */
+    @Override
+    public Collection<Integer> adj(int v) {
+        return List.of();
+    }
+
+    /**
+     * Determine if there is an edge between two specified vertices
+     *
+     * @param v first vertex
+     * @param w second vertex
+     * @return true if an edge exists, false otherwise
+     */
+    @Override
+    public boolean hasEdge(int v, int w) {
+        return false;
+    }
 }
