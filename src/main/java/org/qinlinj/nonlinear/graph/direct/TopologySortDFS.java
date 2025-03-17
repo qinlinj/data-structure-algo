6ba53f09a2333f224d21a1dcd9eb12c74ea60172
qinlinj <justinqinlin@gmail.com>
2025-03-16 23:53:13 -0700

    feat(TopologySortDFS): add fields and constructor to support topological sorting with cycle detection in a directed graph

--- src/main/java/org/qinlinj/nonlinear/graph/direct/TopologySortDFS.java
+++ src/main/java/org/qinlinj/nonlinear/graph/direct/TopologySortDFS.java
@@ -1,4 +1,18 @@
 package org.qinlinj.nonlinear.graph.direct;
 
+import org.qinlinj.nonlinear.graph.Graph;
+
 public class TopologySortDFS {
+    private Graph g;
+    private boolean hasCycle = false;
+
+    private boolean[] visited;
+    private boolean[] isOnPath;
+
+    private int[] res;
+    private int index;
+
+    public TopologySortDFS(Graph g) {
+        this.g = g;
+    }
 }
