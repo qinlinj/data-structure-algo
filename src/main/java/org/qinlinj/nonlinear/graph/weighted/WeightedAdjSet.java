f254ee08088ce3d02bed47e6f9c6681b43496680
qinlinj <justinqinlin@gmail.com>
2025-03-17 23:45:54 -0700

    feat(WeightedAdjSet): implement Graph interface in WeightedAdjSet class to define graph behavior and structure

--- src/main/java/org/qinlinj/nonlinear/graph/weighted/WeightedAdjSet.java
+++ src/main/java/org/qinlinj/nonlinear/graph/weighted/WeightedAdjSet.java
@@ -1,4 +1,32 @@
 package org.qinlinj.nonlinear.graph.weighted;
 
-public class WeightedAdjSet {
+import org.qinlinj.nonlinear.graph.Graph;
+
+import java.util.*;
+
+public class WeightedAdjSet implements Graph {
+    @Override
+    public int getE() {
+        return 0;
+    }
+
+    @Override
+    public int getV() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasEdge(int v, int w) {
+        return false;
+    }
+
+    @Override
+    public Collection<Integer> adj(int v) {
+        return List.of();
+    }
+
+    @Override
+    public int degree(int v) {
+        return 0;
+    }
 }
