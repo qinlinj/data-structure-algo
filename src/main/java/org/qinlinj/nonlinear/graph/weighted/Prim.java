7509603ad9196f6dc920b03bb8fad7ddc5bd7795
qinlinj <justinqinlin@gmail.com>
2025-03-17 23:54:59 -0700

    feat(Prim.java): add Prim class to implement Prim's algorithm for minimum spanning tree generation

---
+++ src/main/java/org/qinlinj/nonlinear/graph/weighted/Prim.java
@@ -0,0 +1,12 @@
+package org.qinlinj.nonlinear.graph.weighted;
+
+import java.util.*;
+
+public class Prim {
+    private WeightedAdjSet g;
+    private List<WeightedEdge> result;
+
+    public Prim(WeightedAdjSet g) {
+        this.g = g;
+    }
+}
