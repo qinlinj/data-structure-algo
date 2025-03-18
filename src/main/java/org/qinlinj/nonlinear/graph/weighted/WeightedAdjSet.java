bc70e1ec28571a32ad3e63884affdaa6c35313de
qinlinj <justinqinlin@gmail.com>
2025-03-17 23:48:00 -0700

    feat(WeightedAdjSet): add constructor to initialize graph from a file to enhance flexibility in graph creation

--- src/main/java/org/qinlinj/nonlinear/graph/weighted/WeightedAdjSet.java
+++ src/main/java/org/qinlinj/nonlinear/graph/weighted/WeightedAdjSet.java
@@ -2,6 +2,9 @@ package org.qinlinj.nonlinear.graph.weighted;
 
 import org.qinlinj.nonlinear.graph.Graph;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class WeightedAdjSet implements Graph {
@@ -9,8 +12,24 @@ public class WeightedAdjSet implements Graph {
     private int E;
     private TreeMap<Integer, Integer>[] adj;
 
-    public WeightedAdjSet(TreeMap<Integer, Integer>[] adj) {
-        this.adj = adj;
+    public WeightedAdjSet(String fileName) {
+        try {
+            BufferedReader reader
+                    = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            String[] arr = line.split(" ");
+            this.V = Integer.valueOf(arr[0]);
+            this.E = Integer.valueOf(arr[1]);
+
+            this.adj = new TreeMap[V];
+            for (int i = 0; i < V; i++) {
+                adj[i] = new TreeMap<>();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
