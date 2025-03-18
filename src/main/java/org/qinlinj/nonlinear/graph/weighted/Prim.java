fd263325e1e1ad4ed5ec52c2cdf9fe1e8af1a3a7
qinlinj <justinqinlin@gmail.com>
2025-03-17 23:56:19 -0700

    feat(Prim.java): integrate ConnectedComponentsAnalyzer to handle disconnected graphs in Prim's algorithm implementation

--- src/main/java/org/qinlinj/nonlinear/graph/weighted/Prim.java
+++ src/main/java/org/qinlinj/nonlinear/graph/weighted/Prim.java
@@ -1,5 +1,7 @@
 package org.qinlinj.nonlinear.graph.weighted;
 
+import org.qinlinj.nonlinear.graph.dfs.ConnectedComponentsAnalyzer;
+
 import java.util.*;
 
 public class Prim {
@@ -8,5 +10,12 @@ public class Prim {
 
     public Prim(WeightedAdjSet g) {
         this.g = g;
+        this.result = new ArrayList<>();
+
+        ConnectedComponentsAnalyzer cc = new ConnectedComponentsAnalyzer(g);
+        if (cc.getCcCount() > 1) return;
+
+        // Prim
+        boolean[] visited = new boolean[g.getV()];
     }
 }
