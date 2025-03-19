4bf0660f6f89d0a21bc633b12eb489da9df76dd9
qinlinj <justinqinlin@gmail.com>
2025-03-18 23:45:19 -0700

    feat(Prim1.java): add constructor and getter for result to enhance class usability and encapsulation

--- src/main/java/org/qinlinj/nonlinear/graph/weighted/Prim1.java
+++ src/main/java/org/qinlinj/nonlinear/graph/weighted/Prim1.java
@@ -5,4 +5,12 @@ import java.util.*;
 public class Prim1 {
     private WeightedAdjSet g;
     private List<WeightedEdge> result;
+
+    public Prim1(WeightedAdjSet g) {
+        this.g = g;
+    }
+
+    public List<WeightedEdge> getResult() {
+        return result;
+    }
 }
