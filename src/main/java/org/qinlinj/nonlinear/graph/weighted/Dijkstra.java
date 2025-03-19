731282c741c6d819bfbbf7003e466842b1be9361
qinlinj <justinqinlin@gmail.com>
2025-03-18 23:50:26 -0700

    feat(Dijkstra.java): add constructor to initialize graph and methods to get minimum distance and validate vertices for better functionality and error handling

--- src/main/java/org/qinlinj/nonlinear/graph/weighted/Dijkstra.java
+++ src/main/java/org/qinlinj/nonlinear/graph/weighted/Dijkstra.java
@@ -6,7 +6,22 @@ public class Dijkstra {
 
     private int[] distance;
     private boolean[] visited;
-    
+
+    public Dijkstra(WeightedAdjSet g) {
+        this.g = g;
+    }
+
     public static void main(String[] args) {
+    }
+
+    public int minDistanceTo(int v) {
+        validateVertex(v);
+        return distance[v];
+    }
+
+    public void validateVertex(int v) {
+        if (v < 0 || v >= g.getV()) {
+            throw new IllegalArgumentException(String.format("Vertex %d invalid", v));
+        }
     }
 }
