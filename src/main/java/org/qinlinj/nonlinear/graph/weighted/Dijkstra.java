c20475f9236d167dab707cb0833851a64d64a4d9
qinlinj <justinqinlin@gmail.com>
2025-03-18 23:47:51 -0700

    feat(Dijkstra.java): add fields for graph representation, source node, distance array, and visited array to support Dijkstra's algorithm implementation

--- src/main/java/org/qinlinj/nonlinear/graph/weighted/Dijkstra.java
+++ src/main/java/org/qinlinj/nonlinear/graph/weighted/Dijkstra.java
@@ -1,6 +1,12 @@
 package org.qinlinj.nonlinear.graph.weighted;
 
 public class Dijkstra {
+    private WeightedAdjSet g;
+    private int source;
+
+    private int[] distance;
+    private boolean[] visited;
+    
     public static void main(String[] args) {
     }
 }
