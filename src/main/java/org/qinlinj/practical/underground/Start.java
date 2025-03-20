8b9519307d94b3701747b612e7868ca8b3626bad
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:36:03 -0700

    feat(Start.java): add constructor and getters for station and startTime to enhance functionality and encapsulation

--- src/main/java/org/qinlinj/practical/underground/Start.java
+++ src/main/java/org/qinlinj/practical/underground/Start.java
@@ -1,4 +1,19 @@
 package org.qinlinj.practical.underground;
 
 public class Start {
+    private int startTime;
+    private String station;
+
+    public Start(String station, int time) {
+        this.station = station;
+        this.startTime = time;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
 }
