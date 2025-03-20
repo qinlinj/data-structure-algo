61c963184932dce86ea7744f17945cd2f9aa5d45
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:52:43 -0700

    feat(Leaderboard1.java): implement Leaderboard1 class to manage player scores and retrieve top scores

---
+++ src/main/java/org/qinlinj/practical/leaderboard/Leaderboard1.java
@@ -0,0 +1,60 @@
+package org.qinlinj.practical.leaderboard;
+
+import java.util.*;
+
+public class Leaderboard1 {
+    private Map<Integer, Integer> map;
+
+    public Leaderboard1() {
+        this.map = new HashMap<>();
+    }
+
+    public static void main(String[] args) {
+        Leaderboard1 leaderboard = new Leaderboard1();
+        leaderboard.addScore(1, 20);
+        leaderboard.addScore(2, 30);
+        leaderboard.addScore(3, 16);
+        leaderboard.addScore(4, 44);
+
+        System.out.println(leaderboard.top(2));
+
+        leaderboard.addScore(2, 34);
+        leaderboard.addScore(3, 23);
+
+        System.out.println(leaderboard.top(1));
+
+        leaderboard.reset(2);
+        leaderboard.reset(4);
+
+        System.out.println(leaderboard.top(1));
+    }
+
+    public void addScore(int playerId, int score) {
+        if (map.containsKey(playerId)) {
+            map.put(playerId, map.get(playerId) + score);
+        } else {
+            map.put(playerId, score);
+        }
+    }
+
+    public int top(int k) {
+        Integer[] scores = map.values().toArray(new Integer[map.values().size()]);
+        Arrays.sort(scores, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += scores[i];
+        }
+        return sum;
+    }
+
+    public void reset(int playerId) {
+        if (map.containsKey(playerId)) {
+            map.remove(playerId);
+        }
+    }
+}
