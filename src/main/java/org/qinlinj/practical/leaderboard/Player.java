4b74d736e0de0b00cb9acb17749ccf8363586e13
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:53:05 -0700

    feat(Player.java): add Player class fields and constructor to encapsulate player data

--- src/main/java/org/qinlinj/practical/leaderboard/Player.java
+++ src/main/java/org/qinlinj/practical/leaderboard/Player.java
@@ -1,4 +1,11 @@
 package org.qinlinj.practical.leaderboard;
 
 public class Player {
+    private int id;
+    private int scores;
+
+    public Player(int id, int scores) {
+        this.id = id;
+        this.scores = scores;
+    }
 }
