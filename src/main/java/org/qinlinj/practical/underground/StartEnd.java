98bd2da2d3fff29ba80a4339b31f792cd8a30916
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:37:36 -0700

    feat(StartEnd.java): add constructor to initialize start and end fields for better object creation feat(StartEnd.java): implement equals method to allow proper comparison of StartEnd objects

--- src/main/java/org/qinlinj/practical/underground/StartEnd.java
+++ src/main/java/org/qinlinj/practical/underground/StartEnd.java
@@ -4,11 +4,21 @@ public class StartEnd {
     private String start;
     private String end;
 
+    public StartEnd(String start, String end) {
+        this.start = start;
+        this.end = end;
+    }
+
     public String getStart() {
         return start;
     }
 
     public String getEnd() {
         return end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
     }
 }
