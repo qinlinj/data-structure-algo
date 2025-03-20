9bea307a6ee7e509828bb7e2530ccef3340865ac
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:50:04 -0700

    feat(Leaderboard.java): add a map to store leaderboard data and initialize it in the constructor to prepare for future leaderboard functionality

--- src/main/java/org/qinlinj/practical/leaderboard/Leaderboard.java
+++ src/main/java/org/qinlinj/practical/leaderboard/Leaderboard.java
@@ -1,4 +1,11 @@
 package org.qinlinj.practical.leaderboard;
 
+import java.util.*;
+
 public class Leaderboard {
+    private Map<Integer, Integer> map;
+
+    public Leaderboard() {
+        this.map = new HashMap<>();
+    }
 }
