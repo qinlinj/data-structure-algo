d307a01779a2498e0c1edf9a841ceb7db4d8102f
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:35:12 -0700

    feat(StartEnd.java): add start and end fields with their respective getters to enhance functionality and data encapsulation

--- src/main/java/org/qinlinj/practical/underground/StartEnd.java
+++ src/main/java/org/qinlinj/practical/underground/StartEnd.java
@@ -1,4 +1,14 @@
 package org.qinlinj.practical.underground;
 
 public class StartEnd {
+    private String start;
+    private String end;
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
 }
