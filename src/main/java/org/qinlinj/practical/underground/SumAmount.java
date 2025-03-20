dfda775bcaea549c7d6975eee9f7e1cee8538633
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:45:13 -0700

    feat(SumAmount.java): add constructor to initialize sum and amount fields for better object instantiation and encapsulation

--- src/main/java/org/qinlinj/practical/underground/SumAmount.java
+++ src/main/java/org/qinlinj/practical/underground/SumAmount.java
@@ -1,4 +1,11 @@
 package org.qinlinj.practical.underground;
 
 public class SumAmount {
+    private int sum;
+    private int amount;
+
+    public SumAmount(int sum, int amount) {
+        this.sum = sum;
+        this.amount = amount;
+    }
 }
