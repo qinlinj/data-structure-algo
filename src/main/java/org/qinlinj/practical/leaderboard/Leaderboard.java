09982ba9e9206c9ee1944f951890be9f86d01de3
qinlinj <justinqinlin@gmail.com>
2025-03-19 23:50:51 -0700

    feat(Leaderboard.java): add main method and stub methods for top and reset to establish structure for leaderboard functionality

--- src/main/java/org/qinlinj/practical/leaderboard/Leaderboard.java
+++ src/main/java/org/qinlinj/practical/leaderboard/Leaderboard.java
@@ -9,11 +9,23 @@ public class Leaderboard {
         this.map = new HashMap<>();
     }
 
+    public static void main(String[] args) {
+
+    }
+
     public void addScore(int playerId, int score) {
         if (map.containsKey(playerId)) {
             map.put(playerId, map.get(playerId) + score);
         } else {
             map.put(playerId, score);
         }
+    }
+
+    public int top(int k) {
+
+    }
+
+    public void reset(int playerId) {
+
     }
 }
