7ef729e84249531127dc2d963b6e095578c617e4
qinlinj <justinqinlin@gmail.com>
2025-03-19 21:18:38 -0700

    feat(UndergroundSystem): implement initial structure for UndergroundSystem class with checkIn, checkOut, and getAverageTime methods to support underground system functionality

--- src/main/java/org/qinlinj/practical/underground/UndergroundSystem.java
+++ src/main/java/org/qinlinj/practical/underground/UndergroundSystem.java
@@ -1,4 +1,19 @@
 package org.qinlinj.practical.underground;
 
 public class UndergroundSystem {
+    public UndergroundSystem() {
+    }
+
+    public void checkIn(int id, String stationName, int t) {
+
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+
+    }
+
 }
