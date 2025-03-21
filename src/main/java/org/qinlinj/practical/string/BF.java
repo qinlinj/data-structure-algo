96db71d9140a53b22c63b45104b7e5004908a10f
qinlinj <justinqinlin@gmail.com>
2025-03-20 23:15:18 -0700

    feat(BF.java): rename class from BF to BF1 and add main method for testing indexOf functionality

--- src/main/java/org/qinlinj/practical/string/BF.java
+++ src/main/java/org/qinlinj/practical/string/BF.java
@@ -1,4 +1,15 @@
 package org.qinlinj.practical.string;
 
-public class BF {
+public class BF1 {
+    public static void main(String[] args) {
+        BF1 b = new BF1();
+        String mainStr = "this is your code";
+        String patternStr = "your";
+
+        System.out.println(b.indexOf(mainStr, patternStr));
+    }
+
+    public int indexOf(String mainStr, String pattern) {
+
+    }
 }
