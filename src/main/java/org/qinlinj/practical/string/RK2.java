712d3aee23a9a85a37cafc4c7acd4bbc3f69f352
qinlinj <justinqinlin@gmail.com>
2025-03-20 23:26:37 -0700

    feat(RK2.java): implement Rabin-Karp string search algorithm to find pattern in a given string

---
+++ src/main/java/org/qinlinj/practical/string/RK2.java
@@ -0,0 +1,39 @@
+package org.qinlinj.practical.string;
+
+public class RK2 {
+
+    public static void main(String[] args) {
+        RK2 b = new RK2();
+        String mainStr = "this is your code";
+        String patternStr = "your";
+
+        System.out.println(b.indexOf(mainStr, patternStr));
+    }
+
+    private int indexOf(String mainStr, String pattern) {
+        if (mainStr == null || pattern == null) return -1;
+
+        int m = mainStr.length();
+        int n = pattern.length();
+        if (m < n) return -1;
+
+        int[] hashCodes = new int[m - n + 1];
+        for (int i = 0; i < m - n + 1; i++) {
+            hashCodes[i] = calHashCode(mainStr.substring(i, i + n));
+        }
+
+        int hashCode = calHashCode(pattern);
+
+        for (int i = 0; i < m - n + 1; i++) {
+            if (hashCode == hashCodes[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private int calHashCode(String str) {
+        return str.hashCode(); // O(n)
+    }
+}
