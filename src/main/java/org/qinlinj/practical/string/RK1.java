cd3d7a74889f1cb28e43c46c41988519e09f14de
qinlinj <justinqinlin@gmail.com>
2025-03-20 23:25:09 -0700

    feat(RK1.java): implement initial setup for the Rabin-Karp string search algorithm by adding hash code calculation and handling edge cases

--- src/main/java/org/qinlinj/practical/string/RK1.java
+++ src/main/java/org/qinlinj/practical/string/RK1.java
@@ -13,5 +13,17 @@ public class RK1 {
     private int indexOf(String mainStr, String pattern) {
         if (mainStr == null || pattern == null) return -1;
 
+        int m = mainStr.length();
+        int n = pattern.length();
+        if (m < n) return -1;
+
+        int[] hashCodes = new int[m - n + 1];
+        for (int i = 0; i < m - n + 1; i++) {
+            hashCodes[i] = calHashCode(mainStr.substring(i, i + n));
+        }
+    }
+
+    private int calHashCode(String str) {
+        return 0;
     }
 }
