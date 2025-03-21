194e105abb4206cc5d89def209ec49e117014f03
qinlinj <justinqinlin@gmail.com>
2025-03-20 23:15:38 -0700

    feat(BF.java): add null checks and length validation in indexOf method to enhance robustness and prevent potential errors

--- src/main/java/org/qinlinj/practical/string/BF.java
+++ src/main/java/org/qinlinj/practical/string/BF.java
@@ -10,6 +10,19 @@ public class BF1 {
     }
 
     public int indexOf(String mainStr, String pattern) {
+        if (mainStr == null || pattern == null) return -1;
 
+        int m = mainStr.length();
+        int n = pattern.length();
+        if (m < n) return -1;
+
+        for (int i = 0; i < m; i++) {
+            int k = i;
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+
+        return -1;
     }
 }
