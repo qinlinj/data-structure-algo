c1aaead21b9332efe641f49f344039344517a500
qinlinj <justinqinlin@gmail.com>
2025-03-21 20:24:00 -0700

    fix(KMP.java): add null checks for mainStr and pattern in indexOf method to prevent NullPointerException fix(KMP.java): add length checks for mainStr and pattern in indexOf method to handle edge cases

--- src/main/java/org/qinlinj/practical/string/KMP.java
+++ src/main/java/org/qinlinj/practical/string/KMP.java
@@ -2,11 +2,17 @@ package org.qinlinj.practical.string;
 
 public class KMP {
     public static void main(String[] args) {
-        
+
     }
 
     public int indexOf(String mainStr, String pattern) {
+        if (mainStr == null || pattern == null) return -1;
 
+        int m = mainStr.length();
+        int n = pattern.length();
+        if (m < n) return -1;
+
+        return -1;
     }
 
     private int[] getNext(char[] pattern) { // O(n^3)
