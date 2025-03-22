8d194283602d0981c28644c0f5a2e6c171a8ce0c
qinlinj <justinqinlin@gmail.com>
2025-03-21 20:21:00 -0700

    feat(KMP.java): add main method and indexOf method for KMP algorithm implementation feat(KMP.java): add getNext method to compute the next array for pattern matching

--- src/main/java/org/qinlinj/practical/string/KMP.java
+++ src/main/java/org/qinlinj/practical/string/KMP.java
@@ -1,4 +1,15 @@
 package org.qinlinj.practical.string;
 
 public class KMP {
+    public static void main(String[] args) {
+        
+    }
+
+    public int indexOf(String mainStr, String pattern) {
+
+    }
+
+    private int[] getNext(char[] pattern) { // O(n^3)
+
+    }
 }
