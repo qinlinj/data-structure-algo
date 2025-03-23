ffa84e7274ff98aa391877c19a5d478576fc743c
qinlinj <justinqinlin@gmail.com>
2025-03-23 00:00:07 -0700

    chore(StringTest.java): add patternStr variable to prepare for future string operations

--- src/main/java/org/qinlinj/practical/string/StringTest.java
+++ src/main/java/org/qinlinj/practical/string/StringTest.java
@@ -3,5 +3,6 @@ package org.qinlinj.practical.string;
 public class StringTest {
     public static void main(String[] args) {
         String mainStr = "justin, shake your code";
+        String patternStr = "your";
     }
 }
