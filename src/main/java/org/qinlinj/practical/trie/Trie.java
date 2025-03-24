fc169e7081d60fa0d8e1cdf8b4fc992aabf18b27
qinlinj <justinqinlin@gmail.com>
2025-03-23 22:22:44 -0700

    feat(Trie.java): add constructor to initialize the root node for the Trie data structure to ensure proper instantiation of the Trie object

--- src/main/java/org/qinlinj/practical/trie/Trie.java
+++ src/main/java/org/qinlinj/practical/trie/Trie.java
@@ -3,6 +3,12 @@ package org.qinlinj.practical.trie;
 import java.util.*;
 
 public class Trie {
+    private Node root;
+
+    public Trie() {
+        this.root = new Node();
+    }
+
     private class Node {
         Map<Character, Node> children;
         boolean isWord;
