f00316ef98e891b8677faa9756af4373239a8333
qinlinj <justinqinlin@gmail.com>
2025-03-23 22:17:13 -0700

    feat(KMP3.java): implement indexOf method to find the index of a pattern in a main string with null checks and length validation

--- src/main/java/org/qinlinj/practical/string/KMP3.java
+++ src/main/java/org/qinlinj/practical/string/KMP3.java
@@ -1,4 +1,13 @@
 package org.qinlinj.practical.string;
 
 public class KMP3 {
+    public int indexOf(String mainStr, String pattern) {
+        if (mainStr == null || pattern == null) return -1;
+
+        int m = mainStr.length();
+        int n = pattern.length();
+        if (m < n) return -1;
+
+        return -1;
+    }
 }
