f9279b2c2790405ab6de3ebfda413f4dbe7d258f
qinlinj <justinqinlin@gmail.com>
2025-03-23 22:14:35 -0700

    feat(KMP2.java): implement indexOf method for KMP string matching algorithm to find the index of a pattern in a main string

--- src/main/java/org/qinlinj/practical/string/KMP2.java
+++ src/main/java/org/qinlinj/practical/string/KMP2.java
@@ -1,4 +1,33 @@
 package org.qinlinj.practical.string;
 
 public class KMP2 {
+    public int indexOf(String mainStr, String pattern) {
+        if (mainStr == null || pattern == null) return -1;
+
+        int m = mainStr.length();
+        int n = pattern.length();
+        if (m < n) return -1;
+
+        int[] next = getNext(pattern.toCharArray());
+
+        int j = 0;
+        for (int i = 0; i < m; i++) {
+            while (j > 0 && mainStr.charAt(i) != pattern.charAt(j)) {
+                j = next[j - 1] + 1;
+            }
+
+            if (mainStr.charAt(i) == pattern.charAt(j)) {
+                j++;
+            }
+
+            if (j == n) {
+                return i - n + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private int[] getNext(char[] charArray) {
+    }
 }
