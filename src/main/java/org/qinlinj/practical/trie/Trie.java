54cd6c95c268e1ad2cb3154727deb026b2b4b1c6
qinlinj <justinqinlin@gmail.com>
2025-03-23 22:22:32 -0700

    feat(Trie.java): add internal Node class to represent trie nodes and manage children and word status

--- src/main/java/org/qinlinj/practical/trie/Trie.java
+++ src/main/java/org/qinlinj/practical/trie/Trie.java
@@ -1,4 +1,15 @@
 package org.qinlinj.practical.trie;
 
+import java.util.*;
+
 public class Trie {
+    private class Node {
+        Map<Character, Node> children;
+        boolean isWord;
+
+        Node() {
+            children = new HashMap<>();
+            isWord = false;
+        }
+    }
 }
