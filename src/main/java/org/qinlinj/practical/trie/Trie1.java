da2dac3bbbc299e7eba45499ea44921b6f58f2ab
qinlinj <justinqinlin@gmail.com>
2025-03-24 20:33:00 -0700

    feat(Trie1.java): add Node inner class to represent trie nodes with children, word status, and character storage

--- src/main/java/org/qinlinj/practical/trie/Trie1.java
+++ src/main/java/org/qinlinj/practical/trie/Trie1.java
@@ -1,4 +1,12 @@
 package org.qinlinj.practical.trie;
 
+import java.util.*;
+
 public class Trie1 {
+    private class Node {
+        List<Node> children;
+        boolean isWord;
+        private Character c;
+        
+    }
 }
