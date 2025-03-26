d532b0ff17960aa058a8516ba01a8d83bb67956c
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:33:33 -0700

    feat(FIFOCache.java): add constructor to initialize cache capacity for FIFO cache implementation

--- src/main/java/org/qinlinj/practical/cache/FIFOCache.java
+++ src/main/java/org/qinlinj/practical/cache/FIFOCache.java
@@ -1,10 +1,17 @@
 package org.qinlinj.practical.cache;
 
+import java.util.*;
+
 public class FIFOCache<K, V> implements Cache<K, V> {
     private Map<K, V> cache;
     private Queue<K> queue;
     private int capacity;
-    
+
+    public FIFOCache(int capacity) {
+
+        this.capacity = capacity;
+    }
+
     @Override
     public V get(K key) {
         return null;
