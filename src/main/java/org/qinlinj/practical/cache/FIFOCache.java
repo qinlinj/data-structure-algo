e0c63b4bb74e6afbf468b1bd0cad94e2e50608b1
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:33:07 -0700

    feat(FIFOCache.java): add cache, queue, and capacity fields to FIFOCache class to support FIFO caching mechanism

--- src/main/java/org/qinlinj/practical/cache/FIFOCache.java
+++ src/main/java/org/qinlinj/practical/cache/FIFOCache.java
@@ -1,6 +1,10 @@
 package org.qinlinj.practical.cache;
 
 public class FIFOCache<K, V> implements Cache<K, V> {
+    private Map<K, V> cache;
+    private Queue<K> queue;
+    private int capacity;
+    
     @Override
     public V get(K key) {
         return null;
