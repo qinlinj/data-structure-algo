d5c8b8f5474a3c67a39d73783bd9ed87974624bb
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:36:32 -0700

    feat(LocalCache.java): implement a local cache using HashMap to store key-value pairs for improved data retrieval

--- src/main/java/org/qinlinj/practical/cache/LocalCache.java
+++ src/main/java/org/qinlinj/practical/cache/LocalCache.java
@@ -1,6 +1,14 @@
 package org.qinlinj.practical.cache;
 
+import java.util.*;
+
 public class LocalCache<K, V> implements Cache<K, V> {
+    private Map<K, V> cache;
+
+    public LocalCache() {
+        cache = new HashMap<>();
+    }
+
     @Override
     public V get(K key) {
         return null;
