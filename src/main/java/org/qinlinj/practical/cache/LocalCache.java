2655a82d2ff0665506217db27a83d5729aedffd0
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:36:10 -0700

    feat(LocalCache.java): implement Cache interface in LocalCache class to provide caching functionality

--- src/main/java/org/qinlinj/practical/cache/LocalCache.java
+++ src/main/java/org/qinlinj/practical/cache/LocalCache.java
@@ -1,4 +1,13 @@
 package org.qinlinj.practical.cache;
 
-public class LocalCache {
+public class LocalCache<K, V> implements Cache<K, V> {
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
 }
