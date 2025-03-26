4eeff44ff51b43bf98131cba47a72bf56a5c1ebf
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:22:41 -0700

    feat(BitMapTest.java): add initial BitMapTest class with main method for future bitmap testing functionality

---
+++ src/main/java/org/qinlinj/practical/bitmap/BitMapTest.java
@@ -0,0 +1,12 @@
+package org.qinlinj.practical.bitmap;
+
+import java.util.*;
+
+public class BitMapTest {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+    }
+
+}
