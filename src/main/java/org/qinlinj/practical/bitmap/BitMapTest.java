8c241affb806dbd1312e3da4732a69ee7a101bac
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:22:51 -0700

    feat(BitMapTest): add data array initialization and random number generation for testing purposes

--- src/main/java/org/qinlinj/practical/bitmap/BitMapTest.java
+++ src/main/java/org/qinlinj/practical/bitmap/BitMapTest.java
@@ -6,7 +6,12 @@ public class BitMapTest {
     private static Random random = new Random();
 
     public static void main(String[] args) {
+        int[] data = new int[10_000_000];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = random.nextInt(100_000_000);
+        }
 
+        int target = data[8888];
     }
 
 }
