dd22208cb03fe62e363f4b02ef5702e63600235c
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:32:59 -0700

    feat(FIFOCache.java): implement generic FIFO cache class with get and put methods to provide basic cache functionality

--- src/main/java/org/qinlinj/practical/cache/FIFOCache.java
+++ src/main/java/org/qinlinj/practical/cache/FIFOCache.java
@@ -1,4 +1,13 @@
 package org.qinlinj.practical.cache;
 
-public class FIFOCache {
+public class FIFOCache<K, V> implements Cache<K, V> {
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
 }
