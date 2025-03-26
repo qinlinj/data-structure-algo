1377d4627f860de850fb8e00a4af8f75681ae90d
qinlinj <justinqinlin@gmail.com>
2025-03-25 22:19:26 -0700

    feat(BitMap.java): add constructor to initialize BitMap with a specified number of bits to enhance usability and flexibility

--- src/main/java/org/qinlinj/practical/bitmap/BitMap.java
+++ src/main/java/org/qinlinj/practical/bitmap/BitMap.java
@@ -3,4 +3,9 @@ package org.qinlinj.practical.bitmap;
 public class BitMap {
     private byte[] bytes;
     private int nBits;
+
+    public BitMap(int nBits) {
+        this.nBits = nBits;
+        this.bytes = new byte[nBits / 8 + 1];
+    }
 }
