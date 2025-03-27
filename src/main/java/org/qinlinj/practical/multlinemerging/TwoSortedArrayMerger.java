96ef8e8cca583e711a28803af8bce8f1153bfdf9
qinlinj <justinqinlin@gmail.com>
2025-03-26 23:58:16 -0700

    feat(TwoSortedArrayMerger): initialize variables for merging two sorted arrays to prepare for implementation

--- src/main/java/org/qinlinj/practical/multlinemerging/TwoSortedArrayMerger.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/TwoSortedArrayMerger.java
@@ -4,6 +4,10 @@ public class TwoSortedArrayMerger {
     public int[] mergeTwoSortedArray(int[] a, int[] b) {
         int m = a.length, n = b.length;
         int[] res = new int[m + n];
+        int i = 0, j = 0;
+        int index = 0;
+
+        return res;
     }
 
 }
