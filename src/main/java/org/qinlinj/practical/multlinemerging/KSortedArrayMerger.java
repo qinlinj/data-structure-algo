3881dd41c923d99df3ea591aea62ae60a7cadf11
qinlinj <justinqinlin@gmail.com>
2025-03-26 23:59:46 -0700

    feat(KSortedArrayMerger): calculate total length of input arrays to prepare for merging process

--- src/main/java/org/qinlinj/practical/multlinemerging/KSortedArrayMerger.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/KSortedArrayMerger.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class KSortedArrayMerger {
     public int[] mergeKSortedArray(List<int[]> data, int k) {
-
+        int len = 0;
+        for (int i = 0; i < k; i++) {
+            len += data.get(i).length;
+        }
     }
 }
