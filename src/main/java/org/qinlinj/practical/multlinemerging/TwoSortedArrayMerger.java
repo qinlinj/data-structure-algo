765d9b3ba1db52fa50acd6cdadb9aa06c4f48f14
qinlinj <justinqinlin@gmail.com>
2025-03-26 23:59:12 -0700

    fix(TwoSortedArrayMerger.java): implement merging logic for two sorted arrays to handle edge cases where one array is exhausted before the other

--- src/main/java/org/qinlinj/practical/multlinemerging/TwoSortedArrayMerger.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/TwoSortedArrayMerger.java
@@ -7,7 +7,15 @@ public class TwoSortedArrayMerger {
         int i = 0, j = 0;
         int index = 0;
         for (int k = 0; k < res.length; k++) {
-
+            if (i == m) {
+                res[index++] = b[j++];
+            } else if (j == n) {
+                res[index++] = a[i++];
+            } else if (a[i] < b[j]) {
+                res[index++] = a[i++];
+            } else {
+                res[index++] = b[j++];
+            }
         }
         return res;
     }
