0f7c7431bfc546bb3baf0b187219998664413ff3
qinlinj <justinqinlin@gmail.com>
2025-03-27 00:06:33 -0700

    feat(FileIOUtils.java): add utility methods for reading and writing files to enhance file handling capabilities

--- src/main/java/org/qinlinj/practical/multlinemerging/FileIOUtils.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/FileIOUtils.java
@@ -1,4 +1,26 @@
 package org.qinlinj.practical.multlinemerging;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Reader;
+import java.io.Writer;
+
 public class FileIOUtils {
+    public static BufferedReader getReader(String name) {
+
+    }
+
+    public static BufferedWriter getWriter(String name) {
+
+    }
+
+    public static void closeReader(Reader reader) {
+
+    }
+
+    public static void closeWriter(Writer writer) {
+
+
+    }
+
 }
