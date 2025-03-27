7eb5f38540fe8cd837c7a9ba20e54c91793f4cae
qinlinj <justinqinlin@gmail.com>
2025-03-27 00:02:53 -0700

    fix(KSortedArrayMerger.java): update comparator logic in minHeap to handle tie-breaking for equal values by comparing indices

--- src/main/java/org/qinlinj/practical/multlinemerging/KSortedArrayMerger.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/KSortedArrayMerger.java
@@ -14,7 +14,7 @@ public class KSortedArrayMerger {
         PriorityQueue<int[]> minHeap = new PriorityQueue<>(k, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-
+                return o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1];
             }
         });
 
