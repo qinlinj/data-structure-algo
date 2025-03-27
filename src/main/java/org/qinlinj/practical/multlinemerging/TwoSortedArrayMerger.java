be437acabfaffd02f684f67033aae142902f2a53
qinlinj <justinqinlin@gmail.com>
2025-03-26 23:58:00 -0700

    feat(TwoSortedArrayMerger): initialize result array and store lengths of input arrays to prepare for merging logic

--- src/main/java/org/qinlinj/practical/multlinemerging/TwoSortedArrayMerger.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/TwoSortedArrayMerger.java
@@ -2,7 +2,8 @@ package org.qinlinj.practical.multlinemerging;
 
 public class TwoSortedArrayMerger {
     public int[] mergeTwoSortedArray(int[] a, int[] b) {
-
+        int m = a.length, n = b.length;
+        int[] res = new int[m + n];
     }
 
 }
