ebbce355eb36e3e13abd1558ce7497fe99aeea3d
qinlinj <justinqinlin@gmail.com>
2025-03-30 23:52:11 -0700

    feat(LRUCache): implement basic structure for LRUCache with capacity and cache map to enhance functionality

--- src/main/java/org/qinlinj/practical/cache/LRUCache.java
+++ src/main/java/org/qinlinj/practical/cache/LRUCache.java
@@ -1,6 +1,14 @@
 package org.qinlinj.practical.cache;
 
+import java.util.*;
+
 public class LRUCache<K, V> implements Cache<K, V> {
+    private Map<K, Node> cache;
+    private int capacity;
+
+    private Node head;
+    private Node tail;
+
     @Override
     public V get(K key) {
         return null;
