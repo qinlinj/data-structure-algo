b227567aab8c522f97ad2a04b508224676f92b6b
qinlinj <justinqinlin@gmail.com>
2025-03-30 23:51:58 -0700

    feat(LRUCache.java): implement Cache interface in LRUCache class to ensure compliance with caching standards and provide required methods for get and put operations

--- src/main/java/org/qinlinj/practical/cache/LRUCache.java
+++ src/main/java/org/qinlinj/practical/cache/LRUCache.java
@@ -1,6 +1,16 @@
 package org.qinlinj.practical.cache;
 
-public class LRUCache<K, V> {
+public class LRUCache<K, V> implements Cache<K, V> {
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
     private class Node {
         K key;
         V value;
