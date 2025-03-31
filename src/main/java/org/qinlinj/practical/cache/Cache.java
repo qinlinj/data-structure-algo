f88107f0212facf7f1f1d441b6529d9f2eb7e968
qinlinj <justinqinlin@gmail.com>
2025-03-30 23:58:49 -0700

    docs(Cache.java): add Javadoc comments to the put method to clarify its behavior and usage in the cache interface

--- src/main/java/org/qinlinj/practical/cache/Cache.java
+++ src/main/java/org/qinlinj/practical/cache/Cache.java
@@ -19,6 +19,22 @@ package org.qinlinj.practical.cache;
 public interface Cache<K, V> {
 
     V get(K key);
-    
+
+    /**
+     * Associates the specified value with the specified key in the cache.
+     * <p>
+     * If the cache previously contained a value for the key, the old value
+     * is replaced. This operation may trigger eviction of other entries
+     * according to the cache's eviction policy if the cache is at capacity.
+     * <p>
+     * This operation is typically expected to be fast (O(1) in most implementations)
+     * and may have cache-specific side effects such as:
+     * - Triggering eviction of other entries
+     * - Updating statistics
+     * - Persisting values to a backing store
+     *
+     * @param key   the key with which the specified value is to be associated
+     * @param value the value to be associated with the specified key
+     */
     void put(K key, V value);
 }
