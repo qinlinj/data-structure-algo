b3e8f6e95d08cd94e8217fcb95b167c73e59bf1c
qinlinj <justinqinlin@gmail.com>
2025-03-30 23:57:08 -0700

    feat(LRUCacheLinkedHashMapImp): add main method for testing LRUCache functionality and demonstrate usage

--- src/main/java/org/qinlinj/practical/cache/LRUCacheLinkedHashMapImp.java
+++ src/main/java/org/qinlinj/practical/cache/LRUCacheLinkedHashMapImp.java
@@ -9,7 +9,20 @@ public class LRUCacheLinkedHashMapImp<K, V> extends LinkedHashMap<K, V> {
         super(initialCapacity, loadFactor, true);
         this.capacity = capacity;
     }
-    
+
+    public static void main(String[] args) {
+        LRUCacheLinkedHashMapImp<Integer, Integer> cache =
+                new LRUCacheLinkedHashMapImp<>(3, 3, 0.75F);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.put(3, 3);
+        cache.put(4, 4);
+        System.out.println(cache.get(3));
+        cache.put(2, 5);
+        cache.put(5, 6);
+        System.out.println(cache.get(4));
+    }
+
     @Override
     protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
         if (size() > capacity) {
