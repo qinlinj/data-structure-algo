94fe9cf41827a5d82d03a5f321a785aab7cb9e49
qinlinj <justinqinlin@gmail.com>
2025-03-30 23:50:38 -0700

    feat(LinkedHashSetTest): add main method to demonstrate LinkedHashSet functionality and uniqueness of elements

--- src/main/java/org/qinlinj/practical/cache/LinkedHashSetTest.java
+++ src/main/java/org/qinlinj/practical/cache/LinkedHashSetTest.java
@@ -1,4 +1,15 @@
 package org.qinlinj.practical.cache;
 
+import java.util.*;
+
 public class LinkedHashSetTest {
+    public static void main(String[] args) {
+        Set<Integer> set = new LinkedHashSet<>();
+        set.add(3);
+        set.add(1);
+        set.add(10);
+        set.add(1);
+        set.add(3);
+        System.out.println(set);
+    }
 }
