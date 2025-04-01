eba80eaca8d2612fda68e02dcfb750b90be3edab
qinlinj <justinqinlin@gmail.com>
2025-03-31 23:52:53 -0700

    feat(LFUCache): implement LFUCache structure with necessary fields for caching functionality
    This change introduces the foundational structure for the LFUCache class, including maps for cache storage, usage count tracking, and capacity management. This setup is essential for implementing the least frequently used caching algorithm effectively.

--- src/main/java/org/qinlinj/practical/cache/LFUCache.java
+++ src/main/java/org/qinlinj/practical/cache/LFUCache.java
@@ -1,6 +1,18 @@
 package org.qinlinj.practical.cache;
 
+import java.util.*;
+
 public class LFUCache<K, V> implements Cache<K, V> {
+    private Map<K, V> cache;
+
+    private Map<K, Integer> keyToUsedCount;
+
+    private Map<Integer, LinkedHashSet<K>> usedCountToKeys;
+
+    private int capacity;
+
+    private int minUsedCount;
+
     @Override
     public V get(K key) {
         return null;
