bc9e31256c0cff9ece16af596da1213afb569563
qinlinj <justinqinlin@gmail.com>
2025-03-31 23:52:35 -0700

    feat(LFUCache): implement generic LFUCache class with get and put methods to provide basic cache functionality

--- src/main/java/org/qinlinj/practical/cache/LFUCache.java
+++ src/main/java/org/qinlinj/practical/cache/LFUCache.java
@@ -1,4 +1,13 @@
 package org.qinlinj.practical.cache;
 
-public class LFUCache {
+public class LFUCache<K, V> implements Cache<K, V> {
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
 }
