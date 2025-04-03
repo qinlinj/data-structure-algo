29dd9b9a52300698514f5a71ca6237d9b84c9489
qinlinj <justinqinlin@gmail.com>
2025-04-02 23:43:53 -0700

    refactor(trie): optimize Trie implementation by changing children from Map to array for improved performance and memory usage
    This change enhances the efficiency of the Trie data structure by using a fixed-size array for child nodes, which reduces overhead and improves access time.

--- src/main/java/org/qinlinj/practical/trie/_208_TrieCharImp_ImplementTriePrefixTree.java
+++ src/main/java/org/qinlinj/practical/trie/_208_TrieCharImp_ImplementTriePrefixTree.java
@@ -1,7 +1,5 @@
 package org.qinlinj.practical.trie;
 
-import java.util.*;
-
 // @formatter:off
 public class _208_TrieCharImp_ImplementTriePrefixTree {
     private Node root;
@@ -11,44 +9,23 @@ public class _208_TrieCharImp_ImplementTriePrefixTree {
     }
 
     public void insert(String word) {
-        Node curr = root;
-        for (Character c : word.toCharArray()) { // O(n)
-            if (!curr.children.containsKey(c)) {
-                curr.children.put(c, new Node());
-            }
-            curr = curr.children.get(c);
-        }
-        curr.isWord = true;
+
     }
 
     public boolean search(String word) {
-        Node curr = root;
-        for (Character c : word.toCharArray()) { // O(n)
-            if (!curr.children.containsKey(c)) {
-                return false;
-            }
-            curr = curr.children.get(c);
-        }
-        return curr.isWord;
+
     }
 
     public boolean startsWith(String prefix) {
-        Node curr = root;
-        for (Character c : prefix.toCharArray()) { // O(n)
-            if (!curr.children.containsKey(c)) {
-                return false;
-            }
-            curr = curr.children.get(c);
-        }
-        return true;
+
     }
 
     private class Node {
-        Map<Character, Node> children;
+        Node[] children;
         boolean isWord;
 
         Node() {
-            children = new HashMap<>();
+            children = new Node[26];
             isWord = false;
         }
     }
