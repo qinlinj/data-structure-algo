9021f9eaeb845703ab2cb7f642f7f4e00da76d6d
qinlinj <justinqinlin@gmail.com>
2025-04-02 23:51:50 -0700

    feat(AutocompleteSystem.java): implement sentence filtering based on current input to enhance autocomplete functionality

--- src/main/java/org/qinlinj/practical/trie/_642_HashMapImp_AutocompleteSystem.java
+++ src/main/java/org/qinlinj/practical/trie/_642_HashMapImp_AutocompleteSystem.java
@@ -21,6 +21,13 @@ public class _642_HashMapImp_AutocompleteSystem {
             currSentence = "";
         } else {
             currSentence += c;
+
+            List<SentenceInfo> list = new ArrayList<>();
+            for (String sentence : map.keySet()) { // O(n*k)
+                if (sentence.startsWith(currSentence)) {
+                    list.add(new SentenceInfo(sentence, map.get(sentence)));
+                }
+            }
             
         }
         return res;
