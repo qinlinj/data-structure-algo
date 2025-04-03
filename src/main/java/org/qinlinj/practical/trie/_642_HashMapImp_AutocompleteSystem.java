94668bcdc5b7c7aac66311426babaef98cffaa56
qinlinj <justinqinlin@gmail.com>
2025-04-02 23:51:02 -0700

    feat(AutocompleteSystem): initialize map and currSentence in constructor to store sentences and their frequencies
    This change allows the autocomplete system to maintain a mapping of sentences to their usage frequencies, enhancing its functionality.

--- src/main/java/org/qinlinj/practical/trie/_642_HashMapImp_AutocompleteSystem.java
+++ src/main/java/org/qinlinj/practical/trie/_642_HashMapImp_AutocompleteSystem.java
@@ -1,7 +1,17 @@
 package org.qinlinj.practical.trie;
 
+import java.util.*;
+
 public class _642_HashMapImp_AutocompleteSystem {
-    public _642_HashMapImp_AutocompleteSystem() {
+    private Map<String, Integer> map;
+
+    private String currSentence = "";
+
+    public _642_HashMapImp_AutocompleteSystem(String[] sentences, int[] times) {
+        map = new HashMap<>();
+        for (int i = 0; i < sentences.length; i++) {
+            map.put(sentences[i], times[i]);
+        }
     }
 
     private class SentenceInfo {
