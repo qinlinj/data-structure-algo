b0e2e8843122a30560368c9e4e873c5e80924944
qinlinj <justinqinlin@gmail.com>
2025-04-02 23:50:22 -0700

    feat(autocomplete): add constructor and SentenceInfo class to manage sentence data and usage frequency in autocomplete system

--- src/main/java/org/qinlinj/practical/trie/_642_HashMapImp_AutocompleteSystem.java
+++ src/main/java/org/qinlinj/practical/trie/_642_HashMapImp_AutocompleteSystem.java
@@ -1,4 +1,16 @@
 package org.qinlinj.practical.trie;
 
 public class _642_HashMapImp_AutocompleteSystem {
+    public _642_HashMapImp_AutocompleteSystem() {
+    }
+
+    private class SentenceInfo {
+        String content;
+        int time;
+
+        SentenceInfo(String content, int time) {
+            this.content = content;
+            this.time = time;
+        }
+    }
 }
