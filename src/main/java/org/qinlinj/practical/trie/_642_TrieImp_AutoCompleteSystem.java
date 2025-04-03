6341f1267b2b6f650770e5d6eafb0153912ef651
qinlinj <justinqinlin@gmail.com>
2025-04-02 23:53:01 -0700

    feat(TrieImp_AutoCompleteSystem): add constructor and SentenceInfo class to enhance functionality and structure of the autocomplete system

--- src/main/java/org/qinlinj/practical/trie/_642_TrieImp_AutoCompleteSystem.java
+++ src/main/java/org/qinlinj/practical/trie/_642_TrieImp_AutoCompleteSystem.java
@@ -1,4 +1,16 @@
 package org.qinlinj.practical.trie;
 
 public class _642_TrieImp_AutoCompleteSystem {
+    public _642_TrieImp_AutoCompleteSystem() {
+    }
+
+    private class SentenceInfo {
+        String content;
+        int time;
+
+        SentenceInfo(String content, int time) {
+            this.content = content;
+            this.time = time;
+        }
+    }
 }
