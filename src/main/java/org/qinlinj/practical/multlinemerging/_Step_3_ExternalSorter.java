95d37be51871f21d0e1590b2144b0024efbbbbc4
qinlinj <justinqinlin@gmail.com>
2025-04-03 23:55:37 -0700

    feat(ExternalSorter): implement mergeSort method to sort files in a directory using a min-heap for efficiency

--- src/main/java/org/qinlinj/practical/multlinemerging/_Step_3_ExternalSorter.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/_Step_3_ExternalSorter.java
@@ -1,4 +1,18 @@
 package org.qinlinj.practical.multlinemerging;
 
+import java.io.File;
+import java.util.*;
+
 public class _Step_3_ExternalSorter {
+    public void mergeSort(String dirName) throws Exception {
+
+        File dir = new File(dirName);
+        File[] children = dir.listFiles();
+
+        PriorityQueue<BufferedIterator> minHeap = new PriorityQueue<>(children.length, new Comparator<BufferedIterator>() {
+            @Override
+            public int compare(BufferedIterator o1, BufferedIterator o2) {
+                return o1.next().compareTo(o2.next());
+            }
+        });
 }
