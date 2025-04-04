e64318810d27cf0a390fbb5a886fc0560c6030ee
qinlinj <justinqinlin@gmail.com>
2025-04-03 23:45:41 -0700

    feat(WordsGenerator): implement a words generator that writes random words to a file
    This change introduces a new main method in the WordsGenerator class that generates random words and writes them to "data/top100/words.txt". The implementation uses a BufferedWriter for file operations and a Random instance to create variable-length words from a predefined character set. This functionality is added to facilitate testing and data generation for further processing.

--- src/main/java/org/qinlinj/practical/multlinemerging/_Step_0_WordsGenerator.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/_Step_0_WordsGenerator.java
@@ -1,4 +1,29 @@
 package org.qinlinj.practical.multlinemerging;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.*;
+
 public class _Step_0_WordsGenerator {
+    private static Random r = new Random();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter writer = FileIOUtils.getWriter("data/top100/words.txt");
+
+        char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+        int m = chars.length;
+
+        for (int i = 0; i < 10000; i++) {
+            StringBuilder line = new StringBuilder();
+            for (int j = 0; j < r.nextInt(16); j++) {
+                line.append(chars[r.nextInt(m)]);
+            }
+            if (line.length() == 0) continue;
+            writer.write(line.toString());
+            writer.newLine();
+        }
+
+        FileIOUtils.closeWriter(writer);
+    }
 }
