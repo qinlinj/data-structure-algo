3a7bbab2cc12ca36ce5b6c30ae5bee20b544e9b5
qinlinj <justinqinlin@gmail.com>
2025-04-03 23:59:21 -0700

    feat(_Step_4_Top_100_Words.java): implement top_100 method to find top 100 words using a min-heap for efficient counting and retrieval

--- src/main/java/org/qinlinj/practical/multlinemerging/_Step_4_Top_100_Words.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/_Step_4_Top_100_Words.java
@@ -1,4 +1,26 @@
 package org.qinlinj.practical.multlinemerging;
 
+import java.util.*;
+
 public class _Step_4_Top_100_Words {
+    public String[] top_100(String fileName) throws Exception {
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>(100, new Comparator<Pair>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                return o1.cnt - o2.cnt;
+            }
+        });
+
+    }
+
+    class Pair {
+        String word;
+        int cnt;
+
+        Pair(String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
+        }
+    }
+
 }
