d3294e85444bcfec4e739ebb9020fa8756c99b27
qinlinj <justinqinlin@gmail.com>
2025-04-03 23:56:26 -0700

    feat(ExternalSorter): add functionality to write sorted words to a file to persist results of the sorting process

--- src/main/java/org/qinlinj/practical/multlinemerging/_Step_3_ExternalSorter.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/_Step_3_ExternalSorter.java
@@ -1,6 +1,7 @@
 package org.qinlinj.practical.multlinemerging;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.util.*;
 
@@ -16,7 +17,7 @@ public class _Step_3_ExternalSorter {
                 return o1.next().compareTo(o2.next());
             }
         });
-        
+
         for (File file : children) {
             BufferedReader br = FileIOUtils.getReader(file.getName());
             BufferedIterator buf = new BufferedIterator(br);
@@ -26,5 +27,19 @@ public class _Step_3_ExternalSorter {
                 buf.close();
             }
         }
+
+        BufferedWriter bw = FileIOUtils.getWriter("data/top100/sorted_words.txt");
+        while (!minHeap.isEmpty()) {
+            BufferedIterator firstBuf = minHeap.poll();
+            bw.write(firstBuf.next());
+            bw.newLine();
+            if (firstBuf.hasNext()) {
+                minHeap.add(firstBuf);
+            } else {
+                firstBuf.close();
+            }
+        }
+
+        FileIOUtils.closeWriter(bw);
     }
 }
