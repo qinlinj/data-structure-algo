24f453b0e29ab86f372cd5d3ba8e351331a04766
qinlinj <justinqinlin@gmail.com>
2025-04-03 23:55:12 -0700

    feat(LittleFileSorter): add main method to enable direct execution of file sorting fix(LittleFileSorter): update file path retrieval to use getPath() for accurate file access

--- src/main/java/org/qinlinj/practical/multlinemerging/_Step_2_LittleFileSorter.java
+++ src/main/java/org/qinlinj/practical/multlinemerging/_Step_2_LittleFileSorter.java
@@ -7,12 +7,17 @@ import java.io.IOException;
 import java.util.*;
 
 public class _Step_2_LittleFileSorter {
+    public static void main(String[] args) throws IOException {
+        String dir = "data/top100/raw_data";
+        new _Step_2_LittleFileSorter().sortEachFile(dir);
+    }
+
     public void sortEachFile(String dirName) throws IOException {
         File dir = new File(dirName);
         File[] littleFiles = dir.listFiles();
 
         for (int i = 0; i < littleFiles.length; i++) {
-            BufferedReader br = FileIOUtils.getReader(littleFiles[i].getName());
+            BufferedReader br = FileIOUtils.getReader(littleFiles[i].getPath());
             List<String> words = new ArrayList<>();
             String line = null;
             while ((line = br.readLine()) != null) {
@@ -30,6 +35,5 @@ public class _Step_2_LittleFileSorter {
             }
             FileIOUtils.closeWriter(bw);
         }
-
     }
 }
