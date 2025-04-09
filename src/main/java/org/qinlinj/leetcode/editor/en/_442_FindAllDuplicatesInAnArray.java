0c29905e34e1f03f9b1a37aaf9023cc92dd1f784
qinlinj <justinqinlin@gmail.com>
2025-04-08 23:35:47 -0700

    feat: add _442_FindAllDuplicatesInAnArray class to implement a solution for finding duplicates in an array
    This commit introduces a new Java class that provides a structure for solving the problem of finding all duplicates in an integer array. The class includes a main method for testing and a placeholder for the solution method, which will be implemented to meet the problem's requirements.

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/_442_FindAllDuplicatesInAnArray.java
@@ -0,0 +1,65 @@
+package org.qinlinj.leetcode.editor.en;
+
+import java.util.*;
+import org.qinlinj.leetcode.editor.common.*;
+// [442] Find All Duplicates in an Array
+public class _442_FindAllDuplicatesInAnArray {
+    
+    public static void main(String[] args) {
+        Solution solution = new _442_FindAllDuplicatesInAnArray().new Solution();
+        // put your test code here
+        
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<Integer> findDuplicates(int[] nums) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
+
+
+
+
+
+//Given an integer array nums of length n where all the integers of nums are in 
+//the range [1, n] and each integer appears at most twice, return an array of all 
+//the integers that appears twice. 
+//
+// You must write an algorithm that runs in O(n) time and uses only constant 
+//auxiliary space, excluding the space needed to store the output 
+//
+// 
+// Example 1: 
+// Input: nums = [4,3,2,7,8,2,3,1]
+//Output: [2,3]
+// 
+// Example 2: 
+// Input: nums = [1,1,2]
+//Output: [1]
+// 
+// Example 3: 
+// Input: nums = [1]
+//Output: []
+// 
+// 
+// Constraints: 
+//
+// 
+// n == nums.length 
+// 1 <= n <= 10⁵ 
+// 1 <= nums[i] <= n 
+// Each element in nums appears once or twice. 
+// 
+//
+// Related TopicsArray | Hash Table 
+//
+// 👍 10734, 👎 422bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
