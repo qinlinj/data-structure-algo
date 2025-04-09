98367912fdbfd65736e9b0b024855c4f5f6979b4
qinlinj <justinqinlin@gmail.com>
2025-04-09 14:27:20 -0700

    feat: add initial implementation of Valid Palindrome problem in Java
    This commit introduces two new Java files for the Valid Palindrome problem.
    The first file contains a basic structure for the solution, while the second
    file includes a more detailed implementation with comments and examples.
    These changes aim to provide a foundation for solving the problem and
    facilitating further development and testing.

---
+++ src/main/java/org/qinlinj/algocamp/day1/_125_ValidPalindrome.java
@@ -0,0 +1,16 @@
+package org.qinlinj.algocamp.day1;
+
+public class _125_ValidPalindrome {
+    public static void main(String[] args) {
+        Solution solution = new _125_ValidPalindrome().new Solution();
+        // put your test code here
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isPalindrome(String s) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/org/qinlinj/leetcode/editor/en/_125_ValidPalindrome.java
@@ -0,0 +1,77 @@
+package org.qinlinj.leetcode.editor.en;
+
+import java.util.*;
+import org.qinlinj.leetcode.editor.common.*;
+// [125] Valid Palindrome
+public class _125_ValidPalindrome {
+    
+    public static void main(String[] args) {
+        Solution solution = new _125_ValidPalindrome().new Solution();
+        // put your test code here
+        
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isPalindrome(String s) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
+
+
+
+
+
+//A phrase is a palindrome if, after converting all uppercase letters into 
+//lowercase letters and removing all non-alphanumeric characters, it reads the same 
+//forward and backward. Alphanumeric characters include letters and numbers. 
+//
+// Given a string s, return true if it is a palindrome, or false otherwise. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "race a car"
+//Output: false
+//Explanation: "raceacar" is not a palindrome.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = " "
+//Output: true
+//Explanation: s is an empty string "" after removing non-alphanumeric 
+//characters.
+//Since an empty string reads the same forward and backward, it is a palindrome.
+//
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 2 * 10⁵ 
+// s consists only of printable ASCII characters. 
+// 
+//
+// Related TopicsTwo Pointers | String 
+//
+// 👍 10214, 👎 8529bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
