0b1fdc2f011ab48663a3604f246b1ca0bd861787
qinlinj <justinqinlin@gmail.com>
2025-04-09 13:22:27 -0700

    feat: add implementation for removing duplicates from a sorted array
    refactor: optimize removeDuplicates method to use a for loop instead of while loop for better readability and performance

--- src/main/java/org/qinlinj/algocamp/day1/_26_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/org/qinlinj/algocamp/day1/_26_RemoveDuplicatesFromSortedArray.java
@@ -1,28 +1,24 @@
-package org.qinlinj.leetcode.editor.en;
+package org.qinlinj.algocamp.day1;
 
-// [26] Remove Duplicates from Sorted Array
 public class _26_RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         Solution solution = new _26_RemoveDuplicatesFromSortedArray().new Solution();
-        int[] data = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        solution.removeDuplicates(data);
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int removeDuplicates(int[] nums) {
-            int slow = 0, fast = 1;
-            for (int i = 1; i < nums.length; i++) {
+            if (nums.length == 0) {
+                return 0;
+            }
+            int slow = 0, fast = 0;
+            while (fast < nums.length) {
                 if (nums[fast] != nums[slow]) {
                     slow++;
                     nums[slow] = nums[fast];
                 }
                 fast++;
             }
-//            System.out.println(Arrays.toString(nums));
             return slow + 1;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
-
 }
