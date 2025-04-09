d04a6e1b674125c176e5c5cf9312f36867e47677
qinlinj <justinqinlin@gmail.com>
2025-04-09 03:28:16 -0700

    feat: add _1002_FindCommonCharacters class to implement common character finder
    This commit introduces a new class that will be used to find common
    characters in an array of strings. The initial structure includes
    a main method for testing and a placeholder for the solution method.
    This sets the foundation for solving the problem as described in
    the provided comments and examples.

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/_1002_FindCommonCharacters.java
@@ -0,0 +1,55 @@
+package org.qinlinj.leetcode.editor.en;
+
+import java.util.*;
+
+// [1002] Find Common Characters
+public class _1002_FindCommonCharacters {
+
+    public static void main(String[] args) {
+        Solution solution = new _1002_FindCommonCharacters().new Solution();
+        // put your test code here
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<String> commonChars(String[] words) {
+
+            return null;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
+
+
+//Given a string array words, return an array of all characters that show up in
+//all strings within the words (including duplicates). You may return the answer 
+//in any order. 
+//
+// 
+// Example 1: 
+// Input: words = ["bella","label","roller"]
+//Output: ["e","l","l"]
+// 
+// Example 2: 
+// Input: words = ["cool","lock","cook"]
+//Output: ["c","o"]
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= words.length <= 100 
+// 1 <= words[i].length <= 100 
+// words[i] consists of lowercase English letters. 
+// 
+//
+// Related TopicsArray | Hash Table | String 
+//
+// 👍 4364, 👎 423bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
