0572c6a987bd6b9343d38b762171deee504aade7
qinlinj <justinqinlin@gmail.com>
2025-04-09 03:26:13 -0700

    refactor: improve findDisappearedNumbers method for better readability and performance by using absolute values and negation feat: add an alternative implementation findDisappearedNumbers1 for finding missing numbers in the array with a different approach

--- src/main/java/org/qinlinj/leetcode/editor/en/_448_FindAllNumbersDisappearedInAnArray.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/_448_FindAllNumbersDisappearedInAnArray.java
@@ -15,25 +15,42 @@ public class _448_FindAllNumbersDisappearedInAnArray {
     class Solution {
         public List<Integer> findDisappearedNumbers(int[] nums) {
             int n = nums.length;
+
             for (int i = 0; i < n; i++) {
-                int val = nums[i] - 1;
-                int index = val % n;
-                nums[index] += n;
+                int index = Math.abs(nums[i]) - 1;
+                if (nums[index] > 0) {
+                    nums[index] = -nums[index];
+                }
             }
+
             ArrayList<Integer> res = new ArrayList<>();
             for (int i = 0; i < n; i++) {
-                if (nums[i] <= n) {
+                if (nums[i] > 0) {
                     res.add(i + 1);
                 }
             }
+
             return res;
         }
     }
+    
     //leetcode submit region end(Prohibit modification and deletion)
 
-
+    public List<Integer> findDisappearedNumbers1(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int index = (nums[i] - 1 + n) % n;
+            nums[index] += n;
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
 }
-
 
 //Given an array nums of n integers where nums[i] is in the range [1, n],
 //return an array of all the integers in the range [1, n] that do not appear in nums. 
