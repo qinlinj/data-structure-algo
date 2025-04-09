351aed5e249f5e79ffb2df8a0f4e9aa9bfcea20d
qinlinj <justinqinlin@gmail.com>
2025-04-09 15:19:21 -0700

    feat(ProductOfArrayExceptSelf): implement productExceptSelf method to calculate the product of array elements except self
    feat(ProductOfArrayExceptSelf): add main method for testing the productExceptSelf functionality with sample data

--- src/main/java/org/qinlinj/algocamp/day1/_238_ProductOfArrayExceptSelf.java
+++ src/main/java/org/qinlinj/algocamp/day1/_238_ProductOfArrayExceptSelf.java
@@ -1,4 +1,28 @@
 package org.qinlinj.algocamp.day1;
 
 public class _238_ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+        Solution solution = new _238_ProductOfArrayExceptSelf().new Solution();
+        // put your test code here
+        int[] data = new int[]{-1, 1, 0, -3, 3};
+        solution.productExceptSelf(data);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int[] output = new int[nums.length];
+            output[0] = 1;
+            for (int i = 1; i < nums.length; i++) {
+                output[i] = nums[i - 1] * output[i - 1];
+            }
+
+            int currRightProduct = 1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                output[i] = currRightProduct * output[i];
+                currRightProduct = currRightProduct * nums[i];
+            }
+            return output;
+        }
+    }
 }
--- src/main/java/org/qinlinj/leetcode/editor/en/_238_ProductOfArrayExceptSelf.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/_238_ProductOfArrayExceptSelf.java
@@ -1,20 +1,30 @@
 package org.qinlinj.leetcode.editor.en;
 
-import java.util.*;
-import org.qinlinj.leetcode.editor.common.*;
 // [238] Product of Array Except Self
 public class _238_ProductOfArrayExceptSelf {
-    
+
     public static void main(String[] args) {
         Solution solution = new _238_ProductOfArrayExceptSelf().new Solution();
         // put your test code here
-        
+        int[] data = new int[]{-1, 1, 0, -3, 3};
+        solution.productExceptSelf(data);
     }
-    
+
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] productExceptSelf(int[] nums) {
-            
+            int[] output = new int[nums.length];
+            output[0] = 1;
+            for (int i = 1; i < nums.length; i++) {
+                output[i] = nums[i - 1] * output[i - 1];
+            }
+
+            int currRightProduct = 1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                output[i] = currRightProduct * output[i];
+                currRightProduct = currRightProduct * nums[i];
+            }
+            return output;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
@@ -23,10 +33,7 @@ public class _238_ProductOfArrayExceptSelf {
 }
 
 
-
-
-
-//Given an integer array nums, return an array answer such that answer[i] is 
+//Given an integer array nums, return an array answer such that answer[i] is
 //equal to the product of all the elements of nums except nums[i]. 
 //
 // The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit 
