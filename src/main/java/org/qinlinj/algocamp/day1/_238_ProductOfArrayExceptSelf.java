e863d6cfa657b7a25ece29d1b9141937b06a8e1f
qinlinj <justinqinlin@gmail.com>
2025-04-09 15:04:04 -0700

    feat: add initial implementation of Product of Array Except Self problem
    This commit introduces the initial structure for the
    Product of Array Except Self problem, including the main
    class and a placeholder for the solution method.
    This sets the foundation for implementing the algorithm
    that calculates the product of all elements except the
    current one without using division.

---
+++ src/main/java/org/qinlinj/algocamp/day1/_238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,4 @@
+package org.qinlinj.algocamp.day1;
+
+public class _238_ProductOfArrayExceptSelf {
+}
---
+++ src/main/java/org/qinlinj/leetcode/editor/en/_238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,67 @@
+package org.qinlinj.leetcode.editor.en;
+
+import java.util.*;
+import org.qinlinj.leetcode.editor.common.*;
+// [238] Product of Array Except Self
+public class _238_ProductOfArrayExceptSelf {
+    
+    public static void main(String[] args) {
+        Solution solution = new _238_ProductOfArrayExceptSelf().new Solution();
+        // put your test code here
+        
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
+
+
+
+
+
+//Given an integer array nums, return an array answer such that answer[i] is 
+//equal to the product of all the elements of nums except nums[i]. 
+//
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit 
+//integer. 
+//
+// You must write an algorithm that runs in O(n) time and without using the 
+//division operation. 
+//
+// 
+// Example 1: 
+// Input: nums = [1,2,3,4]
+//Output: [24,12,8,6]
+// 
+// Example 2: 
+// Input: nums = [-1,1,0,-3,3]
+//Output: [0,0,9,0,0]
+// 
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁵ 
+// -30 <= nums[i] <= 30 
+// The input is generated such that answer[i] is guaranteed to fit in a 32-bit 
+//integer. 
+// 
+//
+// 
+// Follow up: Can you solve the problem in O(1) extra space complexity? (The 
+//output array does not count as extra space for space complexity analysis.) 
+//
+// Related TopicsArray | Prefix Sum 
+//
+// 👍 23944, 👎 1538bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
