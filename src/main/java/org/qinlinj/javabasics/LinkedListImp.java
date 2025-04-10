8d73bd4d5e2ac9f651fff36db8f57ac12cd2d9bb
qinlinj <justinqinlin@gmail.com>
2025-04-09 17:51:04 -0700

    feat(ArrayListUtil): add ArrayList utility class with various operations to demonstrate ArrayList functionalities chore(LinkedListImp): remove empty LinkedListImp class as it serves no purpose feat(LinkedListUtil): add LinkedList utility class to initialize and display a LinkedList with elements

--- src/main/java/org/qinlinj/javabasics/LinkedListImp.java
+++
@@ -1,7 +0,0 @@
-package org.qinlinj.javabasics;
-
-public class LinkedListImp {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ src/main/java/org/qinlinj/javabasics/LinkedListUtil.java
@@ -0,0 +1,14 @@
+package org.qinlinj.javabasics;
+
+import java.util.*;
+
+public class LinkedListImp {
+    public static void main(String[] args) {
+        // init LinkedList with specific element
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1, 2, 3));
+        System.out.println(list);
+
+
+
+    }
+}
