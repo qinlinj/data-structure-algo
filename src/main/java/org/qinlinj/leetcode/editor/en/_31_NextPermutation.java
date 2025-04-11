0eda7119e3b82676d681baec6fa9a71ecd74fb66
qinlinj <justinqinlin@gmail.com>
2025-04-11 01:53:48 -0700

    feat: implement next permutation algorithm with detailed steps and helper methods for clarity and maintainability

--- src/main/java/org/qinlinj/leetcode/editor/en/_31_NextPermutation.java
+++ src/main/java/org/qinlinj/leetcode/editor/en/_31_NextPermutation.java
@@ -1,20 +1,47 @@
 package org.qinlinj.leetcode.editor.en;
 
-import java.util.*;
-import org.qinlinj.leetcode.editor.common.*;
 // [31] Next Permutation
 public class _31_NextPermutation {
-    
+
     public static void main(String[] args) {
         Solution solution = new _31_NextPermutation().new Solution();
         // put your test code here
-        
+
     }
-    
+
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void nextPermutation(int[] nums) {
-            
+            // Step 1: Find the first pair of elements in ascending order from back to front
+            int i = nums.length - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1]) {
+                i--;
+            }
+
+            if (i >= 0) {
+                // Step 2: Find the first element greater than nums[i] from the back
+                int j = nums.length - 1;
+                while (j >= 0 && nums[j] <= nums[i]) {
+                    j--;
+                }
+
+                // Step 3：swap these two elements
+                swap(nums, i, j);
+            }
+
+            reverse(nums, i + 1, nums.length - 1);
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+
+        private void reverse(int[] nums, int start, int end) {
+            while (start < end) {
+                swap(nums, start++, end--);
+            }
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
@@ -23,10 +50,7 @@ public class _31_NextPermutation {
 }
 
 
-
-
-
-//A permutation of an array of integers is an arrangement of its members into a 
+//A permutation of an array of integers is an arrangement of its members into a
 //sequence or linear order. 
 //
 // 
