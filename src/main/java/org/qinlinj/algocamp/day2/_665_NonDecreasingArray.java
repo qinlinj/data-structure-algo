73bbbfd3f8f43cd9eadf39e1e0093308fb39531e
qinlinj <justinqinlin@gmail.com>
2025-04-10 23:59:39 -0700

    feat(NonDecreasingArray): implement checkPossibility method to determine if array can be made non-decreasing by modifying at most one element feat(NonDecreasingArray): add main method for testing the Solution class functionality

--- src/main/java/org/qinlinj/algocamp/day2/_665_NonDecreasingArray.java
+++ src/main/java/org/qinlinj/algocamp/day2/_665_NonDecreasingArray.java
@@ -1,4 +1,29 @@
 package org.qinlinj.algocamp.day2;
 
 public class _665_NonDecreasingArray {
+    public static void main(String[] args) {
+        Solution solution = new _665_NonDecreasingArray().new Solution();
+        // put your test code here
+
+    }
+    
+    class Solution {
+        public boolean checkPossibility(int[] nums) {
+            int count = 0;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[i - 1]) {
+                    count++;
+                    if (count > 1) {
+                        return false;
+                    }
+                    if (i > 1 && nums[i] < nums[i - 2]) {
+                        nums[i] = nums[i - 1];
+                    } else {
+                        nums[i - 1] = nums[i];
+                    }
+                }
+            }
+            return true;
+        }
+    }
 }
