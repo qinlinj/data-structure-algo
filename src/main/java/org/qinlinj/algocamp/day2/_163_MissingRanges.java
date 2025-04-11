cf09e86a32a315d70a738f799b32fd98a449c33c
qinlinj <justinqinlin@gmail.com>
2025-04-11 01:24:36 -0700

    feat(MissingRanges): implement findMissingRanges method to identify missing ranges in a sorted array
    This method enhances the functionality of the class by providing a way to find
    missing ranges between a lower and upper bound in a given sorted array.

--- src/main/java/org/qinlinj/algocamp/day2/_163_MissingRanges.java
+++ src/main/java/org/qinlinj/algocamp/day2/_163_MissingRanges.java
@@ -1,4 +1,34 @@
 package org.qinlinj.algocamp.day2;
 
+import java.util.*;
+
 public class _163_MissingRanges {
+    class Solution {
+        public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
+            List<List<Integer>> lists = new ArrayList<>();
+            if (nums.length == 0) {
+                lists.add(Arrays.asList(lower, upper));
+                return lists;
+            }
+
+            if (nums[0] > lower) {
+                lists.add(Arrays.asList(lower, nums[0] - 1));
+            }
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == nums[i - 1]) {
+                    continue;
+                }
+
+                if (nums[i] > nums[i - 1] + 1) {
+                    lists.add(Arrays.asList(nums[i - 1] + 1, nums[i] - 1));
+                }
+            }
+
+            if (nums[nums.length - 1] < upper) {
+                lists.add(Arrays.asList(nums[nums.length - 1] + 1, upper));
+            }
+
+            return lists;
+        }
 }
