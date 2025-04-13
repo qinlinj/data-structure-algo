3a5b3fed79646df7dbddcd69e9ca42c13083f453
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:21:59 -0700

    feat(HastMapUtil.java): initialize a HashMap in the main method to demonstrate basic usage of HashMap in Java

--- src/main/java/org/qinlinj/javabasics/HastMapUtil.java
+++ src/main/java/org/qinlinj/javabasics/HastMapUtil.java
@@ -1,7 +1,13 @@
 package org.qinlinj.javabasics;
 
+import java.util.*;
+
 public class HastMapUtil {
     public static void main(String[] args) {
-        
+        // init a HashMap
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
     }
 }
