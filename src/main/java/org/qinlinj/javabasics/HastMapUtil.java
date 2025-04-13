0aceaec943f32d61f78091a45b0b12b8b2b50e21
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:32:48 -0700

    feat(HastMapUtil.java): add methods to insert, remove, and check keys in the hashmap to enhance functionality and usability

--- src/main/java/org/qinlinj/javabasics/HastMapUtil.java
+++ src/main/java/org/qinlinj/javabasics/HastMapUtil.java
@@ -33,5 +33,19 @@ public class HastMapUtil {
         // get the value of the specific key, or returns default value if it does not exist
         System.out.println(map.getOrDefault(2, "Default Value"));
         System.out.println(map.getOrDefault(4, "Default Value"));
+
+        // insert a new key-value pair
+        map.put(10, "ten");
+
+        // remove an exist key-value pair by a specific key
+        map.remove(2);
+
+        // check if the hashmap has a specific key
+        if (map.containsKey(2)) {
+            System.out.println("Key 2 -> " + map.get(2));
+        } else {
+            System.out.println("Key 2 not found.");
+        }
+        
     }
 }
