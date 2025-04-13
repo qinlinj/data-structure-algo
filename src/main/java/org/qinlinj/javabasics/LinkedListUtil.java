87c7839842fb369f0f237fb01e2f2b0e1a209d41
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:02:43 -0700

    feat(LinkedListUtil): enhance LinkedListUtil with improved output messages and additional list operations
    refactor(LinkedListUtil): replace list.add() with list.addLast() for clarity and consistency in adding elements to the LinkedList

--- src/main/java/org/qinlinj/javabasics/LinkedListUtil.java
+++ src/main/java/org/qinlinj/javabasics/LinkedListUtil.java
@@ -9,10 +9,19 @@ public class LinkedListUtil {
         System.out.println(list);
 
         System.out.println(list.isEmpty());
-        System.out.println(list.size());
+        System.out.println("List size: " + list.size());
 
         // add an element to the end of the LinkedList
-        list.add(4);
-        
+        list.addLast(4);
+        // add an element to the top of the LinkedList
+        list.addLast(0);
+
+
+        // get the element at the end of the LinkedList
+        System.out.println("the element at the end of the LinkedList: " + list.get(list.size() - 1));
+        // remove the element at the end of the LinkedList
+        list.remove(list.size() - 1);
+
+        System.out.println(list);
     }
 }
