1a9d9918968533cebb64346bf3824b8c1f3bef2c
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:36:14 -0700

    feat(HastSetUtil.java): initialize and print a HashSet with sample values to demonstrate usage

--- src/main/java/org/qinlinj/javabasics/HastSetUtil.java
+++ src/main/java/org/qinlinj/javabasics/HastSetUtil.java
@@ -1,9 +1,13 @@
 package org.qinlinj.javabasics;
 
+import java.util.*;
+
 public class HastSetUtil {
     public static void main(String[] args) {
         // init a HashSet
-
+        HashSet<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(set);
+        
         // check if the HashSet is empty
 
         // get size
