2aeaae8405c2e33db252db69ab4ac93696c91584
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:35:18 -0700

    feat(HastSetUtil.java): add new utility class for HashSet operations to provide basic functionalities and examples for HashSet usage

---
+++ src/main/java/org/qinlinj/javabasics/HastSetUtil.java
@@ -0,0 +1,22 @@
+package org.qinlinj.javabasics;
+
+public class HastSetUtil {
+    public static void main(String[] args) {
+        // init a HashSet
+
+        // check if the HashSet is empty
+
+        // get size
+
+        // check if the HashSet has a specific key
+
+        // get the value of the specific key, or returns null if it does not exist
+
+        // add a new element
+
+        // remove an exist element
+
+        // traverse the HashSet
+
+    }
+}
