190c1738b2a8e87955371324ed5eeb1301a21a7b
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:16:53 -0700

    feat(StackUtil.java): add StackUtil class with basic stack operations as a starting point for stack implementation

---
+++ src/main/java/org/qinlinj/javabasics/StackUtil.java
@@ -0,0 +1,19 @@
+package org.qinlinj.javabasics;
+
+public class StackUtil {
+    public static void main(String[] args) {
+
+        // add element at the top of the stack
+
+
+        // check if the stack is empty
+
+        // get size
+
+        // remove the element at the top of the stack
+
+
+        // get the element at the top of the stack
+
+    }
+}
