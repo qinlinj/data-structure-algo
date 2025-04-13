df9cf22ca456f3cae05dd313a6e4bd96934b3370
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:12:28 -0700

    feat(QueueUtil): add main method to demonstrate basic Queue operations using LinkedList

--- src/main/java/org/qinlinj/javabasics/QueueUtil.java
+++ src/main/java/org/qinlinj/javabasics/QueueUtil.java
@@ -1,4 +1,16 @@
 package org.qinlinj.javabasics;
 
+import java.util.*;
+
 public class QueueUtil {
+    public static void main(String[] args) {
+        // common implementation classes are LinkedList and PriorityQueue
+        Queue<Integer> queue = new LinkedList<>();
+
+        // add element at the end of the queue
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(10);
+
+    }
 }
