0c5af9d51d0799b3c13ec3c634026bfdb4f7df6b
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:23:19 -0700

    docs(HastMapUtil.java): add comments and print statements to improve code clarity and demonstrate HashMap functionality

--- src/main/java/org/qinlinj/javabasics/HastMapUtil.java
+++ src/main/java/org/qinlinj/javabasics/HastMapUtil.java
@@ -6,8 +6,16 @@ public class HastMapUtil {
     public static void main(String[] args) {
         // init a HashMap
         HashMap<Integer, String> map = new HashMap<>();
+
+        // add data
         map.put(1, "one");
         map.put(2, "two");
         map.put(3, "three");
+        System.out.println(map);
+
+        // check if the hashmap is empty
+        System.out.println("is empty: " + map.isEmpty());
+        
+
     }
 }
