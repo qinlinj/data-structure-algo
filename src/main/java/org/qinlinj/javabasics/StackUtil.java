931f1ce0898cc756674dabe7f52c68e9c8217ca6
qinlinj <justinqinlin@gmail.com>
2025-04-12 23:17:45 -0700

    feat(StackUtil.java): implement basic stack operations to demonstrate stack usage in Java

--- src/main/java/org/qinlinj/javabasics/StackUtil.java
+++ src/main/java/org/qinlinj/javabasics/StackUtil.java
@@ -1,10 +1,16 @@
 package org.qinlinj.javabasics;
 
+import java.util.*;
+
 public class StackUtil {
     public static void main(String[] args) {
-
+        Stack<Integer> stack = new Stack<>();
         // add element at the top of the stack
-
+        
+        stack.push(1);
+        stack.push(2);
+        stack.push(5);
+        System.out.println(stack);
 
         // check if the stack is empty
 
