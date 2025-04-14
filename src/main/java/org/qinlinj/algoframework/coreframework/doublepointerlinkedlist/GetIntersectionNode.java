d964fc4a9a40910c4fda5e5fad2aa19d816b024d
qinlinj <justinqinlin@gmail.com>
2025-04-13 23:59:59 -0700

    feat(GetIntersectionNode.java): add a new solution class to find the intersection of two linked lists using length comparison and two pointers for improved clarity and efficiency

--- src/main/java/org/qinlinj/algoframework/coreframework/doublepointerlinkedlist/GetIntersectionNode.java
+++ src/main/java/org/qinlinj/algoframework/coreframework/doublepointerlinkedlist/GetIntersectionNode.java
@@ -24,4 +24,34 @@ public class GetIntersectionNode {
             return p1;
         }
     }
+
+    class Solution1 {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            int lenA = 0, lenB = 0;
+
+            for (ListNode p1 = headA; p1 != null; p1 = p1.next) {
+                lenA++;
+            }
+            for (ListNode p2 = headB; p2 != null; p2 = p2.next) {
+                lenB++;
+            }
+
+            ListNode p1 = headA, p2 = headB;
+            if (lenA > lenB) {
+                for (int i = 0; i < lenA - lenB; i++) {
+                    p1 = p1.next;
+                }
+            } else {
+                for (int i = 0; i < lenB - lenA; i++) {
+                    p2 = p2.next;
+                }
+            }
+
+            while (p1 != p2) {
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            return p1;
+        }
+    }
 }
