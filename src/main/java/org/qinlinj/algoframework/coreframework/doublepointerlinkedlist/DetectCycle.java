d3330a962aa7d54ab0c2f2a666b0ceaf4a75c484
qinlinj <justinqinlin@gmail.com>
2025-04-13 23:58:47 -0700

    feat(DetectCycle.java): implement cycle detection in a linked list using the Floyd's Tortoise and Hare algorithm to enhance data structure utilities

---
+++ src/main/java/org/qinlinj/algoframework/coreframework/doublepointerlinkedlist/DetectCycle.java
@@ -0,0 +1,30 @@
+package org.qinlinj.algoframework.coreframework.doublepointerlinkedlist;
+
+import org.qinlinj.leetcode.editor.common.ListNode;
+
+public class DetectCycle {
+    class Solution {
+        public ListNode detectCycle(ListNode head) {
+            ListNode fast, slow;
+            fast = slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) break;
+            }
+
+            if (fast == null || fast.next == null) {
+
+                return null;
+            }
+            
+            slow = head;
+
+            while (slow != fast) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+}
