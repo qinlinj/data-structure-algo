fd807ebf2cb8ff03a83c99fa36ed1ed441866625
qinlinj <justinqinlin@gmail.com>
2025-04-15 23:18:59 -0700

    feat(ProblemDecompositionMode.java): add new class for problem decomposition mode to support recursion framework development

---
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_142_two_thinking_modes_for_recursion/_1_problem_decomposition_mode/ProblemDecompositionMode.java
@@ -0,0 +1,4 @@
+package org.qinlinj.algoframework._100_core_framework._140_recursion_framework._142_two_thinking_modes_for_recursion._1_problem_decomposition_mode;
+
+public class ProblemDecompositionMode {
+}
