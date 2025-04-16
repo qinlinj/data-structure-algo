c7ca4cb4da05251a87fabf1efb13e0e87a1433d7
qinlinj <justinqinlin@gmail.com>
2025-04-15 23:20:09 -0700

    feat(ProblemDecompositionMode.java): add TreeNode class definition to represent binary tree nodes for better data structure management

--- src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_142_two_thinking_modes_for_recursion/_1_problem_decomposition_mode/ProblemDecompositionMode.java
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_142_two_thinking_modes_for_recursion/_1_problem_decomposition_mode/ProblemDecompositionMode.java
@@ -1,4 +1,27 @@
 package org.qinlinj.algoframework._100_core_framework._140_recursion_framework._142_two_thinking_modes_for_recursion._1_problem_decomposition_mode;
 
+// @formatter:off
 public class ProblemDecompositionMode {
+    /**
+     * Definition for a binary tree node
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
 }
