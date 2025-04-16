2f82da95033b4e1a74dc647d50e2226eae6d1eb5
qinlinj <justinqinlin@gmail.com>
2025-04-15 23:22:31 -0700

    feat(TraversalMode.java): add TreeNode class definition to represent binary tree nodes for improved data structure handling

--- src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_142_two_thinking_modes_for_recursion/_2_traversal_mode/TraversalMode.java
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_142_two_thinking_modes_for_recursion/_2_traversal_mode/TraversalMode.java
@@ -1,4 +1,25 @@
 package org.qinlinj.algoframework._100_core_framework._140_recursion_framework._142_two_thinking_modes_for_recursion._2_traversal_mode;
 
 public class TraversalMode {
+    /**
+     * Definition for a binary tree node
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
