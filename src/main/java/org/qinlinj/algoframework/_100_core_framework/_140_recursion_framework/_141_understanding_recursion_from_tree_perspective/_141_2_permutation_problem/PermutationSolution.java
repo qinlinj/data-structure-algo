60874d60791b30550ff69eb6fd6259171db6470e
qinlinj <justinqinlin@gmail.com>
2025-04-15 23:09:54 -0700

    feat(PermutationSolution.java): implement initial structure for permutation generation with backtracking
    This commit introduces the main function `permute` to find all permutations of a given array of distinct integers. It sets up the necessary data structures and initiates the backtracking process.

--- src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_141_understanding_recursion_from_tree_perspective/_141_2_permutation_problem/PermutationSolution.java
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_141_understanding_recursion_from_tree_perspective/_141_2_permutation_problem/PermutationSolution.java
@@ -1,4 +1,29 @@
 package org.qinlinj.algoframework._100_core_framework._140_recursion_framework._141_understanding_recursion_from_tree_perspective._141_2_permutation_problem;
 
+import java.util.*;
+
 public class PermutationSolution {
+    private List<List<Integer>> result = new LinkedList<>();
+
+    /**
+     * Main function to find all permutations of the given array of distinct integers.
+     *
+     * @param nums An array of distinct integers
+     * @return All possible permutations
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        // Track the current permutation being built
+        LinkedList<Integer> track = new LinkedList<>();
+
+        // Keep track of which elements have been used
+        boolean[] used = new boolean[nums.length];
+
+        // Start the backtracking process
+        backtrack(nums, track, used);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+    }
 }
