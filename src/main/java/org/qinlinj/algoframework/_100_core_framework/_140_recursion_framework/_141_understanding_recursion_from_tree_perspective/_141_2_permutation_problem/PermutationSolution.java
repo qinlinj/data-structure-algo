d82bdafbddc28701d23cafdd94b451430d42db58
qinlinj <justinqinlin@gmail.com>
2025-04-15 23:10:07 -0700

    feat(PermutationSolution.java): implement backtracking function to build permutations of distinct integers
    docs(PermutationSolution.java): add method documentation for backtrack function to clarify its purpose and parameters

--- src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_141_understanding_recursion_from_tree_perspective/_141_2_permutation_problem/PermutationSolution.java
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_140_recursion_framework/_141_understanding_recursion_from_tree_perspective/_141_2_permutation_problem/PermutationSolution.java
@@ -24,6 +24,38 @@ public class PermutationSolution {
         return result;
     }
 
+    /**
+     * Backtracking function to build permutations.
+     *
+     * @param nums  The input array of distinct integers
+     * @param track The current permutation being built
+     * @param used  Boolean array tracking which elements have been used
+     */
     private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        // Base case: if the track is the same length as nums, we have a complete permutation
+        if (track.size() == nums.length) {
+            // Add a copy of the current permutation to our result
+            result.add(new LinkedList<>(track));
+            return;
+        }
+
+        // Try each number as the next element in our permutation
+        for (int i = 0; i < nums.length; i++) {
+            // Skip elements that have already been used
+            if (used[i]) {
+                continue;
+            }
+
+            // Make a choice - add the current number to our permutation
+            track.add(nums[i]);
+            used[i] = true;
+
+            // Explore further with this choice
+            backtrack(nums, track, used);
+
+            // Undo the choice (backtrack) to try other possibilities
+            track.removeLast();
+            used[i] = false;
+        }
     }
 }
