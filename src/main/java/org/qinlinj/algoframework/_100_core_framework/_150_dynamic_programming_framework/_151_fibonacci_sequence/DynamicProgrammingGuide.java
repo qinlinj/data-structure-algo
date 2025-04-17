7056828466cd0bdd0ba8a92f2319026dff163c34
qinlinj <justinqinlin@gmail.com>
2025-04-16 23:53:06 -0700

    feat(DynamicProgrammingGuide.java): add DynamicProgrammingGuide class to demonstrate Fibonacci sequence calculation and optimization techniques

---
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_150_dynamic_programming_framework/_151_fibonacci_sequence/DynamicProgrammingGuide.java
@@ -0,0 +1,13 @@
+package org.qinlinj.algoframework._100_core_framework._150_dynamic_programming_framework._151_fibonacci_sequence;
+
+public class DynamicProgrammingGuide {
+    /**
+     * Example: Fibonacci Sequence
+     * Problem: Calculate the nth Fibonacci number where F(1)=1, F(2)=1, F(n)=F(n-1)+F(n-2) for n>2
+     *
+     * This demonstrates the inefficiency of naive recursion and three approaches to optimize:
+     * 1. Naive recursive solution (exponential time)
+     * 2. Top-down DP with memoization (linear time)
+     * 3. Bottom-up DP with tabulation (linear time)
+     */
+}
