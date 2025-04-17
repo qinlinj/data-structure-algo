9e77b7f674f860460452e74e4e27e47838f056cc
qinlinj <justinqinlin@gmail.com>
2025-04-17 00:06:56 -0700

    feat(CoinChangeProblem): add Coin Change Problem class to demonstrate dynamic programming concepts

---
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_150_dynamic_programming_framework/_152_coin_change_problem/CoinChangeProblem.java
@@ -0,0 +1,22 @@
+package org.qinlinj.algoframework._100_core_framework._150_dynamic_programming_framework._152_coin_change_problem;
+
+// @formatter:off
+/**
+ * Dynamic Programming: Coin Change Problem
+ * ----------------------------------------
+ *
+ * This class explores the famous "Coin Change" problem (LeetCode 322) as a case study
+ * for understanding dynamic programming concepts, particularly:
+ *
+ * 1. Optimal substructure
+ * 2. State identification
+ * 3. State transition equations
+ * 4. Recursive vs iterative solutions
+ *
+ * Problem Statement:
+ * Given an array of coin denominations and a target amount, find the minimum
+ * number of coins needed to make up that amount. Return -1 if the amount
+ * cannot be made up by any combination of coins.
+ */
+public class CoinChangeProblem {
+}
