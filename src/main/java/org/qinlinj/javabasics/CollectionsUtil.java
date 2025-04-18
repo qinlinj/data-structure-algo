1d532f1ae0d08a8b6fae20ec617790ddf50f34b9
qinlinj <justinqinlin@gmail.com>
2025-04-18 12:30:04 -0700

    feat(CollectionsUtil.java): add main method to demonstrate usage of Collections utility methods

--- src/main/java/org/qinlinj/javabasics/CollectionsUtil.java
+++ src/main/java/org/qinlinj/javabasics/CollectionsUtil.java
@@ -1,4 +1,10 @@
 package org.qinlinj.javabasics;
 
+import java.util.*;
+
 public class CollectionsUtil {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Collections.nCopies(10, 0));
+        Collections.reverse(list);
+    }
 }
