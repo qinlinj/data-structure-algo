a770468959b8e1e78016905c01108d6a372cd3c7
qinlinj <justinqinlin@gmail.com>
2025-04-17 23:25:48 -0700

    docs(TotalPermutationProblem.java): add documentation for backtracking algorithm framework to provide clarity on its purpose and functionality

--- src/main/java/org/qinlinj/algoframework/_100_core_framework/_160_backtracking_algo_application_strategy/TotalPermutationProblem.java
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_160_backtracking_algo_application_strategy/TotalPermutationProblem.java
@@ -1,4 +1,15 @@
 package org.qinlinj.algoframework._100_core_framework._160_backtracking_algo_application_strategy;
 
-public class TotalPermutationProblem {
+// @formatter:off
+/**
+ * Backtracking Algorithm Framework and Explanation
+ * -----------------------------------------------
+ *
+ * Overview:
+ * Backtracking is essentially a depth-first search (DFS) algorithm applied to a decision tree.
+ * It systematically explores all possible solutions by building candidates incrementally and
+ * abandoning a candidate (backtracking) when it determines the candidate cannot lead to a valid solution.
+ *
+ */
+ public class TotalPermutationProblem {
 }
