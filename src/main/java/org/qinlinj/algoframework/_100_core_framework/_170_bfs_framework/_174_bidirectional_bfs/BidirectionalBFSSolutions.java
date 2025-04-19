91ed0e2412c70155a3fbb468ab566509841892ea
qinlinj <justinqinlin@gmail.com>
2025-04-18 22:58:50 -0700

    feat(BidirectionalBFSSolutions.java): implement slidingPuzzle method to solve sliding puzzle using BFS algorithm chore(BidirectionalBFSSolutions.java): add import for java.util package to support data structures used in BFS implementation docs(BidirectionalBFSSolutions.java): add Javadoc comments for slidingPuzzle method to describe its purpose and parameters

--- src/main/java/org/qinlinj/algoframework/_100_core_framework/_170_bfs_framework/_174_bidirectional_bfs/BidirectionalBFSSolutions.java
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_170_bfs_framework/_174_bidirectional_bfs/BidirectionalBFSSolutions.java
@@ -1,5 +1,7 @@
 package org.qinlinj.algoframework._100_core_framework._170_bfs_framework._174_bidirectional_bfs;
 
+import java.util.*;
+
 /**
  * Bidirectional BFS Solutions for Classic Puzzles
  * <p>
@@ -33,5 +35,38 @@ public class BidirectionalBFSSolutions {
      * - 0 represents the empty space that can be swapped with adjacent numbers
      * - We need to find the minimum number of moves to reach [[1,2,3],[4,5,0]]
      */
+    /**
+     * Solves the sliding puzzle using standard BFS.
+     *
+     * @param board Initial state of the board as 2x3 array
+     * @return Minimum moves to reach target, or -1 if impossible
+     */
+    public int slidingPuzzle(int[][] board) {
+        // Target state: "123450"
+        String target = "123450";
 
+        // Convert initial board to string representation
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                sb.append(board[i][j]);
+            }
+        }
+        String start = sb.toString();
+
+        // Standard BFS
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        queue.offer(start);
+        visited.add(start);
+
+        int step = 0;
+
+
+        return -1;
+    }
+
+    private String[] getSlidingNeighbors(String current) {
+    }
 }
