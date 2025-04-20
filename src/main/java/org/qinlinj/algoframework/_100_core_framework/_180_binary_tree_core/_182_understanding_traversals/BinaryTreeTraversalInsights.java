7986afba9946c8b3e511e7c7942ec420d34c85fb
qinlinj <justinqinlin@gmail.com>
2025-04-19 22:58:37 -0700

    feat(BinaryTreeTraversalInsights.java): add definitions for TreeNode and ListNode classes to enhance data structure representation

--- src/main/java/org/qinlinj/algoframework/_100_core_framework/_180_binary_tree_core/_182_understanding_traversals/BinaryTreeTraversalInsights.java
+++ src/main/java/org/qinlinj/algoframework/_100_core_framework/_180_binary_tree_core/_182_understanding_traversals/BinaryTreeTraversalInsights.java
@@ -1,4 +1,24 @@
 package org.qinlinj.algoframework._100_core_framework._180_binary_tree_core._182_understanding_traversals;
 
 public class BinaryTreeTraversalInsights {
+    // Definition for a binary tree node
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // Definition for a singly linked list node
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
 }
