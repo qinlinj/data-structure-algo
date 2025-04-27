f055354496c1526bf16840482bdfbc9839219ce1
qinlinj <justinqinlin@gmail.com>
2025-04-27 00:21:39 -0700

    feat(BinarySearchCoreFramework): add a new class to summarize binary search concepts and framework for various problems

---
+++ src/main/java/org/qinlinj/algoframework/_300_classical_data_structure_algo/_330_array_algo_II/_335_binary_search_framework/_335_a_BinarySearchCoreFramework.java
@@ -0,0 +1,27 @@
+package org.qinlinj.algoframework._300_classical_data_structure_algo._330_array_algo_II._335_binary_search_framework;
+
+/**
+ * Binary Search Core Framework - Summary
+ * <p>
+ * This file summarizes key concepts about generalizing binary search beyond sorted arrays.
+ * <p>
+ * Binary search can be used to solve various problems efficiently when:
+ * 1. You can identify a monotonic function f(x) of a variable x
+ * 2. You want to find a value of x where f(x) meets a specific target constraint
+ * <p>
+ * Binary Search Framework:
+ * 1. Identify the variable x, function f(x), and target value
+ * 2. Determine the search range for x (min and max possible values)
+ * 3. Decide whether to search for left boundary or right boundary
+ * 4. Implement binary search algorithm accordingly
+ * <p>
+ * The article discusses several examples demonstrating this framework:
+ * - Finding elements in sorted arrays (basic binary search)
+ * - Koko eating bananas (finding minimum eating speed)
+ * - Shipping packages within days (finding minimum capacity)
+ * - Splitting array into subarrays (minimizing maximum subarray sum)
+ * <p>
+ * This class organizes these concepts into separate examples.
+ */
+public class _335_a_BinarySearchCoreFramework {
+}
