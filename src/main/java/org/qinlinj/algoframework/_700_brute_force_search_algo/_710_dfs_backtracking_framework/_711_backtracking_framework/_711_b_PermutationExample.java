ea16f25c427a088332933c1c51d2dc0e58e95cdb
qinlinj <justinqinlin@gmail.com>
2025-05-10 23:57:58 -0700

    feat(PermutationExample.java): implement permutation generation using backtracking to enhance algorithm functionality docs(PermutationExample.java): add method documentation for permute to clarify usage and parameters

--- src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_710_dfs_backtracking_framework/_711_backtracking_framework/_711_b_PermutationExample.java
+++ src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_710_dfs_backtracking_framework/_711_backtracking_framework/_711_b_PermutationExample.java
@@ -1,4 +1,26 @@
 package org.qinlinj.algoframework._700_brute_force_search_algo._710_dfs_backtracking_framework._711_backtracking_framework;
 
+import java.util.*;
+
 public class _711_b_PermutationExample {
+    private List<List<Integer>> result = new LinkedList<>();
+
+    /**
+     * Main function to compute all permutations of a given array
+     *
+     * @param nums Array of distinct integers
+     * @return List of all possible permutations
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        // Record the "path" (current permutation being built)
+        LinkedList<Integer> track = new LinkedList<>();
+
+        // Track which elements have been used to avoid duplicates
+        boolean[] used = new boolean[nums.length];
+
+        // Start backtracking
+        backtrack(nums, track, used);
+
+        return result;
+    }
 }
