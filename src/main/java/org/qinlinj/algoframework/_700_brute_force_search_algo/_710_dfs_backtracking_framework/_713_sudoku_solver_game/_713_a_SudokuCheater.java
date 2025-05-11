afcf18c1cfc193e4fbd36326c10d6ab8a98c51b7
qinlinj <justinqinlin@gmail.com>
2025-05-11 00:07:01 -0700

    feat(SudokuCheater.java): add BoardHandler interface to manage board operations and improve code structure

--- src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_710_dfs_backtracking_framework/_713_sudoku_solver_game/_713_a_SudokuCheater.java
+++ src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_710_dfs_backtracking_framework/_713_sudoku_solver_game/_713_a_SudokuCheater.java
@@ -1,4 +1,30 @@
 package org.qinlinj.algoframework._700_brute_force_search_algo._710_dfs_backtracking_framework._713_sudoku_solver_game;
 
 public class _713_a_SudokuCheater {
+    /**
+     * Interface to handle the board operations
+     * This simulates the boardHandler from the JavaScript version
+     */
+    public interface BoardHandler {
+        /**
+         * Get the value at position (row, col)
+         *
+         * @return null if empty, otherwise the digit 1-9
+         */
+        Integer get(int row, int col);
+
+        /**
+         * Set a value at position (row, col)
+         *
+         * @param value The digit to set (1-9), or null to clear the cell
+         */
+        void set(int row, int col, Integer value);
+
+        /**
+         * Check if the cell at position (row, col) is editable
+         *
+         * @return true if the cell can be modified, false if it's a preset value
+         */
+        boolean isEditable(int row, int col);
+    }
 }
