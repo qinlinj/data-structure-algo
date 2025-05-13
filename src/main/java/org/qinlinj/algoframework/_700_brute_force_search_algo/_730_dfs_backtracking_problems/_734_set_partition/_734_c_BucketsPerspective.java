34d3eabcd29d46e8a18f2b2a2f65fcab5fd76a87
qinlinj <justinqinlin@gmail.com>
2025-05-13 10:49:10 -0700

    feat(BucketsPerspective.java): add canPartitionKSubsets method to determine if an array can be partitioned into k subsets with equal sum using a backtracking approach

--- src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_c_BucketsPerspective.java
+++ src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_c_BucketsPerspective.java
@@ -1,4 +1,30 @@
 package org.qinlinj.algoframework._700_brute_force_search_algo._730_dfs_backtracking_problems._734_set_partition;
 
 public class _734_c_BucketsPerspective {
+    /**
+     * Determines if the array can be partitioned into k subsets with equal sum
+     * using the buckets' perspective approach
+     */
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        // Basic validation
+        if (k > nums.length) return false;
+
+        // Calculate the sum of all elements
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        // If the sum cannot be divided evenly by k, return false
+        if (sum % k != 0) return false;
+
+        // Calculate the target sum for each subset
+        int target = sum / k;
+
+        // Create a boolean array to track which elements are used
+        boolean[] used = new boolean[nums.length];
+
+        // Start the backtracking process with the first bucket
+        return backtrack(k, 0, nums, 0, used, target);
+    }
 }
