95002be095de62973f2291540994f20874df458b
qinlinj <justinqinlin@gmail.com>
2025-05-13 10:48:24 -0700

    feat(ElementsPerspective.java): add reverseArray helper method to sort arrays in descending order for improved functionality

--- src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_b_ElementsPerspective.java
+++ src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_b_ElementsPerspective.java
@@ -86,4 +86,19 @@ public class _734_b_ElementsPerspective {
         // Cannot partition the array with the current arrangement
         return false;
     }
+
+    /**
+     * Helper method to reverse an array (used to sort in descending order)
+     */
+    private void reverseArray(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
