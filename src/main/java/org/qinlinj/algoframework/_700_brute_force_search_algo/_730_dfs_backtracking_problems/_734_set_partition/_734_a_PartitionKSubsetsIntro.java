1cc7b833123415c69381ce9338818b757d78ca81
qinlinj <justinqinlin@gmail.com>
2025-05-13 10:47:25 -0700

    feat(PartitionKSubsetsIntro.java): add demonstrateExample method to provide examples for canPartitionKSubsets functionality chore(PartitionKSubsetsIntro.java): import java.util package to use Arrays utility for example demonstration

--- src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_a_PartitionKSubsetsIntro.java
+++ src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_a_PartitionKSubsetsIntro.java
@@ -1,5 +1,7 @@
 package org.qinlinj.algoframework._700_brute_force_search_algo._730_dfs_backtracking_problems._734_set_partition;
 
+import java.util.*;
+
 public class _734_a_PartitionKSubsetsIntro {
     /**
      * Basic implementation of canPartitionKSubsets to validate if an array
@@ -65,6 +67,39 @@ public class _734_a_PartitionKSubsetsIntro {
 
         // Cannot partition the array with the current arrangement
         return false;
+    }
+
+    /**
+     * Helper method to demonstrate the problem with an example
+     */
+    public void demonstrateExample() {
+        int[] example1 = {4, 3, 2, 3, 5, 2, 1};
+        int k1 = 4;
+        int targetSum1 = Arrays.stream(example1).sum() / k1;
+
+        System.out.println("Example 1:");
+        System.out.println("nums = " + Arrays.toString(example1));
+        System.out.println("k = " + k1);
+        System.out.println("Total sum = " + Arrays.stream(example1).sum());
+        System.out.println("Target sum for each subset = " + targetSum1);
+        System.out.println("Can partition into " + k1 + " equal subsets? " +
+                canPartitionKSubsets(example1, k1));
+
+        // Example of a possible partition:
+        System.out.println("One possible partition:");
+        System.out.println("Subset 1: [4, 1] (sum = 5)");
+        System.out.println("Subset 2: [3, 2] (sum = 5)");
+        System.out.println("Subset 3: [3, 2] (sum = 5)");
+        System.out.println("Subset 4: [5] (sum = 5)");
+
+        System.out.println("\nExample 2:");
+        int[] example2 = {1, 2, 3, 4};
+        int k2 = 3;
+        System.out.println("nums = " + Arrays.toString(example2));
+        System.out.println("k = " + k2);
+        System.out.println("Total sum = " + Arrays.stream(example2).sum());
+        System.out.println("Can partition into " + k2 + " equal subsets? " +
+                canPartitionKSubsets(example2, k2));
     }
 
 }
