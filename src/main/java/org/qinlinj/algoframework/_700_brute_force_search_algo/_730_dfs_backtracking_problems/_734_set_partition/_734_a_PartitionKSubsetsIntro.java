402d4fc5b1ba3b6168b571e14e9cb0afbacd3ac2
qinlinj <justinqinlin@gmail.com>
2025-05-13 10:47:10 -0700

    feat(PartitionKSubsetsIntro): add canPartitionKSubsets method to validate if an array can be divided into k subsets with equal sum

--- src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_a_PartitionKSubsetsIntro.java
+++ src/main/java/org/qinlinj/algoframework/_700_brute_force_search_algo/_730_dfs_backtracking_problems/_734_set_partition/_734_a_PartitionKSubsetsIntro.java
@@ -1,4 +1,30 @@
 package org.qinlinj.algoframework._700_brute_force_search_algo._730_dfs_backtracking_problems._734_set_partition;
 
 public class _734_a_PartitionKSubsetsIntro {
+    /**
+     * Basic implementation of canPartitionKSubsets to validate if an array
+     * can be divided into k subsets with equal sum
+     */
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        // Basic validation
+        if (k > nums.length) return false;
+
+        // Calculate the sum of all elements
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        // If the sum cannot be divided evenly by k, return false
+        if (sum % k != 0) return false;
+
+        // Calculate the target sum for each subset
+        int targetSum = sum / k;
+
+        // Create an array to track the current sum of each subset
+        int[] buckets = new int[k];
+
+        // Start the backtracking process
+        return canPartition(nums, 0, buckets, targetSum);
+    }
 }
