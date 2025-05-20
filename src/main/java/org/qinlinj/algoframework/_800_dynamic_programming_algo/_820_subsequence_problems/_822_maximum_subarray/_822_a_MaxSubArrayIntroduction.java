9eb054368f05bb005e55d60f13a1f04b326dbab9
qinlinj <justinqinlin@gmail.com>
2025-05-19 23:27:19 -0700

    feat(MaxSubArrayIntroduction.java): add introduction to the Maximum Subarray Problem with example and framework for solution approaches

---
+++ src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_820_subsequence_problems/_822_maximum_subarray/_822_a_MaxSubArrayIntroduction.java
@@ -0,0 +1,45 @@
+package org.qinlinj.algoframework._800_dynamic_programming_algo._820_subsequence_problems._822_maximum_subarray;
+
+/**
+ * Introduction to the Maximum Subarray Problem (LeetCode #53)
+ * <p>
+ * Problem Description:
+ * Given an integer array nums, find the contiguous subarray (containing at least one number)
+ * which has the largest sum and return its sum.
+ * <p>
+ * Example:
+ * Input: nums = [-3,1,3,-1,2,-4,2]
+ * Output: 5
+ * Explanation: The subarray [1,3,-1,2] has the largest sum 5.
+ * <p>
+ * This class introduces the problem and sets up the basic framework. The solution can be
+ * approached in multiple ways:
+ * 1. Sliding Window
+ * 2. Dynamic Programming
+ * 3. Prefix Sum
+ * <p>
+ * Each approach has distinct advantages and illustrates different algorithm design paradigms.
+ */
+public class _822_a_MaxSubArrayIntroduction {
+
+    /**
+     * Main method to demonstrate the problem with a simple example
+     */
+    public static void main(String[] args) {
+        int[] nums = {-3, 1, 3, -1, 2, -4, 2};
+        System.out.println("The input array is: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nThe maximum subarray problem is explored in the following classes using different approaches.");
+    }
+
+    /**
+     * This is the function signature required by LeetCode
+     */
+    public int maxSubArray(int[] nums) {
+        // This method will be implemented in the following classes
+        // with different approaches
+        return 0;
+    }
+}
