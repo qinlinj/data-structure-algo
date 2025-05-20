a4d17b8eece5e3cd2b1875a4c2ce465044229e28
qinlinj <justinqinlin@gmail.com>
2025-05-20 00:02:39 -0700

    feat(MinInsertionsToPalindrome.java): add minInsertions method to calculate minimum insertions for palindrome conversion

--- src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_820_subsequence_problems/_824_subsequence_problem_templates/_824_e_MinInsertionsToPalindrome.java
+++ src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_820_subsequence_problems/_824_subsequence_problem_templates/_824_e_MinInsertionsToPalindrome.java
@@ -1,4 +1,10 @@
 package org.qinlinj.algoframework._800_dynamic_programming_algo._820_subsequence_problems._824_subsequence_problem_templates;
 
 public class _824_e_MinInsertionsToPalindrome {
+    /**
+     * Direct approach: Minimum insertions equals length of string minus length of LPS
+     */
+    public static int minInsertions(String s) {
+        return s.length() - longestPalindromeSubseq(s);
+    }
 }
