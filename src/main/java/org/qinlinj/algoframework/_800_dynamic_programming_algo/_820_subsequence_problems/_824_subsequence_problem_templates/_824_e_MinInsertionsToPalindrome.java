1dec8507c7d7a3b3af6f83c73a9c7c099a1e12a3
qinlinj <justinqinlin@gmail.com>
2025-05-20 00:02:58 -0700

    feat(MinInsertionsToPalindrome.java): add alternative DP approach for calculating minimum insertions to make a string a palindrome to enhance functionality and provide more options for solving the problem

--- src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_820_subsequence_problems/_824_subsequence_problem_templates/_824_e_MinInsertionsToPalindrome.java
+++ src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_820_subsequence_problems/_824_subsequence_problem_templates/_824_e_MinInsertionsToPalindrome.java
@@ -34,4 +34,27 @@ public class _824_e_MinInsertionsToPalindrome {
 
         return dp[0][n - 1];
     }
+
+    /**
+     * Alternative approach: directly calculate minimum insertions with DP
+     */
+    public static int minInsertionsDP(String s) {
+        int n = s.length();
+
+        // dp[i][j] = min insertions to make s[i...j] a palindrome
+        int[][] dp = new int[n][n];
+
+        // Fill the dp array
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
 }
