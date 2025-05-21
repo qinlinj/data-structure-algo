1ddfb2524d32b9d0ec564dbf2cb424e520693d9e
qinlinj <justinqinlin@gmail.com>
2025-05-21 13:26:30 -0700

    feat(CoinChangeIIOptimized.java): implement space-optimized solution for Coin Change II problem using 1D DP array to improve efficiency and reduce space complexity

--- src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_830_knapsack_problems/_833_unbounded_knapsack/_833_d_CoinChangeIIOptimized.java
+++ src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_830_knapsack_problems/_833_unbounded_knapsack/_833_d_CoinChangeIIOptimized.java
@@ -1,4 +1,29 @@
 package org.qinlinj.algoframework._800_dynamic_programming_algo._830_knapsack_problems._833_unbounded_knapsack;
 
 public class _833_d_CoinChangeIIOptimized {
+    /**
+     * Space-optimized solution for the Coin Change II problem using 1D DP array
+     *
+     * @param amount the target amount
+     * @param coins  array of available coin denominations
+     * @return the number of ways to make change for the amount
+     */
+    public static int change(int amount, int[] coins) {
+        // Create a 1D DP array
+        int[] dp = new int[amount + 1];
+
+        // Base case: one way to make zero amount (use no coins)
+        dp[0] = 1;
+
+        // For each coin, update the dp array
+        for (int coin : coins) {
+            // Update dp[j] for each amount j that can use this coin
+            for (int j = coin; j <= amount; j++) {
+                // Add the number of ways to make amount j - coin
+                dp[j] += dp[j - coin];
+            }
+        }
+
+        return dp[amount];
+    }
 }
