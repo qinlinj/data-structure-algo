d4447aedc2aab8bf58aad0b878c720c8b0dba0ce
qinlinj <justinqinlin@gmail.com>
2025-05-21 16:34:15 -0700

    feat(KnapsackApproach.java): add countSubsetSum method to implement 2D dynamic programming for counting subsets with a given sum

--- src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_830_knapsack_problems/_834_target_sum_variation/_834_d_KnapsackApproach.java
+++ src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_830_knapsack_problems/_834_target_sum_variation/_834_d_KnapsackApproach.java
@@ -28,4 +28,38 @@ public class _834_d_KnapsackApproach {
         // Find number of subsets with the calculated sum
         return countSubsetSum(nums, subsetSum);
     }
+
+    /**
+     * 2D dynamic programming approach to count subsets with a given sum
+     *
+     * @param nums array of non-negative integers
+     * @param sum  the target subset sum
+     * @return number of ways to form the subset sum
+     */
+    private static int countSubsetSum(int[] nums, int sum) {
+        int n = nums.length;
+
+        // dp[i][j] = number of ways to make sum j using first i elements
+        int[][] dp = new int[n + 1][sum + 1];
+
+        // Base case: empty subset can form sum 0 in exactly 1 way
+        dp[0][0] = 1;
+
+        // Fill the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                // Current element is nums[i-1] (0-indexed array)
+
+                // Don't include current element
+                dp[i][j] = dp[i - 1][j];
+
+                // Include current element if possible
+                if (j >= nums[i - 1]) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
 }
