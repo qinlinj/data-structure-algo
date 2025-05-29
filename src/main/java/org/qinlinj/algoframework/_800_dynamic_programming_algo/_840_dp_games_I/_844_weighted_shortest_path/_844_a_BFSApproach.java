b4a675b518c29505bd6f23dee7792110a1f5c3aa
qinlinj <justinqinlin@gmail.com>
2025-05-28 22:16:12 -0700

    docs(BFSApproach.java): add documentation and a Node class to demonstrate the limitations of BFS in weighted graphs and the necessity of using a priority queue for Dijkstra's algorithm

--- src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_840_dp_games_I/_844_weighted_shortest_path/_844_a_BFSApproach.java
+++ src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_840_dp_games_I/_844_weighted_shortest_path/_844_a_BFSApproach.java
@@ -1,4 +1,27 @@
 package org.qinlinj.algoframework._800_dynamic_programming_algo._840_dp_games_I._844_weighted_shortest_path;
 
 public class _844_a_BFSApproach {
+    /**
+     * Demonstrates why regular BFS doesn't work for weighted graphs
+     * and why we need priority queue (Dijkstra's algorithm)
+     */
+    public static class WeightedGraphExample {
+
+        // Node class for priority queue
+        static class Node implements Comparable<Node> {
+            int city;
+            int cost;
+            int stops;
+
+            Node(int city, int cost, int stops) {
+                this.city = city;
+                this.cost = cost;
+                this.stops = stops;
+            }
+
+            @Override
+            public int compareTo(Node other) {
+                return Integer.compare(this.cost, other.cost);
+            }
+        }
 }
