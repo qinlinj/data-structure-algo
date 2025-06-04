674882a23f4d7e74c7c8946e9c3d42480e242011
qinlinj <justinqinlin@gmail.com>
2025-06-03 23:23:16 -0700

    feat(GreedyChoiceProperty): add MaxValueProblem class with greedy solution for maximum value with limited bills to demonstrate greedy choice property

--- src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_870_greedy_algorithms/_871_greedy_algorithm_framework/_864_b_GreedyChoiceProperty.java
+++ src/main/java/org/qinlinj/algoframework/_800_dynamic_programming_algo/_870_greedy_algorithms/_871_greedy_algorithm_framework/_864_b_GreedyChoiceProperty.java
@@ -1,4 +1,23 @@
 package org.qinlinj.algoframework._800_dynamic_programming_algo._870_greedy_algorithms._871_greedy_algorithm_framework;
 
 public class _864_b_GreedyChoiceProperty {
+
+    /**
+     * Problem 1: Maximum value with limited number of bills
+     * This problem HAS greedy choice property
+     */
+    public static class MaxValueProblem {
+
+        /**
+         * Greedy solution: Always choose higher denomination
+         * Time Complexity: O(1)
+         */
+        public static int solve(int numBills) {
+            // Local optimal choice: always pick 100-yuan bill
+            // Global optimal solution: numBills * 100
+            return numBills * 100;
+        }
+
+    }
 }
+
