44ffb4fb508147ea0c32db90536bf995145bde22
qinlinj <justinqinlin@gmail.com>
2025-06-05 17:39:42 -0700

    feat(StoneGame.java): add method to determine if the first player can guarantee a win in Stone Game with a mathematical proof

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_910_mathematical_techniques_I/_911_one_line_solutions/_911_b_StoneGame.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_910_mathematical_techniques_I/_911_one_line_solutions/_911_b_StoneGame.java
@@ -1,4 +1,14 @@
 package org.qinlinj.algoframework._900_other_common_algo_tricks._910_mathematical_techniques_I._911_one_line_solutions;
 
 public class _911_b_StoneGame {
+    /**
+     * Determines if the first player can guarantee a win in Stone Game
+     *
+     * @param piles array of stone piles
+     * @return true - first player can always win with optimal strategy
+     */
+    public boolean stoneGame(int[] piles) {
+        // Mathematical proof: first player always wins
+        return true;
+    }
 }
