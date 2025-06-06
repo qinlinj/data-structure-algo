3a722268620a491e4b1c15a79f793b06566ea9c4
qinlinj <justinqinlin@gmail.com>
2025-06-05 17:48:06 -0700

    feat(InterestingBitTricks.java): add methods to convert characters between cases using bitwise operations for improved performance and clarity

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_910_mathematical_techniques_I/_912_common_bit_operations/_912_a_InterestingBitTricks.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_910_mathematical_techniques_I/_912_common_bit_operations/_912_a_InterestingBitTricks.java
@@ -1,4 +1,25 @@
 package org.qinlinj.algoframework._900_other_common_algo_tricks._910_mathematical_techniques_I._912_common_bit_operations;
 
 public class _912_a_InterestingBitTricks {
+    /**
+     * Converts English character to lowercase using OR operation with space
+     */
+    public char toLowerCase(char c) {
+        return (char) (c | ' ');
+    }
+
+    /**
+     * Converts English character to uppercase using AND operation with underscore
+     */
+    public char toUpperCase(char c) {
+        return (char) (c & '_');
+    }
+
+    /**
+     * Toggles case of English character using XOR operation with space
+     */
+    public char toggleCase(char c) {
+        return (char) (c ^ ' ');
+    }
+
 }
