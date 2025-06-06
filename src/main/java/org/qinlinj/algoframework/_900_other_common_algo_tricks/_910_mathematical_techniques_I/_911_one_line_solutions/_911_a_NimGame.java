139e768f23173f49369ab7881b09917c023d6a86
qinlinj <justinqinlin@gmail.com>
2025-06-05 17:39:16 -0700

    feat(NimGame.java): add canWinNim method to determine winning strategy for Nim Game and provide documentation for clarity

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_910_mathematical_techniques_I/_911_one_line_solutions/_911_a_NimGame.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_910_mathematical_techniques_I/_911_one_line_solutions/_911_a_NimGame.java
@@ -1,4 +1,15 @@
 package org.qinlinj.algoframework._900_other_common_algo_tricks._910_mathematical_techniques_I._911_one_line_solutions;
 
 public class _911_a_NimGame {
+    /**
+     * Determines if the first player can guarantee a win in Nim Game
+     *
+     * @param n number of stones
+     * @return true if first player can win, false otherwise
+     */
+    public boolean canWinNim(int n) {
+        // If starting with multiple of 4 stones, you lose
+        // Otherwise, you can control opponent to face multiples of 4
+        return n % 4 != 0;
+    }
 }
