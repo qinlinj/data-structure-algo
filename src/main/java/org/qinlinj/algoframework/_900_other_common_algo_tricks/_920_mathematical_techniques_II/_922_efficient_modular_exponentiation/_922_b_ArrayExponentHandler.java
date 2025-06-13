6cc6cd7e1b6301287c777689f07aeb1d17a091e6
qinlinj <justinqinlin@gmail.com>
2025-06-12 23:45:20 -0700

    feat(ArrayExponentHandler.java): add new class for efficient modular exponentiation to encapsulate related functionality

---
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_922_efficient_modular_exponentiation/_922_b_ArrayExponentHandler.java
@@ -0,0 +1,4 @@
+package org.qinlinj.algoframework._900_other_common_algo_tricks._920_mathematical_techniques_II._922_efficient_modular_exponentiation;
+
+public class _922_b_ArrayExponentHandler {
+}
