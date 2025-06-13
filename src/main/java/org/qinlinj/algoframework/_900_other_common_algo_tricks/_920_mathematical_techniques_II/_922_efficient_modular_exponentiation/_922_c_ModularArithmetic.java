f094425bfcc974fcefef3c8031188731af343fe7
qinlinj <justinqinlin@gmail.com>
2025-06-12 23:47:04 -0700

    feat(ModularArithmetic.java): add a constant MOD for modular arithmetic to improve code clarity and maintainability

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_922_efficient_modular_exponentiation/_922_c_ModularArithmetic.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_922_efficient_modular_exponentiation/_922_c_ModularArithmetic.java
@@ -1,4 +1,6 @@
 package org.qinlinj.algoframework._900_other_common_algo_tricks._920_mathematical_techniques_II._922_efficient_modular_exponentiation;
 
 public class _922_c_ModularArithmetic {
+    private static final int MOD = 1337;
+
 }
