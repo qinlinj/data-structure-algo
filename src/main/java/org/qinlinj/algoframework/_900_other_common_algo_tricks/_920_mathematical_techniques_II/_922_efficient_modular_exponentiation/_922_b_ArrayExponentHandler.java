ac76e9d24cb7a2e890a447badcdb8379be1e2ae6
qinlinj <justinqinlin@gmail.com>
2025-06-12 23:45:31 -0700

    feat(ArrayExponentHandler.java): add ExponentProcessor class to demonstrate array-based exponent handling techniques

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_922_efficient_modular_exponentiation/_922_b_ArrayExponentHandler.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_922_efficient_modular_exponentiation/_922_b_ArrayExponentHandler.java
@@ -1,4 +1,10 @@
 package org.qinlinj.algoframework._900_other_common_algo_tricks._920_mathematical_techniques_II._922_efficient_modular_exponentiation;
 
 public class _922_b_ArrayExponentHandler {
+    /**
+     * Demonstrates different approaches to handle array-based exponents
+     */
+    public static class ExponentProcessor {
+        
+    }
 }
