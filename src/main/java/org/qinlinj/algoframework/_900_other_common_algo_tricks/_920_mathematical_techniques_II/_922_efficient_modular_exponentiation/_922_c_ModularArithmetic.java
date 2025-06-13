e8497495a321521c30a7fb0d0f0c6a3a162f24cc
qinlinj <justinqinlin@gmail.com>
2025-06-12 23:47:18 -0700

    feat(ModularArithmetic.java): add ModularCalculator class for future modular arithmetic operations

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_922_efficient_modular_exponentiation/_922_c_ModularArithmetic.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_922_efficient_modular_exponentiation/_922_c_ModularArithmetic.java
@@ -3,4 +3,7 @@ package org.qinlinj.algoframework._900_other_common_algo_tricks._920_mathematica
 public class _922_c_ModularArithmetic {
     private static final int MOD = 1337;
 
+    public static class ModularCalculator {
+
+    }
 }
