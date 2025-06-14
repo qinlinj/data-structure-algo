79b9b3f62a5728e7b2b3f116bd2da5b9826c244c
qinlinj <justinqinlin@gmail.com>
2025-06-13 21:12:52 -0700

    feat(OptimalSolution.java): add visualizeAlgorithm method to demonstrate the algorithm's workings with a visual representation for better understanding

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_923_finding_missing_and_duplicate_elements/_923_c_OptimalSolution.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_923_finding_missing_and_duplicate_elements/_923_c_OptimalSolution.java
@@ -87,5 +87,52 @@ public class _923_c_OptimalSolution {
         return new int[]{duplicate, missing};
     }
 
+    /**
+     * Demonstrates why the algorithm works with visual representation
+     */
+    public static void visualizeAlgorithm() {
+        System.out.println("\n=== Algorithm Visualization ===");
 
+        int[] nums = {1, 2, 2, 4};
+        System.out.println("Problem: " + Arrays.toString(nums));
+        System.out.println("Should contain: [1, 2, 3, 4]");
+        System.out.println("Issue: 2 appears twice, 3 is missing");
+
+        System.out.println("\nVisualization of marking process:");
+        System.out.println("Array indices: [0] [1] [2] [3]");
+        System.out.println("Should contain: 1   2   3   4");
+        System.out.println("Actually has:   1   2   2   4");
+
+        int[] working = nums.clone();
+        System.out.println("\nInitial state: " + Arrays.toString(working));
+
+        // Process each element
+        String[] states = new String[nums.length + 1];
+        states[0] = Arrays.toString(working);
+
+        for (int i = 0; i < nums.length; i++) {
+            int element = Math.abs(working[i]);
+            int targetIndex = element - 1;
+
+            System.out.println("\nProcessing element " + element + ":");
+            System.out.println("  Maps to index " + targetIndex);
+
+            if (working[targetIndex] < 0) {
+                System.out.println("  Index " + targetIndex + " already marked -> DUPLICATE!");
+            } else {
+                working[targetIndex] *= -1;
+                System.out.println("  Marking index " + targetIndex + " as visited");
+            }
+
+            states[i + 1] = Arrays.toString(working);
+            System.out.println("  State: " + states[i + 1]);
+        }
+
+        System.out.println("\nFinal analysis:");
+        for (int i = 0; i < working.length; i++) {
+            if (working[i] > 0) {
+                System.out.println("  Index " + i + " not visited -> Element " + (i + 1) + " missing");
+            }
+        }
+    }
 }
