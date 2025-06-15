28e81bb4bb539cc82d64020a3935f20115162012
qinlinj <justinqinlin@gmail.com>
2025-06-14 21:34:56 -0700

    feat(PalindromeNumber.java): implement isPalindrome method to check if an integer is a palindrome and add method documentation for clarity

--- src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_925_math_tricks_practice/_925_b_PalindromeNumber.java
+++ src/main/java/org/qinlinj/algoframework/_900_other_common_algo_tricks/_920_mathematical_techniques_II/_925_math_tricks_practice/_925_b_PalindromeNumber.java
@@ -1,4 +1,25 @@
 package org.qinlinj.algoframework._900_other_common_algo_tricks._920_mathematical_techniques_II._925_math_tricks_practice;
 
 public class _925_b_PalindromeNumber {
+    /**
+     * Determines if an integer is a palindrome
+     *
+     * @param x the integer to check
+     * @return true if x is a palindrome, false otherwise
+     */
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        // y is the reversed version of x
+        int y = 0;
+        while (temp > 0) {
+            int lastDigit = temp % 10;
+            temp = temp / 10;
+            // Technique to build number from most significant digit
+            y = y * 10 + lastDigit;
+        }
+        return y == x;
+    }
 }
